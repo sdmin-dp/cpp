48756e45ea02a5299f82da24cffeba5768d02213
yusiwen2014 <3907363360@qq.com>
2026-01-31 17:23:35 +0800

    modified:   ".cph-ng/code\346\226\207\344\273\266/text.cpp.bin" 	modified:   "aha_659_\346\270\205\351\231\244\351\232\234\347\242\215\357\274\210\344\272\214\345\210\206\345\233\276\346\234\200\345\260\217\347\202\271\350\246\206\347\233\226\357\274\211.cpp" 	modified:   "code\346\226\207\344\273\266/text.cpp"
    	modified:   ".cph-ng/code\346\226\207\344\273\266/text.cpp.bin"
    	deleted:    "\344\275\231\346\200\235\347\250\263.code-workspace"
    
    	"acc_659_\346\270\205\351\231\244\351\232\234\347\242\215\357\274\210\344\272\214\345\210\206\345\233\276\346\234\200\345\260\217\347\202\271\350\246\206\347\233\226\357\274\211.cpp"
    	"acc_\346\257\224\350\265\2331_\347\251\272\347\223\266\346\215\242\351\245\256\346\226\231.cpp"
    	"\351\253\230\347\262\276\345\272\246\351\231\244\346\263\225.cpp"

--- aha_659_清除障碍（二分图最小点覆盖）.cpp
+++ aha_659_清除障碍（二分图最小点覆盖）.cpp
@@ -1,22 +1,45 @@
 #include<bits/stdc++.h>
-#define ll long long
 using namespace std;
+#define ll long long
 #define el '\n'
-const ll N=1e5+5;
-
-void solve(){
-    
+const ll N=200+5;
+ll n,m;
+vector<ll> g[N];
+bool vis[N];
+ll res[N];
+ll cnt=0;
+bool dfs(ll x){
+//	cerr<<"you are a dog";
+	for(auto i:g[x]){
+		if(!vis[i]){
+			vis[i]=1;
+			if(!res[i]||dfs(res[i])){
+				res[i]=x;
+				return 1;
+			}
+		}
+	}
+	return 0;
 }
-
-int main() {
-    //ios::sync_with_stdio(0);
-    //cin.tie(0);cout.tie(0);
-    //freopen("xxx.in","r",stdin);
-    //freopen("xxx.out","w",stdout);
-    ll T=1;
-    //cin>>T;
-    while(T--){
-        solve();
+void solve(){
+	cin>>n>>m;
+	for(int i=1;i<=m;i++){
+        ll x,y;
+        cin>>x>>y;
+        g[x].push_back(y);
     }
-    return 0;
+    ll cnt=0;
+    for(int i=1;i<=n;i++){
+        memset(vis,0,sizeof(vis));
+        cnt+=dfs(i);
+    }
+    cout<<cnt;
+}
+int main(){
+	ll T=1;
+	//cin>>T;
+	while(T--){
+		solve();
+	}
+	return 0;
 }
--- code文件/text.cpp
+++ code文件/text.cpp
@@ -1,22 +1,51 @@
 #include<bits/stdc++.h>
 #define ll long long
+
 using namespace std;
-#define el '\n'
-const ll N=1e5+5;
-ll a=1,b=1;
-void solve(){
-    1||a++&&b++||0;
-    cout<<a<<" "<<b;
+
+vector<int> g[1005];
+int match[1005], vis[1005];
+
+bool dfs(int u) {
+    for (auto v : g[u]) {
+        if (!vis[v]) {
+            vis[v] = true;
+            if (!match[v] || dfs(match[v])) {
+                match[v] = u;
+                return 1 ;
+            }
+        }
+    }
+    return 0;
 }
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    for (int i = 1; i <= k; i++) {
+        int x, y;
+        cin >> x >> y;
+        g[x].push_back(y);
+    }
+    int cnt = 0;
+    for (int i = 1; i <= n; i++) {
+        memset(vis, 0, sizeof(vis));
+        cnt += dfs(i);
+    }
+    cout << cnt;
+}
+
 int main() {
-    //ios::sync_with_stdio(0);
-    //cin.tie(0);cout.tie(0);
-    //freopen("xxx.in","r",stdin);
-    //freopen("xxx.out","w",stdout);
-    ll T=1;
-    //cin>>T;
-    while(T--){
+    ios::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+
+    // freopen("xxx.in", "r", stdin);
+    // freopen("xxx.out", "w", stdout);
+
+    int T = 1; // cin >> T;
+    while(T--) {
         solve();
     }
+
     return 0;
 }
