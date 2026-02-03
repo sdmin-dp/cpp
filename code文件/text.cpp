f59029bd41d4be1834fc5c425800417f52c60fb1
yusiwen2014 <3907363360@qq.com>
2026-02-03 19:45:49 +0800

    modified:   P3372.cpp 	modified:   "code\346\226\207\344\273\266/text.cpp" 	renamed:    "\346\250\241\346\235\277\345\217\212\351\207\215\350\246\201\344\273\243\347\240\201/\347\272\277\346\256\265\346\240\221.cpp" -> "\346\250\241\346\235\277\345\217\212\351\207\215\350\246\201\344\273\243\347\240\201/\347\272\277\346\256\265\346\240\221\346\256\213\350\241\200\347\211\210.cpp" 	new file:   "\346\250\241\346\235\277\345\217\212\351\207\215\350\246\201\344\273\243\347\240\201/\347\272\277\346\256\265\346\240\221\346\273\241\350\241\200\347\211\210(\346\207\222\346\240\207\350\256\260).cpp"

--- code文件/text.cpp
+++ code文件/text.cpp
@@ -3,8 +3,10 @@
 using namespace std;
 #define el '\n'
 const ll N=1e5+5;
-struct node{
-    ll l,r,sum;
+struct segment{
+    ll l,r;
+    ll sum;
+    ll tag;
 }t[N*4];
 ll n,m;
 ll a[N];
@@ -12,57 +14,72 @@ void pushup(ll idx){
     t[idx].sum=t[idx*2].sum+t[idx*2+1].sum;
 }
 void build(ll l=1,ll r=n,ll idx=1){
-    t[idx].l=l;
-    t[idx].r=r;
-    t[idx].sum=0;
+    t[idx].l=l;t[idx].r=r;
     if(l==r){
         t[idx].sum=a[l];
         return;
     }
-    ll mid=(l+r)>>1;
+    ll mid=(l+r)/2;
     build(l,mid,idx*2);
     build(mid+1,r,idx*2+1);
     pushup(idx);
 }
-ll query(ll b,ll e,ll idx=1){
-    ll l=t[idx].l,r=t[idx].r;
-    if(b<=l&&r<=e) return t[idx].sum;
-    if(b>r||e<l) return 0;
-    return query(b,e,idx*2)+query(b,e,idx*2+1);
+void pushdown(ll idx){
+    ll l=t[idx].l;
+    ll r=t[idx].r;
+    ll tag=t[idx].tag;
+    if(l==r||tag==0) return;
+    //1.将左右儿子的更新
+    //2.更新左右儿子的tag
+    //3.清空自己的tag
+    t[idx*2].sum+=(t[idx*2].r-t[idx*2].l+1)*tag;
+    t[idx*2+1].sum+=(t[idx*2+1].r-t[idx*2+1].l+1)*tag;
+    t[idx*2].tag+=tag;
+    t[idx*2+1].tag+=tag;
+    t[idx].tag=0;
 }
-void update(ll b,ll e,ll k,ll idx=1){
-//    cout<<idx<<" ";
-    if(t[idx].l==t[idx].r){
-        if(t[idx].l>=b&&t[idx].l<=e){
-            t[idx].sum+=k;
-        }
+void update(ll s,ll e,ll k,ll idx=1){
+    ll l=t[idx].l;
+    ll r=t[idx].r;
+    if(s<=l&&r<=e){
+        ll len=r-l+1;
+        t[idx].sum+=len*k;
+        t[idx].tag+=k;
         return;
     }
-    if(t[idx].l>e||t[idx].r<b) return;
-    update(b,e,k,idx*2);
-    update(b,e,k,idx*2+1);
+    pushdown(idx);
+    ll mid=(l+r)/2;
+    if(s<=mid) update(s,e,idx*2);
+    if(e>mid) update(s,e,k,idx*2+1);
     pushup(idx);
+}
+ll query(ll s,ll e,ll idx=1){
+    ll l=t[idx].l,r=t[idx].r;
+    if(s<=l&&r<=e) return t[idx].sum;
+    if(s<r||e<l) return 0;
+    pushdown(idx);
+    return query(s,e,idx*2)+query(s,e,idx*2+1);
 }
 void solve(){
     cin>>n>>m;
     for(int i=1;i<=n;i++) cin>>a[i];
     build();
-    for(int i=1;i<=m;i++){
+    while(m--){
         ll op,x,y,k;
         cin>>op;
-//        cout<<op<<el;
         if(op==1){
             cin>>x>>y>>k;
             update(x,y,k);
-        }else{
+        }
+        else{
             cin>>x>>y;
             cout<<query(x,y)<<el;
         }
-    }   
+    }
 }
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);cout.tie(0);
+    //ios::sync_with_stdio(0);
+    //cin.tie(0);cout.tie(0);
     //freopen("xxx.in","r",stdin);
     //freopen("xxx.out","w",stdout);
     ll T=1;
@@ -71,4 +88,4 @@ int main() {
         solve();
     }
     return 0;
-}	
+}
--- 模板及重要代码/线段树满血版(懒标记).cpp
+++ 模板及重要代码/线段树满血版(懒标记).cpp
@@ -89,6 +89,3 @@ int main() {
     }
     return 0;
 }
-/*
-
-*/
