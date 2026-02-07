ee2f42638dba627215e02238e73ecba31e9539b7
yusiwen2014 <3907363360@qq.com>
2026-02-07 18:55:02 +0800

    modified:   B3851.cpp

--- B3851.cpp
+++ B3851.cpp
@@ -1,72 +1,92 @@
 #include<bits/stdc++.h>
-using namespace std;
 #define ll long long
+using namespace std;
 #define el '\n'
-const ll N=2e1+5;
-struct node{
-	ll x,q;
-	string name="00";
-};
-node b[300];
-map<char,ll> mp;
-ll n,m;  
-ll x,y;
-string s;
+const ll N=1e2+5;
+ll n,m;
+string s[N];
 ll a[N][N];
-ll mx[10];
-string w={"0123456789ABCDEF"};
-bool cmp(node a,node b){
-	if(a.q!=b.q) return a.q>b.q;
-	else return a.x<b.x;
+ll ans[N][N];
+pair<ll,ll> sum[257];
+map<char,ll> mp;
+map<ll,char> mp2;
+ll stoten(string s){
+	return mp[s[0]]*16+mp[s[1]];
 }
-void solve()
-{
-	mp.insert({'0',0});mp.insert({'1',1});mp.insert({'2',2});mp.insert({'3',3});mp.insert({'4',4});mp.insert({'5',5});mp.insert({'6',6});mp.insert({'7',7});mp.insert({'8',8});mp.insert({'9',9});mp.insert({'A',10});mp.insert({'B',11});mp.insert({'C',12});mp.insert({'D',13});mp.insert({'E',14});mp.insert({'F',15});
+string t;
+bool cmp(pair<ll,ll> x,pair<ll,ll> y){
+	if(x.first!=y.first) return x.first>y.first;
+	else return x.second<y.second;
+}
+void solve(){
+	for(int i=0;i<=255;i++) sum[i].second=i;
+	
 	cin>>n;
-	for(int i=1;i<=n;i++){
-		cin>>s;
-		m=int(s.size()/2);
-		for(int j=0;j<s.size()/2;j++){
-			x=mp[s[j*2]],y=mp[s[j*2+1]];
-			a[i][j]=x*16+y;
-			b[a[i][j]].q++;
-			b[a[i][j]].x=a[i][j];
-			b[a[i][j]].name[0]=s[j*2];
-			b[a[i][j]].name[1]=s[j*2+1];
-		}
+	for(int i=0;i<=9;i++){
+		mp2.insert({i,char(i+48)});
+		mp.insert({char(i+48),i});
 	}
-	sort(b,b+290,cmp);
-	for(int i=0;i<16;i++){
-		cout<<b[i].name;
+	for(int i=10;i<=35;i++){
+		mp2.insert({i,char(i-10+'A')});
+		mp.insert({char(i-10+'A'),i});
+	}
+	for(int i=1;i<=n;i++) cin>>s[i];
+	
+	for(int i=1;i<=n;i++){
+		ll cnt=0;
+		for(int j=0;j<s[i].size();j+=2){
+			t=char(s[i][j]);
+			t+=char(s[i][j+1]);
+			a[i][++cnt]=stoten(t);
+			sum[a[i][cnt]].first++;
+		}
+		m=cnt;
+	}
+	sort(sum,sum+256,cmp);
+	for(int i=0;i<=15;i++){
+		ll u=sum[i].second;
+		// cout<<u<<" ";
+		t="";
+		for(int i=1;i<=2;i++){
+			ll k=u%16;
+			t+=mp2[k];
+			// cout<<mp2[k]<<el;
+			u/=16;
+		}
+		cout<<t[1]<<t[0];
 	}
 	cout<<el;
 	for(int i=1;i<=n;i++){
-		for(int j=0;j<m;j++){
-			ll mn=LONG_LONG_MAX;
-			ll p=0;
-			for(int k=0;k<16;k++){
-				if(abs(a[i][j]-b[k].x)<mn){
-					mn=abs(a[i][j]-b[k].x);
-					p=k;
+		for(int j=1;j<=m;j++){
+			ll id=0,mn=1e9;//id记录灰阶值，mn记录最小差值
+			for(int k=0;k<=15;k++){
+				//abs(a[i][j]-sum[i].second)
+				if(abs(a[i][j]-sum[k].second)<mn){
+					id=k;
+					mn=abs(a[i][j]-sum[k].second);
 				}
 			}
-			cout<<w[p];
+			ans[i][j]=id;
+			// cerr<<ans[i][j]<<" ";
+		}
+		// cerr<<el;
+	}
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=m;j++){
+			cout<<mp2[ans[i][j]];
 		}
 		cout<<el;
 	}
 }
-int main()
-{
+int main(){
 	ios::sync_with_stdio(0);
 	cin.tie(0);cout.tie(0);
-//	freopen("xxx.in","r",stdin);
-//	freopen("xxx.out","w",stdout);
+	//freopen("xxx.in","r",stdin);
+	//freopen("xxx.out","w",stdout);
 	ll T=1;
-//	cin>>T;
-	while(T--)
-	{
+	//cin>>T;
+	while(T--){
 		solve();
 	}
 	return 0;
 }
-
