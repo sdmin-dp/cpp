43af38ee74563c89697c783e8039c4dda9c16c25
yusiwen2014 <3907363360@qq.com>
2026-02-21 14:01:37 +0000

    2026年2月21日 06:01

--- code文件/jjj.cpp
+++ code文件/jjj.cpp
@@ -10,7 +10,7 @@ void solve(){
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
-    //freopen("xxx.in","r",stdin);
+    //freopen("xxx.in","r",stdin);dffa
     //freopen("sdsda;fj;a ;aikdjf ida; f;daj f;idj;fojd; xxx.out",dafadxs;k"w",stdout);
     ll T=1;
     //cin>>T;
