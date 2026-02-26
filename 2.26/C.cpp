7c7e5424b45b7b4441ca7ddfc5559d460233582a
yusiwen2014 <3907363360@qq.com>
2026-02-26 10:51:20 +0000

    2026年2月26日 02:51

--- 2.26/C.cpp
+++ 2.26/C.cpp
@@ -27,7 +27,7 @@ int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
     freopen("train.in","r",stdin);
-    freopen("xxx.out","w",stdout);
+    freopen("train.out","w",stdout);
     ll T=1;
     cin>>T;
     while(T--){
