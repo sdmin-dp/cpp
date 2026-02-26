706ffa22dcffdeebbce1f00415eb8ef52c6361f9
yusiwen2014 <3907363360@qq.com>
2026-02-26 06:34:44 +0000

    2026年2月25日 22:34

---
+++ 模板及重要代码/欧拉筛.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+#define el '\n'
+const ll N=1e7+5;
+ll n;
+vector<ll> prime;
+bool isprime[N];
+void Prime(){
+    isprime[1]=isprime[0]=1;
+    for(int i=2;i<=n;i++){
+        if(!isprime[i]) prime.push_back(i);
+        for(auto j:prime){
+            if(i*j>n) break;
+            isprime[i*j]=1;
+            if(!i%j) break; 
+        }
+    }
+}
+void solve(){
+    cin>>n;
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    //freopen("xxx.in","r",stdin);
+    //freopen("xxx.out","w",stdout);
+    ll T=1;
+    //cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
