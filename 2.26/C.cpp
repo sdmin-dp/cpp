d9212430a2478c24740ae5d8168af1c33eeb4496
yusiwen2014 <3907363360@qq.com>
2026-02-26 10:44:46 +0000

    2026年2月26日 02:44

--- 2.26/C.cpp
+++ 2.26/C.cpp
@@ -2,11 +2,26 @@
 #define ll long long
 using namespace std;
 #define el '\n'
-const ll N=1e5+5;
+const ll N=1e2+5;
 ll n,m;
-
+vector<ll> g[N];
 void solve(){
-    
+    for(int i=1;i<=n;i++) g[i].clear();
+    cin>>n>>m;
+    for(int i=1;i<=m;i++){
+        ll x,y;
+        cin>>x>>y;
+        g[x].push_back(y);
+        g[y].push_back(x);
+    }
+    ll cnt=0;
+    for(int i=1;i<=n;i++){
+        if(g[i].size()%2!=0){
+            cnt++;
+        }
+    }
+    if(cnt!=0&&cnt!=2) cout<<"NO"<<el;
+    else cout<<"YES"<<el;
 }
 int main(){
     ios::sync_with_stdio(0);
@@ -16,6 +31,7 @@ int main(){
     ll T=1;
     cin>>T;
     while(T--){
+        
         solve();
     }
     return 0;
