6f93b559c3dfacff294c88c74e4fed98711d76df
yusiwen2014 <3907363360@qq.com>
2026-02-26 02:30:26 +0000

    2026年2月25日 18:30

--- 东方博宜2026.2月月赛/A.cpp
+++ 东方博宜2026.2月月赛/A.cpp
@@ -5,9 +5,18 @@ using namespace std;
 const ll N=1e5+5;
 ll n,q;
 ll a[N];
+ll sum1[N],sum2[N],sum3[N];
 void solve(){
     cin>>n>>q;
-    f
+    for(int i=1;i<=n;i++){
+        cin>>a[i];
+        if(a[i]==1) sum1[i]=sum1[i-1]+1;
+        else if(a[i]==2) sum2[i]=sum2[i-1]+1;
+        else if(a[i]==3) sum3[i]=sum3[i-1]+1;
+    }
+    for(int i=1;i<=q;i++){
+        
+    }
 }
 int main(){
     ios::sync_with_stdio(0);
