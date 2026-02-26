5c345e25e1c2f5d4d673f4e3834542afdc3ca0fa
yusiwen2014 <3907363360@qq.com>
2026-02-26 11:50:36 +0000

    2026年2月26日 03:50

--- 2.26/D.cpp
+++ 2.26/D.cpp
@@ -6,10 +6,23 @@ const ll N=1e5+5;
 ll n,m;
 pair<ll,ll> a[N];
 ll ans[N];
+ll sumdan;//下m-1天蛋的费用，用于判断
+ll sum;//统计后面提供的美味值
 void solve(){
     cin>>n>>m;
-    for(int i=1;i<=n;i++){
-        
+    for(int i=1;i<=n;i++) cin>>a[i].first>>a[i].second;
+    for(int i=n;i>=1;i--){
+        if(a[i].first>=0&&a[i].second>=0) ans[i]=max(m*a[i].first,(m-1)*a[i].first+a[i].second);
+        if(a[i].first>=0&&a[i].second<0) ans[i]=m*a[i].first;
+        if(a[i].first<0&&a[i].second>=0) ans[i]=a[i].second;
+        else{
+            if(-min(a[i].first*m,a[i].second)>sum){
+                sum=0;
+                n=i-1;
+            }else if(min(a[i].first,a[i].second)){
+                
+            }
+        }
     }
 }
 int main(){
