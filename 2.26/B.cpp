4c225aca6eac5ac7954159ccd6d2f2219f5723ea
yusiwen2014 <3907363360@qq.com>
2026-02-26 10:20:55 +0000

    2026年2月26日 02:20

--- 2.26/B.cpp
+++ 2.26/B.cpp
@@ -47,7 +47,7 @@ int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
     freopen("scalerotate.in","r",stdin);
-    freopen("xxx.out","w",stdout);
+    freopen("scalerotate.out","w",stdout);
     ll T=1;
     //cin>>T;
     while(T--){
