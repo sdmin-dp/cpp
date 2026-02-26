e15abea9d59105f934e7c5c86b5cd820adb4273d
yusiwen2014 <3907363360@qq.com>
2026-02-26 10:11:06 +0000

    2026年2月26日 02:11

--- 2.26/B.cpp
+++ 2.26/B.cpp
@@ -16,8 +16,8 @@ void zhuan90(){
             b[I][J]=a[i][j];
         }
     }
-    for(int i=1;i<=n+1;i++){
-        for(int j=1;j<=m+1;j++){
+    for(int i=1;i<=m;i++){
+        for(int j=1;j<=n;j++){
             a[i][j]=b[i][j];
         }
     }
