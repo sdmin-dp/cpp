d12064d81f36d816c4b78edd2f434b704b7ce8ea
yusiwen2014 <3907363360@qq.com>
2026-02-26 10:05:55 +0000

    2026年2月26日 02:05

--- 2.26/B.cpp
+++ 2.26/B.cpp
@@ -6,10 +6,29 @@ const ll N=1e1+5;
 ll r,k,n,m;
 char a[N][N];
 char ans[N*10][N*10];
+void zhuan90(){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            ll I=i,J=j;
+            swap(I,J);
+            J=n-j+1;
+            ans[I][J]=a[i][j];
+        }
+    }
+}
 void solve(){
     cin>>r>>k>>n>>m;
     for(int i=1;i<=n;i++){
-        
+        for(int j=1;j<=m;j++){
+            cin>>a[i][j];
+        }
+    }
+    zhuan90();
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            cerr<<ans[i][j];
+        }
+        cerr<<el;
     }
 }
 int main(){
