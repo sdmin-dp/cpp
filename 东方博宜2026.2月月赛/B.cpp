4b511f7e0b3173f504484f949239e527b7183390
yusiwen2014 <3907363360@qq.com>
2026-02-26 03:06:29 +0000

    2026年2月25日 19:06

--- 东方博宜2026.2月月赛/B.cpp
+++ 东方博宜2026.2月月赛/B.cpp
@@ -3,9 +3,18 @@
 using namespace std;
 #define el '\n'
 const ll N=1e5+5;
-ll x;
+ll n,k;
+ll a[N];
 void solve(){
-    
+    cin>>n>>k;
+    for(int i=1;i<=n;i++){
+        cin>>a[i];
+    }
+    //先走哪边？\
+    如果走远的一边\
+    x     o   x     x\
+    1 2 3 4 5 6 7 8 9\
+    答案是，走近的一边
 }
 int main(){
     ios::sync_with_stdio(0);
