a301541234575e65e5f6ad43e127e3a9f76fd8d4
yusiwen2014 <3907363360@qq.com>
2026-02-26 14:45:38 +0000

    2026年2月26日 06:45

--- CSP-J yusiwen/scalerotate/scalerotate.cpp
+++ CSP-J yusiwen/scalerotate/scalerotate.cpp
@@ -46,7 +46,7 @@ void solve(){
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
-    freopen("scsalerotate.in","r",stdin);
+    freopen("scalerotate.in","r",stdin);
     freopen("scalerotate.out","w",stdout);
     ll T=1;
     //cin>>T;
