9964c80a1d94f5b20283559c8ef7843cfefaf1ad
yusiwen2014 <3907363360@qq.com>
2026-02-26 05:38:13 +0000

    2026年2月25日 21:38

--- U652633.cpp
+++ U652633.cpp
@@ -3,9 +3,35 @@
 using namespace std;
 #define el '\n'
 const ll N=1e5+5;
-
+ll n;
+vector<pair<ll,ll>> v;
+void prime(){
+    ll x=n;
+    for(int i=1;i*i<=x;i++){
+        if(n%i==0){
+            ll cnt=0;
+            while(x%i==0){
+                cnt++;
+                x/=i;
+            }
+            v.push_back({i,cnt});
+        }
+    }
+}
 void solve(){
-    
+    cin>>n;
+    prime();
+    bool flag=1;
+    for(auto i:v){
+        if(flag){
+            cout<<i.first<<"^"<<i.second;
+            flag=0;
+        }
+        else{
+            cout<<i.first<<"^"<<i.second;
+        }
+        
+    }
 }
 int main(){
     ios::sync_with_stdio(0);
