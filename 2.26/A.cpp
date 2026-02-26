d24a2df073f7cf4acab72014c156e7a80dd3d6b5
yusiwen2014 <3907363360@qq.com>
2026-02-26 09:48:04 +0000

    2026年2月26日 01:48

--- 2.26/A.cpp
+++ 2.26/A.cpp
@@ -2,15 +2,33 @@
 #define ll long long
 using namespace std;
 #define el '\n'
-const ll N=1e5+5;
+const ll N=10+5;
 string s;
 ll n;
 ll a[N];
-void Sort(){
-
+ll b[N];
+ll cnt=0;
+void Sort(ll l,ll r){
+    if(l<r){
+        ll mid=(l+r)/2;
+        Sort(l,mid);
+        Sort(mid+1,r);
+        ll i=1,j=mid+1,t=1;
+        while(i<=mid&&j<=r){
+            if(a[i]<=a[j]) b[t++]=a[i++];
+            else b[t++]=a[j++],cnt+=mid-i+1;
+        }
+        while(i<=mid) b[t++]=a[i++];
+        while(j<=r) b[t++]=a[i++];
+        for(i=l;i<=r;i++) a[i]=b[i];
+    }
 }
 void solve(){
     cin>>s;
+    n=s.size();
+    for(int i=0;i<s.size();i++) a[i+1]=s[i]-'A'+1;
+    Sort(1,n);
+    cout<<cnt;
 }
 int main(){
     ios::sync_with_stdio(0);
