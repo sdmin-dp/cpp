de068884a37614b7857fe19b30148f0b1fbc5e78
yusiwen2014 <3907363360@qq.com>
2026-02-27 12:52:13 +0000

    2026年2月27日 04:52

---
+++ T736381.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+#define el '\n'
+const ll N=1e5+5;
+ll n,m;
+ll a[N];
+void solve(){
+    cin>>n;
+    for(int i=1;i<=n;i++) cin>>a[i];
+    cin>>m;
+    for(int i=1;i<=n;i++){
+        
+    }
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    //freopen("xxx.in","r",stdin);
+    //freopen("xxx.out","w",stdout);
+    ll T=1;
+    //cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
