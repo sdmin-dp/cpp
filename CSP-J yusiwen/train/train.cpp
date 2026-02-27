3761e9267ae7c64ef80950ee9fd619b3a29bf110
yusiwen2014 <3907363360@qq.com>
2026-02-27 10:42:38 +0000

    2026年2月27日 02:42

--- CSP-J yusiwen/train/train.cpp
+++ CSP-J yusiwen/train/train.cpp
@@ -5,7 +5,6 @@ using namespace std;
 const ll N=5e2+5;
 ll n,m;
 vector<ll> g[N];
-bool vis[N];
 ll fa[N];
 ll sz[N];
 ll find(ll x){
@@ -17,9 +16,12 @@ void add(ll x,ll Y){
     if(x!=Y) fa[x]=Y;
 }
 void solve(){
-    for(int i=0;i<n;i++) g[i].clear();
     cin>>n>>m;
-    for(int i=0;i<n;i++) fa[i]=i;
+    for(int i=0;i<n;i++){
+        g[i].clear();
+        fa[i]=i;
+        sz[i]=0;
+    }
     for(int i=1;i<=m;i++){
         ll x,y;
         cin>>x>>y;
@@ -29,19 +31,19 @@ void solve(){
     }
     ll cnt=0;
     for(int i=0;i<n;i++){
-        if(g[i].size()%2!=0){
-            cnt++;
+        if(g[i].size()%2!=0) cnt++;
+    }
+    for(int i=0;i<n;i++){
+        if(!g[i].empty()){
+            sz[find(i)]++;
         }
     }
-    for(int i=0;i<n;i++) fa[i]=find(i);
-    for(int i=0;i<n;i++) sz[fa[i]]++;
-    ll sum=0;
-    for(int i=0;i<n;i++) if(sz[i]>=2) sum++;
-    // for(int i=0;i<n;i++) cerr<<fa[i]<<" ";
-    // cerr<<el;
-    // for(int i=0;i<n;i++) cerr<<sz[i]<<" ";
-    if((cnt!=0&&cnt!=2)||sum>1) cout<<"NO"<<el;
-    else cout<<"YES"<<el;
+    ll num_comp=0;
+    for(int i=0;i<n;i++){
+        if(fa[i]==i && sz[i]>0) num_comp++;
+    }
+    if((cnt==0 || cnt==2) && num_comp<=1) cout<<"YES"<<el;
+    else cout<<"NO"<<el;
 }
 int main(){
     ios::sync_with_stdio(0);
