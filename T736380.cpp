f47985ade81b0f5c6299c448c71384ca0cb64203
yusiwen2014 <3907363360@qq.com>
2026-02-27 12:49:24 +0000

    2026年2月27日 04:49

--- T736380.cpp
+++ T736380.cpp
@@ -8,14 +8,17 @@ ll x;
 ll v[3];
 void solve(){
     cin>>n>>a>>b;
+    ll ans1=0,ans2=0;
     for(int i=1;i<=n;i++){
         cin>>x;
         v[0]=a;v[1]=b;v[2]=x;
         sort(v,v+2+1);
         if(v[0]*v[1]==v[2]||v[0]+v[1]==v[2]){
-            
+            ans1++;
+            ans2=max(ans2,x);
         }
     }
+    cout<<
 }
 int main(){
     ios::sync_with_stdio(0);
