4bc23e7a6e370b4457b471a3a25a58527529d7c9
yusiwen2014 <3907363360@qq.com>
2026-02-27 03:51:49 +0000

    2026年2月26日 19:51

--- CSP-J yusiwen/bamboo/bamboo.cpp
+++ CSP-J yusiwen/bamboo/bamboo.cpp
@@ -27,7 +27,7 @@ void solve(){
                 res=max(res,dp[cost][i][j]);
                 for(int k=1;k<=n;k++){
                     if(l[k]==l[i]||l[k]==l[j])continue; // 新竹子的长度不能和前两根竹子的长度相同
-                    if(l[k]>l[i]&&l[k]>l[j])continue;
+                    if(l[k]>l[i]&&l[k]>l[j])continue;// 新竹子的长度不能大于前两根竹子的长度
                     if(l[k]<l[i]&&l[k]<l[j])continue;
                     if(cost+w[k]>c)continue;
                     if(dp[cost+w[k]][j][k]<dp[cost][i][j]+l[k]){
