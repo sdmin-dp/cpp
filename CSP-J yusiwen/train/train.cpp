8fffc4637c62f5380e772896cdbd065f85d187bb
yusiwen2014 <3907363360@qq.com>
2026-02-27 10:36:37 +0000

    2026年2月27日 02:36

--- CSP-J yusiwen/train/train.cpp
+++ CSP-J yusiwen/train/train.cpp
@@ -37,6 +37,7 @@ void solve(){
     for(int i=0;i<n;i++) sz[fa[i]]++;
     ll sum=0;
     for(int i=0;i<n;i++) if(sz[i]>=2) sum++;
+    cerr<<sum;
     if((cnt!=0&&cnt!=2)||sum>1) cout<<"NO"<<el;
     else cout<<"YES"<<el;
 }
