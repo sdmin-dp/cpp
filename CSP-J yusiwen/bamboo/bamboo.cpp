6f022d05472d92a75e512a7ae066b3d071b8ce79
yusiwen2014 <3907363360@qq.com>
2026-02-27 03:48:52 +0000

    2026年2月26日 19:48

--- CSP-J yusiwen/bamboo/bamboo.cpp
+++ CSP-J yusiwen/bamboo/bamboo.cpp
@@ -1,6 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define ll long long
 const ll N=55;
+ll n,c;
 ll dp[N][N][N],l[N],w[N];
 int maiin(){
 	cin>>n>>c;
@@ -10,28 +12,29 @@ int maiin(){
 		for(int j=1;j<=n;j++){
 			if(l[i]==l[j]) continue;
 			if(w[i]+w[j]>c) continue;
-			dp[w][i]+w[j][i][j]=max(dp[w[i]+w[j][i][j],l[i]+l[j]);
+			dp[w[i]]+w[j][i][j]=max(dp[w[i]]+w[j][i][j],l[i]+l[j]);
 		}
 	}
+    
+	ll res=0;
 	memset(dp,-1,sizeof(dp));
 	for(int cost=0;cost<=c;cost++){
-		for(int i=1;j<=n;i++){
+		for(int i=1;i<=n;i++){
 			if(dp[cost][i][j]==-1) continue;
 			for(int k=1;k<=n;k++){
-				if(l[kl=l[i]){
+				if(l[k]=l[i]){
 					if[l[k]==l[i]) continue;
 					if(l[k]==l[j]) continue;
 					if(l[j]>l[i]&&l[k]>l[j]) continue;
 					if(l[k]<l[i]&&l[k]<l[j]) continue;
 					if(cost+w[k]>c) continue;
-                    if(dp[cost+w[k]][j][k]<dp[cost][i][j]){
-
+                    if(dp[cost+w[k]][j][k]<dp[cost][i][j]+l[k]){
+                        dp[cost+w[k]][j][k]+dp[cost][i][j]+l[k];
+                        res=max(res,dp[cost][i][j]+l[k]);
                     }
-					dp[cost+w[k]][j][k]=max(dp[cost+w[k]][j][k],dp[cost][i][j]+l[k]);
 			}
 		}
 	}
-	ll res=0;
 	for(int cost=0;cost<=c;cost++){
 		for(int i=1;i<=n;i++){
 			for(int j=1;j<=n;j++){
@@ -40,4 +43,4 @@ int maiin(){
 		}
 	}
 	cout<<res;
-}```
+}
