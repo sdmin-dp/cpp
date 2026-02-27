62138e06f8a5df08d8faba70a834c0ac0cedd044
yusiwen2014 <3907363360@qq.com>
2026-02-27 10:55:19 +0000

    2026年2月27日 02:55

--- CSP-J yusiwen/ab/ab.cpp
+++ CSP-J yusiwen/ab/ab.cpp
@@ -4,17 +4,31 @@ using namespace std;
 #define el '\n'
 const ll N=10+5;
 string s;
-void solve(){
-    ll cnt=0,ans=0;
-    for(auto i:s){
-        if(i=='B'){
-            cnt++;
+ll n;
+ll a[N];
+ll b[N];
+ll cnt=0;
+void Sort(ll l,ll r){
+    if(l<r){
+        ll mid=(l+r)/2;
+        Sort(l,mid);
+        Sort(mid+1,r);
+        ll i=l,j=mid+1,t=1;
+        while(i<=mid&&j<=r){
+            if(a[i]<=a[j]) b[t++]=a[i++];
+            else b[t++]=a[j++],cnt+=mid-i+1;
         }
-        if(i=='A'){
-            ans+=cnt;
-        }
+        while(i<=mid) b[t++]=a[i++];
+        while(j<=r) b[t++]=a[j++];
+        for(i=l;i<=r;i++) a[i]=b[i];
     }
-    cout<<ans;
+}
+void solve(){
+    cin>>s;
+    n=s.size();
+    for(int i=0;i<s.size();i++) a[i+1]=s[i]-'A'+1;
+    Sort(1,n);
+    cout<<cnt;
 }
 int main(){
     ios::sync_with_stdio(0);
