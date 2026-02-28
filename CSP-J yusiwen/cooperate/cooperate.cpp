2822b08d410449e065ce6c6d757bf3cbcd3b0aff
yusiwen2014 <3907363360@qq.com>
2026-02-28 12:09:27 +0000

    2026年2月28日 04:09

--- CSP-J yusiwen/cooperate/cooperate.cpp
+++ CSP-J yusiwen/cooperate/cooperate.cpp
@@ -3,9 +3,24 @@
 using namespace std;
 #define el '\n'
 const ll N=1e5+5;
-
+ll n;
+pair<ll,ll> a[N];
+ll ans=1e18;
+void dfs(ll x,ll sum1,ll sum2){
+    if(x==n+1){
+        ans=min(ans,max(sum1,sum2));
+        return;
+    }
+    dfs(x+1,sum1+a[x].first,sum2);
+    dfs(x+1,sum1,sum2+a[x].second);
+}
 void solve(){
-    
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        cin>>a[i].first>>a[i].second;
+    }
+    dfs(1,0,0);
+    cout<<ans;
 }
 int main(){
     ios::sync_with_stdio(0);
