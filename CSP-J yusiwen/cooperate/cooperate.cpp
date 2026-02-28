a3f9ab023ebbffb83c3424510ec6dcdd66a903e0
yusiwen2014 <3907363360@qq.com>
2026-02-28 12:11:39 +0000

    2026年2月28日 04:11

--- CSP-J yusiwen/cooperate/cooperate.cpp
+++ CSP-J yusiwen/cooperate/cooperate.cpp
@@ -25,8 +25,8 @@ void solve(){
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
-    //freopen("xxx.in","r",stdin);
-    //freopen("xxx.out","w",stdout);
+    freopen("cooperate.in","r",stdin);
+    freopen("cooperate.out","w",stdout);
     ll T=1;
     //cin>>T;
     while(T--){
