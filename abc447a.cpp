8e3916d17df488dee12a17d151b0ccc7f97f5cf3
yusiwen2014 <3907363360@qq.com>
2026-02-28 12:40:46 +0000

    2026年2月28日 04:40

---
+++ abc447a.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+#define el '\n'
+const ll N=1e5+5;
+ll n,m;
+void solve(){
+    cin>>n>>m;
+    cout<<(n>=m*2-1:)    
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    //freopen("xxx.in","r",stdin);
+    //freopen("xxx.out","w",stdout);
+    ll T=1;
+    //cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
