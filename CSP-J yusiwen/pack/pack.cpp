1a132071136b61768d852dafdbdb04ced0553572
yusiwen2014 <3907363360@qq.com>
2026-03-01 12:22:11 +0000

    2026年3月1日 04:22

--- CSP-J yusiwen/pack/pack.cpp
+++ CSP-J yusiwen/pack/pack.cpp
@@ -45,8 +45,8 @@ void solve(){
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
-    // freopen("pack.in","r",stdin);
-    // freopen("pack.out","w",stdout);
+    freopen("pack.in","r",stdin);
+    freopen("pack.out","w",stdout);
     ll T=1;
     //cin>>T;
     while(T--){
