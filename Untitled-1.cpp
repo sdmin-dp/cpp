ed502f27cd5dfa249581080daa5fecb9885d7077
yusiwen <3907363360@qq.com>
2026-03-03 14:05:01 +0000

    2026年3月3日 22:05

---
+++ Untitled-1.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+#define el '\n'
+const ll N=1e5+5;
+
+void solve(){
+    ll x;
+    cin>>x;
+    for( int i = 1;i<=x;i++){
+        
+    }
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    //freopen("xxx.in","r",stdin);
+    //freopen("xxx.out","w",stdout);
+    ll T=1;
+    //cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
