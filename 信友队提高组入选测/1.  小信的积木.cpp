8ab952ade3d8edd39559b8e1c295b1f4d572098e
yusiwen <3907363360@qq.com>
2026-03-03 12:23:47 +0000

    2026年3月3日 20:23

--- 信友队提高组入选测/1.  小信的积木.cpp
+++ 信友队提高组入选测/1.  小信的积木.cpp
@@ -2,10 +2,21 @@
 #define ll long long
 using namespace std;
 #define el '\n'
-const ll N=1e5+5;
-
+const ll N=5+5;
+ll n,m;
+ll s[N];
+ll vis[N];
+void dfs(ll x,ll y){
+    for(int i=1;i<=m;i++){
+        if(i>=m){
+            
+        }
+    }
+}
 void solve(){
-    
+    cin>>n;
+    for(int i=1;i<=n;i++) cin>>s[i];
+    cin>>m;
 }
 int main(){
     ios::sync_with_stdio(0);
