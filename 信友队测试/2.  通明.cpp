35be640c42c6cf6b40db65b01cc34cbaeb6685e2
yusiwen <3907363360@qq.com>
2026-03-04 11:47:37 +0000

    2026年3月4日 19:47

--- 信友队测试/2.  通明.cpp
+++ 信友队测试/2.  通明.cpp
@@ -3,9 +3,10 @@
 using namespace std;
 #define el '\n'
 const ll N=1e5+5;
-
+ll a[]={0,1,2,3,3,4,5,6};
 void solve(){
-    
+    ll pos=lower_bound(a+1,a+8,3)-a;
+    cout<<pos;
 }
 int main(){
     ios::sync_with_stdio(0);
