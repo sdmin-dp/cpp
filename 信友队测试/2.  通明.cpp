74932adcd57b39abe4b8b2121af2d0b356fd5f35
yusiwen <3907363360@qq.com>
2026-03-04 12:59:00 +0000

    2026年3月4日 20:59

--- 信友队测试/2.  通明.cpp
+++ 信友队测试/2.  通明.cpp
@@ -2,11 +2,23 @@
 #define ll long long
 using namespace std;
 #define el '\n'
-const ll N=1e5+5;
+const ll N=5e3+5;
 ll n,w;
 pair<ll,ll> a[N];
+ll dp[N];
+ll ans;
+//second -> id,first -> val
 void solve(){
-
+    cin>>n>>w;
+    for(int i=1;i<=n;i++){
+        cin>>a[i].first;
+        a[i].second=i;
+        dp[i]=1;
+    }
+    sort(a+1,a+n+1);
+    for(int i=1;i<=n;i++) for(int j=1;j<i;j++) if(a[j].first<a[i].first&&abs(a[i].second-a[j].second)<=w) dp[i]=max(dp[i],dp[j+1]);
+    for(int i=1;i<=n;i++) ans=max(ans,dp[i]);
+    cout<<ans;
 }
 int main(){
     ios::sync_with_stdio(0);
