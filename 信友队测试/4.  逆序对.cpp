14995fb6c4c03d36d4b535bc329d46b906fe3212
yusiwen <3907363360@qq.com>
2026-03-04 11:27:50 +0000

    2026年3月4日 19:27

--- 信友队测试/4.  逆序对.cpp
+++ 信友队测试/4.  逆序对.cpp
@@ -1,1 +1,21 @@
-#
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+#define el '\n'
+const ll N=1e5+5;
+
+void solve(){
+    
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    //freopen("xxx.in","r",stdin);
+    //freopen("xxx.out","w",stdout);
+    ll T=1;
+    //cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
