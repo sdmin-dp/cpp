28c3886be1aa8332a032dfbd516db7da62012fc3
yusiwen <3907363360@qq.com>
2026-03-04 13:23:15 +0000

    2026年3月4日 21:23

--- 信友队测试/3.  邦布照相.cpp
+++ 信友队测试/3.  邦布照相.cpp
@@ -6,10 +6,11 @@ const ll N=1e5+5;
 ll n,m;
 ll a[N];
 string ans="999999999999999999999999999999999999999999999999999999999999999999999999999999";
+vector<bool> flag(n+1,0);
 void dfs(ll x,string s){
     if(x==n+1){
         if(s.size()!=m) return;
-        vector<bool> flag(n+1,0);
+        flag.resize(n+1,0);
         for(int i=0;i<s.size();i++) flag[s[i]-48]=1;
         for(int i=1;i<=m;i++) if(!flag[i]) return;
         ans=min(ans,s);
