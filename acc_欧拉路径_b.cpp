9f489bf11eae85f9d9d37ae461090cc5a07ce6b5
yusiwen <3907363360@qq.com>
2026-03-07 09:03:23 +0000

    2026年3月7日 17:03

--- acc_欧拉路径_b.cpp
+++ acc_欧拉路径_b.cpp
@@ -1,34 +1,57 @@
-#include<bits/stdc++.h>
-#define ll long long
+#include <bits/stdc++.h>
 using namespace std;
-#define el '\n'
-const ll N=1e5+5;
-ll n,m;
-vector<ll> g[N];
-void solve(){
-    while(cin>>n>>m){
-        for(int i=1;i<=n;i++) g[i].clear();
-        for(int i=1;i<=m;i++){
-            ll x,y;
-            cin>>x>>y;
-            g[x].push_back(y);
-            g[y].push_back(x);
-        }
-        ll cnt=0;
-        for(int i=1;i<=n;i++) if(g[i].size()%2==1) cnt++;
-        if(cnt==0) cout<<1<<el;
-        else cout<<cnt/2<<el;
+
+const int N = 1e5 + 5;
+vector<int> g[N];
+bool vis[N];
+int deg[N];
+vector<int> component_nodes;
+
+void dfs(int u) {
+    vis[u] = true;
+    component_nodes.push_back(u);
+    for (int v : g[u]) {
+        if (!vis[v]) dfs(v);
     }
 }
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);cout.tie(0);
-    //freopen("xxx.in","r",stdin);
-    //freopen("xxx.out","w",stdout);
-    ll T=1;
-    //cin>>T;
-    while(T--){
-        solve();
+
+void solve() {
+    int n, m;
+    while (cin >> n >> m) {
+        for (int i = 1; i <= n; i++) {
+            g[i].clear();
+            vis[i] = false;
+            deg[i] = 0;
+        }
+        for (int i = 0; i < m; i++) {
+            int u, v;
+            cin >> u >> v;
+            g[u].push_back(v);
+            g[v].push_back(u);
+            deg[u]++; deg[v]++;
+        }
+
+        int total_strokes = 0;
+        for (int i = 1; i <= n; i++) {
+            if (!vis[i] && deg[i] > 0) {
+                component_nodes.clear();
+                dfs(i);
+                
+                int odd_cnt = 0;
+                for (int node : component_nodes) {
+                    if (deg[node] % 2 != 0) odd_cnt++;
+                }
+                
+                if (odd_cnt == 0) total_strokes += 1;
+                else total_strokes += odd_cnt / 2;
+            }
+        }
+        cout << total_strokes << endl;
     }
+}
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    solve();
     return 0;
 }
