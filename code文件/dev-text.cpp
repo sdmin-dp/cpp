3472a6de950ac17165988f3b44e03f06fe429c5a
yusiwen <3907363360@qq.com>
2026-03-07 11:58:04 +0000

    2026年3月7日 19:58

--- code文件/dev-text.cpp
+++ code文件/dev-text.cpp
@@ -3,47 +3,14 @@
 using namespace std;
 #define el '\n'
 const ll N=500+5;
-ll n;
-bool a[N][N];
-deque<ll> ans;
-void dfs(ll x){
-    for(int i=1;i<=n;i++){
-        if(a[x][i]){
-            a[x][i]=0;
-            a[i][x]=0;
-            dfs(i);
-        }
-    }
-    ans.push_front(x);
-}
+
 void solve(){
-    cin>>n;
-    for(int i=1;i<=n;i++){
-        ll x,y;
-        cin>>x>>y;
-        a[x][y]=1;
-        a[y][x]=1;
-    }
-    ll b=1;
-    ll cnt=0;
-    for(int i=1;i<=n;i++){
-        cnt=0;
-        for(int j=1;j<=n;j++) cnt+=a[i][j];
-        if(cnt%2){
-            b=i;
-            break;
-        }
-    }
-    dfs(b);
-    for(auto i:ans) cout<<i<<el;
+	cout<<1; 
 }
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
-    //freopen("xxx.in","r",stdin);
-    //freopen("xxx.out","w",stdout);
     ll T=1;
-    //cin>>T;
     while(T--){
         solve();
     }
