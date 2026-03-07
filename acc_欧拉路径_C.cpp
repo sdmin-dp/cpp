624a416668b57ab78cc5598d6b640258656f993d
yusiwen <3907363360@qq.com>
2026-03-07 11:47:45 +0000

    2026年3月7日 19:47

--- acc_欧拉路径_C.cpp
+++ acc_欧拉路径_C.cpp
@@ -40,10 +40,7 @@ void solve(){
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
-    //freopen("xxx.in","r",stdin);
-    //freopen("xxx.out","w",stdout);
     ll T=1;
-    //cin>>T;
     while(T--){
         solve();
     }
