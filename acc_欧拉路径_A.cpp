0d7dbb0d1e05d5f0f82f8b50d688c637b873c486
yusiwen <3907363360@qq.com>
2026-03-07 03:15:45 +0000

    2026年3月7日 11:15

--- acc_欧拉路径_A.cpp
+++ acc_欧拉路径_A.cpp
@@ -11,7 +11,11 @@ void solve(){
         ll x,y;
         cin>>x>>y;
         g[x].push_back(y);
-    }    
+        g[y].push_back(x);
+    }
+    ll cnt=0;
+    for(int i=1;i<=n;i++) if(g[i].size()%2==1) cnt++;
+    cout<<cnt;
 }
 int main(){
     ios::sync_with_stdio(0);
