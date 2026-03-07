aa5ffcb0ae6c9dd00f414bbb1e204f3b2365beb4
yusiwen <3907363360@qq.com>
2026-03-07 11:53:34 +0000

    2026年3月7日 19:53

--- acc_欧拉路径_C.cpp
+++ acc_欧拉路径_C.cpp
@@ -5,37 +5,37 @@ using namespace std;
 const ll N=500+5;
 ll n;
 bool a[N][N];
-// deque<ll> ans;
-// void dfs(ll x){
-//     for(int i=1;i<=n;i++){
-//         if(a[x][i]){
-//             a[x][i]=0;
-//             a[i][x]=0;
-//             dfs(i);
-//         }
-//     }
-//     ans.push_front(x);
-// }
+deque<ll> ans;
+void dfs(ll x){
+    for(int i=1;i<=n;i++){
+        if(a[x][i]){
+            a[x][i]=0;
+            a[i][x]=0;
+            dfs(i);
+        }
+    }
+    ans.push_front(x);
+}
 void solve(){
-    // cin>>n;
-    // for(int i=1;i<=n;i++){
-    //     ll x,y;
-    //     cin>>x>>y;
-    //     a[x][y]=1;
-    //     a[y][x]=1;
-    // }
-    // ll b=1;
-    // ll cnt=0;
-    // for(int i=1;i<=n;i++){
-    //     cnt=0;
-    //     for(int j=1;j<=n;j++) cnt+=a[i][j];
-    //     if(cnt%2){
-    //         b=i;
-    //         break;
-    //     }
-    // }
-    // dfs(b);
-    // for(auto i:ans) cout<<i<<el;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        ll x,y;
+        cin>>x>>y;
+        a[x][y]=1;
+        a[y][x]=1;
+    }
+    ll b=1;
+    ll cnt=0;
+    for(int i=1;i<=n;i++){
+        cnt=0;
+        for(int j=1;j<=n;j++) cnt+=a[i][j];
+        if(cnt%2){
+            b=i;
+            break;
+        }
+    }
+    dfs(b);
+    for(auto i:ans) cout<<i<<el;
 }
 int main(){
     ios::sync_with_stdio(0);
