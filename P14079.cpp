a080870e4a0c8acb2947805eb31b8a2a27a60fb1
yusiwen <3907363360@qq.com>
2026-03-08 10:43:01 +0000

    2026年3月8日 18:43

--- P14079.cpp
+++ P14079.cpp
@@ -1,10 +1,6 @@
 #include<bits/stdc++.h>
-#define ll long long
-using namespace std;
-#define el '\n'
-const ll N=1e5+5;
-ll n,a,b,x,y;
-void solve(){
+long long n,a,b,x,y;
+int main(){
     cin>>n>>a>>b;
     while(n--){
         cin>>x>>y;
@@ -12,17 +8,6 @@ void solve(){
         else if(x==1||y==1) cout<<a<<el;
         else if(__gcd(x,y)==1) cout<<min(a,2*b)<<el;
         else cout<<min(2*a,b)<<el;
-    }
-}
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);cout.tie(0);
-    //freopen("xxx.in","r",stdin);
-    //freopen("xxx.out","w",stdout);
-    ll T=1;
-    //cin>>T;
-    while(T--){
-        solve();
     }
     return 0;
 }
