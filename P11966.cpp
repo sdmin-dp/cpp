04276e8753948d196f9ba4b8b956fb874308df1d
yusiwen <3907363360@qq.com>
2026-03-08 10:58:39 +0000

    2026年3月8日 18:58

--- P11966.cpp
+++ P11966.cpp
@@ -5,8 +5,19 @@ using namespace std;
 const ll N=1e5+5;
 ll n,m,b,q;
 vector<pair<ll,ll>> g[N];
+ll dis[N];
+void dijkstra(){
+    queue<ll> q;
+    q.push(b);
+}
 void solve(){
-    
+    cin>>n>>m>>b>>q;
+    for(int i=1;i<=m;i++){
+        ll u,v,w;
+        cin>>u>>v>>w;
+        g[u].push_back({v,w});
+        g[v].push_back({u,w});
+    }
 }
 int main(){
     ios::sync_with_stdio(0);
