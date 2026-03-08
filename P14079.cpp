26fd67408f0ccbd1cf53f8084aa8b85f4a707181
yusiwen <3907363360@qq.com>
2026-03-08 10:44:54 +0000

    2026年3月8日 18:44

--- P14079.cpp
+++ P14079.cpp
@@ -1,13 +1,12 @@
 #include<bits/stdc++.h>
 long long n,a,b,x,y;
 int main(){
-    cin>>n>>a>>b;
+    std::cin>>n>>a>>b;
     while(n--){
-        cin>>x>>y;
-        if(x==y) cout<<0<<el;
-        else if(x==1||y==1) cout<<a<<el;
-        else if(__gcd(x,y)==1) cout<<min(a,2*b)<<el;
-        else cout<<min(2*a,b)<<el;
+        std::cin>>x>>y;
+        if(x==y) std::cout<<0<<'\n';
+        else if(x==1||y==1) std::cout<<a<<'\n';
+        else if(std::__gcd(x,y)==1) std::cout<<std::min(a,2*b)<<'\n';
+        else std::cout<<std::min(2*a,b)<<'\n';
     }
-    return 0;
 }
