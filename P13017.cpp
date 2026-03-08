9631e64403d10119b948e3e5c3a8d57e76ccb4a4
yusiwen <3907363360@qq.com>
2026-03-08 07:10:24 +0000

    2026年3月8日 15:10

--- P13017.cpp
+++ P13017.cpp
@@ -1,18 +1,32 @@
-
+#include<bits/stdc++.h>
+#define ll long long
 using namespace std;
-
-int n, m, a[100005];
-long long ans;
-int main() {
+#define el '\n'
+const ll N=1e5+5;
+ll n,m;
+ll a[N];
+ll ans;
+void solve(){
 	cin>>n>>m;
-	for (int i = 1; i <= m; i++) {
-		int u, v;
+	for(int i=1;i<=m;i++){
+		int u,v;
 		cin>>u>>v;
 		a[u]++;
 		a[v]++;
 	}
-	for (int i = 1; i <= n; i++)
-	    ans += (long long)a[i] * (a[i] - 1) / 2;
-	printf("%lld", ans);
-	return 0;
+	for (int i=1;i<=n;i++) ans+=a[i]*(a[i]-1)/2;
+	cout<<ans;
+
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    //freopen("xxx.in","r",stdin);
+    //freopen("xxx.out","w",stdout);
+    ll T=1;
+    //cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
 }
