8cfbd06ad2a0037a1bc2e5a556dc37faae2539d4
yusiwen <3907363360@qq.com>
2026-03-08 08:56:57 +0000

    2026年3月8日 16:56

---
+++ P14923.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+#define el '\n'
+const ll N=1e5+5;
+ll n,m;
+vector<pair<ll,ll>> g[N];
+ll dis[N];
+ll b,e;
+void dijkstra(){
+    priority_queue<pair<ll,ll>,vector<pair<ll,ll>>,greater<pair<ll,ll>>> q;//堆优化
+    q.push({0,b});
+    memset(dis,0x3f,sizeof(dis));
+    dis[1]=0;
+    //dis[i]数组存的是b到i的最短路
+    while(!q.empty()){
+        auto x=q.top();
+        q.pop();
+        if(x.first>dis[x.second]) continue;//一个小优化
+        for(auto i:g[x.second]){
+            if(dis[i.first]>x.second+i.second){//松弛
+                dis[i.first]=x.second+i.second;
+                q.push({i.first,dis[i.first]});
+            }
+        }
+    }
+}
+void solve(){
+    
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    //freopen("xxx.in","r",stdin);
+    //freopen("xxx.out","w",stdout);
+    ll T=1;
+    //cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
