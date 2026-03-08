448810fcd20857390a6f1effba33db861b9010af
yusiwen <3907363360@qq.com>
2026-03-08 09:05:03 +0000

    2026年3月8日 17:05

--- P14923.cpp
+++ P14923.cpp
@@ -13,13 +13,12 @@ void dijkstra(ll b,ll e){
     q.push({0,b});
     memset(dis,0x3f,sizeof(dis));
     dis[1]=0;
-    //dis[i]数组存的是b到i的最短路
     while(!q.empty()){
         auto x=q.top();
         q.pop();
-        if(x.first>dis[x.second]) continue;//一个小优化
+        if(x.first>dis[x.second]) continue;
         for(auto i:g[x.second]){
-            if(dis[i.first]>x.second+i.second){//松弛
+            if(dis[i.first]>x.second+i.second){
                 dis[i.first]=x.second+i.second;
                 q.push({i.first,dis[i.first]});
             }
@@ -36,6 +35,7 @@ void solve(){
         g[u].push_back({v,w});
         g[v].push_back({u,w});
     }
+    dijkstra(mouse);
 }
 int main(){
     ios::sync_with_stdio(0);
