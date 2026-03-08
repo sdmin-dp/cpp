9bcc66df7b93be6239da9c0365e5a93a9ba579f6
yusiwen <3907363360@qq.com>
2026-03-08 06:15:18 +0000

    2026年3月8日 14:15

--- P14921.cpp
+++ P14921.cpp
@@ -4,18 +4,37 @@ using namespace std;
 #define el '\n'
 const ll N=1e5+5;
 ll n,m;
-vector<ll> g[N];
-void SPFA(){
-    
+vector<pair<ll,ll>> g[N];
+ll dis[N];
+bool inq[N];
+ll b,e;
+ll SPFA(){
+    queue<ll> q;
+    q.push(b);
+    dis[b]=0;
+    inq[b]=1;
+    while(!q.empty()){
+        ll x=q.front();
+        inq[x]=0;
+        q.pop();
+        for(auto v:g[x]){
+            if(!inq[v.first]&&dis[v.first]>dis[x]+v.second){
+	            inq[v.first]=1;
+                dis[v.first]=dis[x]+v.second;
+                q.push(v.first);
+            }
+        }
+    }
+    return dis[e];
 }
 void solve(){
     cin>>n>>m;
     for(int i=1;i<=m;i++){
         ll x,y;cin>>x>>y;
-        g[x].push_back(y);
-        g[y].push_back(x);
+        g[x].push_back({y,1});
+        g[y].push_back({x,1});
     }
-
+    for()
 }
 int main(){
     ios::sync_with_stdio(0);
