b8b12a3f2e28243958d97573527557464013ebdb
yusiwen <3907363360@qq.com>
2026-03-10 11:25:27 +0000

    2026年3月10日 19:25

--- P13018.cpp
+++ P13018.cpp
@@ -6,13 +6,13 @@ const ll N=2e5+5;
 ll n;
 ll dp[N];
 pair<ll,ll> a[N];
-ll val(){
-    
-}
+ll val(ll x){return (x+50000);}
 void solve(){
     cin>>n;
     for(int i=1;i<=n;i++) cin>>a[i].first>>a[i].second;
-    
+    for(int i=1;i<=n;i++){
+        fi(a[i])
+    }
 }
 int main(){
     ios::sync_with_stdio(0);
