3fe2b07f55a000ee05d3e0c28e07a78f74b3df8c
yusiwen <3907363360@qq.com>
2026-03-10 11:22:52 +0000

    2026年3月10日 19:22

--- P13018.cpp
+++ P13018.cpp
@@ -3,9 +3,12 @@
 using namespace std;
 #define el '\n'
 const ll N=2e5+5;
+ll n;
 ll dp[N];
+pair<ll,ll> a[N];
 void solve(){
-    
+    cin>>n;
+    for(int i=1;i<=n;i++) cin>>a[i].first>>a[i].second;
 }
 int main(){
     ios::sync_with_stdio(0);
