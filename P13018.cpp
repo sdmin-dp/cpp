d757897a8abaf55b0b797738ffe097d9cf1b4a01
yusiwen <3907363360@qq.com>
2026-03-10 11:35:01 +0000

    2026年3月10日 19:35

--- P13018.cpp
+++ P13018.cpp
@@ -19,12 +19,14 @@ void solve(){
     for(int i=1;i<=n;i++){
         ll w=a[i].first-a[i].second;
         ll v=a[i].second+a[i].first;
-        // if(w>=0){
-        //     //如果是正数，那么倒着就是j-w<=j,就要倒着遍历
-        //     for(int i=m;i>=w;i--){
-        //         if(dp[j-w]!=)
-        //     } 
-        // }
+        if(w>=0){
+            //如果是正数，那么倒着就是j-w<=j,就要倒着遍历
+            for(int j=m;j>=w;j--){
+                if(dp[j-w]!=-4485090715960753727){
+                    dp[j]=
+                }
+            } 
+        }
     }
 }
 int main(){
