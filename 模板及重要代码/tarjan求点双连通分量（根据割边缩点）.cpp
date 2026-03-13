d59f6ef0aaa03667782c70221bff9e2fd24d1e41
yusiwen <3907363360@qq.com>
2026-03-13 11:28:10 +0000

    2026年3月13日 19:28

--- 模板及重要代码/tarjan求点双连通分量（根据割边缩点）.cpp
+++ 模板及重要代码/tarjan求点双连通分量（根据割边缩点）.cpp
@@ -51,12 +51,8 @@ void solve(){
             change(i);
         }
     }
-    /*
-    缩点n步走：
-    1.如果i和j是一个颜色的，就不用缩了;如果是不同的，则将color[i]到color[j]加一条边
-    2.没了
-    */
-    for(int i=1;i<=n;i++) for(auto j:g[i]) if(color[i]!=color[j]) ng[color[i]].push_back(color[j]);
+    for(auto i:bridge) ng[color[i.first]].push_back(color[i.second]);
+    
 }
 int main(){
     ios::sync_with_stdio(0);
