ff672cb18a00d920358736f64128b11a6689e4e8
yusiwen <3907363360@qq.com>
2026-03-14 12:39:15 +0000

    2026年3月14日 20:39

--- abc449c.cpp
+++ abc449c.cpp
@@ -7,14 +7,19 @@ ll n,l,r;
 string s;
 //l<=j-i<=r
 vector<ll> v[27];
+ll cnt=1;
 void solve(){
     cin>>n>>l>>r;
     cin>>s;
     for(int i=0;i<n;i++) v[s[i]-'a'+1].push_back(i);
     for(int i=1;i<=26;i++) sort(v[i].begin(),v[i].end(),greater<ll>());
     for(int i=0;i<n;i++){
-        if(j<=)
+        for(auto j:v[s[i]-'a'+1]){
+            if(j<i||j-i<l) break;
+            cnt++;
+        } 
     }
+    cout<<cnt;
 }
 int main(){
     ios::sync_with_stdio(0);
