64332a824ab556afb5411d2b8c4f00e7e13dc8ea
yusiwen <3907363360@qq.com>
2026-03-14 13:13:45 +0000

    2026年3月14日 21:13

--- abc449d.cpp
+++ abc449d.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 #define ll long long
 #define el '\n'
-ll l,r,D,U;
-ll get_evens(ll l,ll r){
+ll l,r,d,u;
+ll get(ll l,ll r){
     if(l>r)return 0;
     auto f=[](ll n){
         if(n<0)return 0ll;
@@ -14,16 +14,14 @@ ll get_evens(ll l,ll r){
     return f(-l)+f(r)-1;
 }
 void solve(){
-    if(!(cin>>l>>r>>D>>U))return;
+    cin>>l>>r>>d>>u;
     ll ans=0;
-    for(ll y=D;y<=U;y++){
+    for(ll y=d;y<=u;y++){
         ll ay=abs(y);
         ll mid_l=max(l,-ay),mid_r=min(r,ay);
-        if(mid_l<=mid_r){
-            if(ay%2==0)ans+=(mid_r-mid_l+1);
-        }
-        if(l<-ay)ans+=get_evens(l,min(r,-ay-1));
-        if(r>ay)ans+=get_evens(max(l,ay+1),r);
+        if(mid_l<=mid_r) if(ay%2==0)ans+=(mid_r-mid_l+1);
+        if(l<-ay)ans+=get(l,min(r,-ay-1));
+        if(r>ay)ans+=get(max(l,ay+1),r);
     }
     cout<<ans<<el;
 }
