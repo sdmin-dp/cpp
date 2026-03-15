b0b749e91d69273ce1bafd2113fc314f442ce154
yusiwen <3907363360@qq.com>
2026-03-15 08:00:17 +0000

    2026年3月15日 16:00

---
+++ text.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+#define el '\n'
+const ll N=1e5+5;
+const ll p=1e9;
+ll n;
+void solve(){
+    ll a=n/3,b=n%3;
+    ll ans;
+    if(b==1) ans=(pow(3,a-1)*4);
+    else ans=pow(3,a);
+    cout<<ans%p<<el;
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    //freopen("xxx.in","r",stdin);
+    //freopen("xxx.out","w",stdout);
+    ll T=1;
+    cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
