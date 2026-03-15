b8f7af79a3fa71fbb667a2696a92c493d3310b9a
yusiwen <3907363360@qq.com>
2026-03-15 11:21:22 +0000

    2026年3月15日 19:21

--- P1692.cpp
+++ P1692.cpp
@@ -5,15 +5,36 @@ using namespace std;
 const ll N=1e2+5;
 ll n,m;
 vector<ll> g[N];
+vector<bool> ans,vis;
+ll mx;
+bool check(ll x){
+    for(auto i:g[x]) if(vis[i]) return 0;
+    return 1;
+}
+void dfs(ll x,ll sum){
+    if(x==n+1){
+        if(sum>mx) ans=vis,mx=sum;
+        return;
+    }
+    if(sum+(n-x+1)<=mx) return;
+    if(!check(x)){
+        vis[x]=1;
+        dfs(x+1,sum+1);
+        vis[x]=0;
+    }
+    dfs(x+1,sum);
+}
 void solve(){
     cin>>n>>m;
+    ans.resize(n+1);
+    vis.resize(n+1);
     for(int i=1;i<=m;i++){
         ll u,v;
         cin>>u>>v;
         g[u].push_back(v);
         g[v].push_back(u);
     }
-    
+    for(int i=1;i<=n;i++) cout<<ans[i]<<" ";
 }
 int main(){
     ios::sync_with_stdio(0);
