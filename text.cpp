52ec94a0be32df789196a67991250294e5af516d
yusiwen <3907363360@qq.com>
2026-03-15 08:13:17 +0000

    2026年3月15日 16:13

--- text.cpp
+++ text.cpp
@@ -5,12 +5,21 @@ using namespace std;
 const ll N=1e5+5;
 const ll p=1e9;
 ll n;
+ll qpow(ll a,ll b){
+    ll res=1;
+    while(b){
+        if(b%2) res*=a;
+        b/=2;
+        a*=a;
+    }
+    return res;
+}
 void solve(){
     cin>>n;
     ll a=n/3,b=n%3;
     ll ans;
-    if(b==1) ans=(pow(3,a-1)*4);
-    else ans=pow(3,a);
+    if(b==1) ans=(qpow(3,a-1)*4);
+    else ans=qpow(3,a);
     cout<<ans<<el;
 }
 int main(){
