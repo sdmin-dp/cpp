0278d1052f47a082e60f8f7c29ac3831160f7ef8
yusiwen <3907363360@qq.com>
2026-03-15 12:02:09 +0000

    2026年3月15日 20:02

--- P2910.cpp
+++ P2910.cpp
@@ -3,10 +3,26 @@
 using namespace std;
 #define el '\n'
 const ll N=1e5+5;
-ll
+ll n,m;
 ll a[N];
+ll dis[N][N];
 void solve(){
-    
+    cin>>n>>m;
+    for(int i=1;i<=m;i++) cin>>a[i];
+    memset(dis,0x3f,sizeof(dis));
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            cin>>dis[i][j];
+        }
+    }
+    for(int k=1;k<=n;k++){
+        for(int i=1;i<=k;i++){
+            if(dis[i][k]==0x3f3f3f3f3f3f3f3f) continue;
+            for(int j=k;j<=n;j++){
+                if(dis[i][k]+dis[k][j])
+            }
+        }
+    }
 }
 int main(){
     ios::sync_with_stdio(0);
