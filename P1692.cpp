a62354b29db3185b2bcc6b81a912b1d7d74ce169
yusiwen <3907363360@qq.com>
2026-03-15 11:26:13 +0000

    2026年3月15日 19:26

--- P1692.cpp
+++ P1692.cpp
@@ -12,11 +12,12 @@ bool check(ll x){
     return 1;
 }
 void dfs(ll x,ll sum){
+    if(sum+(n-x+1)<=mx) return;
     if(x==n+1){
-        if(sum>mx) ans=vis,mx=sum;
+        /*if(sum>mx) */ans=vis,mx=sum;
         return;
     }
-    if(sum+(n-x+1)<=mx) return;
+    
     if(check(x)){
         vis[x]=1;
         dfs(x+1,sum+1);
