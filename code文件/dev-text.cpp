fdd2e3564b494a02ae7f47b29413305bdf04a350
yusiwen <3907363360@qq.com>
2026-03-16 13:55:05 +0000

    2026年3月16日 21:55

--- code文件/dev-text.cpp
+++ code文件/dev-text.cpp
@@ -2,15 +2,48 @@
 #define ll long long
 using namespace std;
 #define el '\n'
-const ll N=500+5;
-
+const ll N=1e5+5;
+ll c,p,s,n;
+ll a[4][N];
+ll ans;
+set<pair<string,pair<ll,ll>>> stt;
 void solve(){
-	cout<<1; 
+    cin>>c>>p>>s>>n;
+    ans=c*p*s;
+    for(int i=1;i<=n;i++){
+        string st;
+        ll x,y;
+        cin>>st;
+        cin>>x>>y;
+        stt.insert({st,{x,y}});
+    }
+    for(int i=1;i<=n;i++){
+        string st;
+        ll x,y;
+        cin>>st;
+        cin>>x>>y;
+        if(st=="CP"){
+            a[1][x]++;
+            a[2][y]++;
+            ans-=s;
+        }else{
+            a[2][x]++;
+            a[3][y]++;
+            ans-=c;
+        }
+    }
+    for(int i=1;i<=c;i++) if(a[1][i]!=0) ans+=a[1][i]-1;
+    for(int i=1;i<=p;i++) if(a[2][i]!=0) ans+=a[2][i]-1;
+    for(int i=1;i<=s;i++) if(a[3][i]!=0) ans+=a[3][i]-1;
+    cout<<ans;
 }
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
+    //freopen("xxx.in","r",stdin);
+    //freopen("xxx.out","w",stdout);
     ll T=1;
+    //cin>>T;
     while(T--){
         solve();
     }
