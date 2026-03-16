4e033d4cd8a2316a999ac1e6df22385c8842efbf
yusiwen <3907363360@qq.com>
2026-03-16 14:08:23 +0000

    2026年3月16日 22:08

--- text.cpp
+++ text.cpp
@@ -1,28 +1,50 @@
-#include<bits/stdc++.h>
-#define ll long long
+#include <iostream>
+#include <vector>
+#include <set>
+#include <string>
+
 using namespace std;
-#define el '\n'
-const ll N=1e6+5;
-const ll p=1e9;
-ll n;
-ll dp[N];
-void solve(){
-    cin>>n;
-    cout<<dp[n];
-}
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);cout.tie(0);
-    //freopen("xxx.in","r",stdin);
-    //freopen("xxx.out","w",stdout);
-    ll T=1;
-    cin>>T;
-    for(int i=1;i<=N;i++){
-        if(i<=4){dp[i]=i;continue;}
-        if(i%3==0) dp[i]=max(dp[i-3]*3,dp[i-4]*4,dp[i-2]*2);
+
+typedef long long ll;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    ll N, M, K, P;
+    if (!(cin >> N >> M >> K >> P)) return 0;
+
+    set<pair<int, int>> cp_set, ps_set;
+    vector<ll> countCP(M + 1, 0);
+    vector<ll> countPS(M + 1, 0);
+
+    for (int i = 0; i < P; ++i) {
+        string type;
+        int x, y;
+        cin >> type >> x >> y;
+        if (type == "CP") {
+            if (cp_set.find({x, y}) == cp_set.end()) {
+                cp_set.insert({x, y});
+                countCP[y]++;
+            }
+        } else {
+            if (ps_set.find({x, y}) == ps_set.end()) {
+                ps_set.insert({x, y});
+                countPS[x]++; // 这里的x是裤子编号，y是鞋子编号
+            }
+        }
     }
-    while(T--){
-        solve();
+
+    ll total = N * M * K;
+    ll minusCP = (ll)cp_set.size() * K;
+    ll minusPS = (ll)ps_set.size() * N;
+    
+    ll plusIntersection = 0;
+    for (int y = 1; y <= M; ++y) {
+        plusIntersection += countCP[y] * countPS[y];
     }
+
+    cout << total - minusCP - minusPS + plusIntersection << endl;
+
     return 0;
-}   
+}
