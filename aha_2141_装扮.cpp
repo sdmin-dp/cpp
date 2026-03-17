ac0e8e60b7588daf1ac4ca40dbc0a91800b8b30e
yusiwen <3907363360@qq.com>
2026-03-17 11:15:12 +0000

    2026年3月17日 19:15

--- aha_2141_装扮.cpp
+++ aha_2141_装扮.cpp
@@ -35,9 +35,9 @@ void solve(){
             ans-=c;
         }
     }
-    for(int i=1;i<=c;i++) if(a[1][i]!=0) ans+=a[1][i]-1;
+    // for(int i=1;i<=c;i++) if(a[1][i]!=0) ans+=a[1][i]-1;
     for(int i=1;i<=p;i++) if(a[2][i]!=0) ans+=a[2][i]-1;
-    for(int i=1;i<=s;i++) if(a[3][i]!=0) ans+=a[3][i]-1;
+    // for(int i=1;i<=s;i++) if(a[3][i]!=0) ans+=a[3][i]-1;
     cout<<ans;
 }
 int main(){
