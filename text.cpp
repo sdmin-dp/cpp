3bda01ed9ad5cdd4b84084aa60323c7690f3046f
yusiwen <3907363360@qq.com>
2026-03-17 09:41:39 +0000

    2026年3月17日 17:41

--- text.cpp
+++ text.cpp
@@ -1,50 +1,74 @@
-#include <iostream>
-#include <vector>
-#include <set>
-#include <string>
-
+#include<bits/stdc++.h>
 using namespace std;
-
-typedef long long ll;
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-
-    ll N, M, K, P;
-    if (!(cin >> N >> M >> K >> P)) return 0;
-
-    set<pair<int, int>> cp_set, ps_set;
-    vector<ll> countCP(M + 1, 0);
-    vector<ll> countPS(M + 1, 0);
-
-    for (int i = 0; i < P; ++i) {
-        string type;
-        int x, y;
-        cin >> type >> x >> y;
-        if (type == "CP") {
-            if (cp_set.find({x, y}) == cp_set.end()) {
-                cp_set.insert({x, y});
-                countCP[y]++;
+#define ll long long
+#define el '\n'
+const ll N=1005;
+ll m,idx,dfn[N],low[N],dcc_cnt;
+bool is_cut[N];
+vector<ll> g[N],dcc[N];
+stack<ll> stk;
+set<ll> st;
+void tarjan(ll x,ll root){
+    dfn[x]=low[x]=++idx;
+    stk.push(x);
+    if(x==root&&g[x].empty()){
+        dcc[++dcc_cnt].push_back(x);
+        return;
+    }
+    ll child=0;
+    for(auto i:g[x]){
+        if(!dfn[i]){
+            child++;
+            tarjan(i,root);
+            low[x]=min(low[x],low[i]);
+            if(low[i]>=dfn[x]){
+                if(x!=root||child>1)is_cut[x]=1;
+                dcc_cnt++;
+                ll y;
+                do{
+                    y=stk.top();stk.pop();
+                    dcc[dcc_cnt].push_back(y);
+                }while(y!=i);
+                dcc[dcc_cnt].push_back(x);
             }
-        } else {
-            if (ps_set.find({x, y}) == ps_set.end()) {
-                ps_set.insert({x, y});
-                countPS[x]++; // 这里的x是裤子编号，y是鞋子编号
+        }else low[x]=min(low[x],dfn[i]);
+    }
+}
+void solve(){
+    ll T=0;
+    while(cin>>m){
+        if(!m) return;
+        T++;
+        for(int i=0;i<N;i++){
+            g[i].clear();dcc[i].clear();
+            dfn[i]=low[i]=is_cut[i]=0;
+        }
+        st.clear();while(!stk.empty())stk.pop();
+        idx=dcc_cnt=0;
+        for(int i=1;i<=m;i++){
+            ll u,v;cin>>u>>v;
+            g[u].push_back(v);g[v].push_back(u);
+            st.insert(u);st.insert(v);
+        }
+        for(auto i:st)if(!dfn[i])tarjan(i,i);
+        unsigned long long res_num=0,res_way=1;
+        if(dcc_cnt==1){
+            res_num=2;
+            res_way=(unsigned long long)dcc[1].size()*(dcc[1].size()-1)/2;
+        }else{
+            for(int i=1;i<=dcc_cnt;i++){
+                ll cnt=0;
+                for(auto node:dcc[i])if(is_cut[node])cnt++;
+                if(cnt==1){
+                    res_num++;
+                    res_way*=(dcc[i].size()-1);
+                }
             }
         }
+        cout<<"Case "<<T<<": "<<res_num<<" "<<res_way<<el;
     }
-
-    ll total = N * M * K;
-    ll minusCP = (ll)cp_set.size() * K;
-    ll minusPS = (ll)ps_set.size() * N;
-    
-    ll plusIntersection = 0;
-    for (int y = 1; y <= M; ++y) {
-        plusIntersection += countCP[y] * countPS[y];
-    }
-
-    cout << total - minusCP - minusPS + plusIntersection << endl;
-
     return 0;
+}
+int mian(){
+    
 }
