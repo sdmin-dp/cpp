9054d6c7916e5a20a2389652c4e3d5ca0663be7c
yusiwen <3907363360@qq.com>
2026-03-19 13:16:21 +0000

    2026年3月19日 21:16

--- text.cpp
+++ text.cpp
@@ -1,21 +1,28 @@
-#include<bits/stdc++.h>
-#define ll long long
+#include <iostream>
 using namespace std;
-#define el '\n'
-const ll N=1e5+5;
-
-void solve(){
-    
-}
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);cout.tie(0);
-    //freopen("xxx.in","r",stdin);
-    //freopen("xxx.out","w",stdout);
-    ll T=1;
-    //cin>>T;
-    while(T--){
-        solve();
+ 
+constexpr int N = 2e5 + 5;
+ 
+int ls[N]; // 上一个颜色为 color 的客栈位置
+int sum[N]; // 记录每种颜色当前可选的左侧客栈数量
+int cc[N]; // 当前位置之前，每种色调出现的次数
+int lp_idx = -1; // 上一个价格小于等于 p 的客栈位置
+int main()
+{
+    int n, k, p;
+    cin >> n >> k >> p;
+    long long ans = 0;
+    for (int i = 1; i <= n; i++) {
+        int color, price;
+        cin >> color >> price;
+        if (price <= p)
+            lp_idx = i;
+        if (lp_idx >= ls[color])
+            sum[color] = cc[color];
+        ls[color] = i;
+        ans += sum[color];
+        cc[color]++;
     }
+    cout << ans << endl;
     return 0;
 }
