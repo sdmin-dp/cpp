6c156d3f8f8a8e3acaed728ceb75aa5813395886
yusiwen <3907363360@qq.com>
2026-03-19 11:52:42 +0000

    2026年3月19日 19:52

--- 模板及重要代码/ST表求RMQ.cpp
+++ 模板及重要代码/ST表求RMQ.cpp
@@ -14,16 +14,19 @@ ll lg2[N];
 void init(){
     //从i开始一个数，不就是a[i]吗？
     for(int i=1;i<=n;i++) st[i][0]=a[i];
-    //从2
+    //从i开始2^j个数，就是从i开始2^(j-1)个数和从i+2^(j-1)个数中的最大值取个max
     for(int j=1;j<=mxlog;j++){
         for(int i=1;i+(1<<j)-1<=n;i++){
             st[i][j]=max(st[i][j-1],st[i+(1<<(j-1))][j-1]); 
         }
     }
+    //i/2的幂次+1,i/2*2=i 
     for(int i=2;i<=n;i++) lg2[i]=lg2[i/2]+1;
 }
 ll query(ll l,ll r){
+    //两块长2^k的板子，可以覆盖l~r
     ll k=lg2[r-l+1];
+    //这两块板子的最大值取个max
     return max(st[l][k],st[r-(1<<k)+1][k]);
 }
 void solve(){
