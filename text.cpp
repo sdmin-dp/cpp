c56da89a87e3f3c3151c13c3404dc39fbe0495ee
yusiwen <3907363360@qq.com>
2026-03-19 12:10:03 +0000

    2026年3月19日 20:10

--- text.cpp
+++ text.cpp
@@ -3,25 +3,9 @@
 using namespace std;
 #define el '\n'
 const ll N=1e5+5;
-ll cnt;
-void dfs(string s){
-    if(s.size()==2){
-        ll r=0,g=0,b=0;
-        for(auto i:s){
-            if(i=='r') r++; 
-            else if(i=='g') g++;
-            else b++;
-        }
-        if(r<=1&&b<=1){cnt++;cout<<s<<el;}
-        return;
-    }
-    dfs(s+'r');
-    dfs(s+'g');
-    dfs(s+'b');
-}
+
 void solve(){
-    dfs("");
-    cout<<cnt;
+    
 }
 int main(){
     ios::sync_with_stdio(0);
