d8bb9ef41238d808735550f0725b4e9076014247
yusiwen <3907363360@qq.com>
2026-03-19 13:12:58 +0000

    2026年3月19日 21:12

--- acc_RMQ_E.cpp
+++ acc_RMQ_E.cpp
@@ -7,11 +7,18 @@ ll n,m,p;
 pair<ll,ll> a[N];
 ll color[55];
 ll mxprice[55];
+ll cnt;
 void solve(){
     cin>>n>>m>>p;
     for(int i=1;i<=n;i++) cin>>a[i].first>>a[i].second;
     for(int i=1;i<=n;i++){
-        if(a[i])
+        if(a[i].second<=p){
+            cnt+=color[a[i].first]++;
+        }
+        else{
+            ll k=mxprice[a[i].first];
+            cnt+=color[k]
+        }
     }
 }
 int main(){
