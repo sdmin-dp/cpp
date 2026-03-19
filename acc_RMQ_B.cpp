1b1703fc91f82d5534aeea96cfc561f973fd5ec0
yusiwen <3907363360@qq.com>
2026-03-19 12:07:55 +0000

    2026年3月19日 20:07

--- acc_RMQ_B.cpp
+++ acc_RMQ_B.cpp
@@ -9,7 +9,7 @@ ll a[N];
 pair<ll,ll> st[N][20];
 ll lg2[N];
 void init(){
-    for(int i=1;i<=n;i++) st[i][0]=a[i];
+    for(int i=1;i<=n;i++) st[i][0]={a[i],a[i]};
     for(int j=1;j<=mxlog;j++){
         for(int i=1;i+(1<<j)-1<=n;i++){
             st[i][j].first=max(st[i][j-1].first,st[i+(1<<(j-1))][j-1].first); 
@@ -18,9 +18,9 @@ void init(){
     }
     for(int i=2;i<=n;i++) lg2[i]=lg2[i/2]+1;
 }
-ll query(ll l,ll r){
+pair<ll,ll> query(ll l,ll r){
     ll k=lg2[r-l+1];
-    return max(st[l][k],st[r-(1<<k)+1][k]);
+    return {max(st[l][k].first,st[r-(1<<k)+1][k].first),min(st[l][k].second,st[r-(1<<k)+1][k].second)};
 }
 void solve(){
     cin>>n>>m;
@@ -29,7 +29,8 @@ void solve(){
     for(int i=1;i<=m;i++){
         ll x,y;
         cin>>x>>y;
-        cout<<query(x,y)<<el;
+        pair<ll,ll> p=query(x,y);
+        cout<<p.first<<" "<<p.second<<el;
     }
 }
 int main(){
