440f558ce9483d0daed5f99e2bed0aa848c0e99c
yusiwen <3907363360@qq.com>
2026-03-19 11:49:23 +0000

    2026年3月19日 19:49

--- 模板及重要代码/ST表求RMQ.cpp
+++ 模板及重要代码/ST表求RMQ.cpp
@@ -7,10 +7,14 @@ const ll N=1e5+5;
 const ll mxlog=17;
 ll n,m;
 ll a[N];
+//st表，从i开始2^j个数的最大值
 ll st[N][20];
+//预处理log2[i]
 ll lg2[N];
 void init(){
+    //从i开始一个数，不就是a[i]吗？
     for(int i=1;i<=n;i++) st[i][0]=a[i];
+    //从2
     for(int j=1;j<=mxlog;j++){
         for(int i=1;i+(1<<j)-1<=n;i++){
             st[i][j]=max(st[i][j-1],st[i+(1<<(j-1))][j-1]); 
