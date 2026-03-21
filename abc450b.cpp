22cdd17d45dda3b653e385e082da7eadea0cfdd0
yusiwen <3907363360@qq.com>
2026-03-21 12:26:57 +0000

    2026年3月21日 20:26

--- abc450b.cpp
+++ abc450b.cpp
@@ -8,16 +8,16 @@ ll c[N][N];
 void solve(){
     cin>>n;
     for(int i=1;i<=n;i++) for(int j=i;j<=n;i++) cin>>c[i][j];
-    for(int i=1;i<=n;i++){
-        for(int j=i+1;j<=n;j++){
-            for(int k=j+1;k<=n;k++){
-                if(c[i][j]+c[j][k]<c[i][k]){
-                    cout<<"Yes";
-                    return;
-                }
-            }
-        }
-    }
+    // for(int i=1;i<=n;i++){
+    //     for(int j=i+1;j<=n;j++){
+    //         for(int k=j+1;k<=n;k++){
+    //             if(c[i][j]+c[j][k]<c[i][k]){
+    //                 cout<<"Yes";
+    //                 return;
+    //             }
+    //         }
+    //     }
+    // }
     cout<<"No";
 }
 int main(){
