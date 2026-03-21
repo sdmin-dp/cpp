8f72b6fa1eeb418573cc9437dfe94ed8c878f1b8
yusiwen <3907363360@qq.com>
2026-03-21 11:26:26 +0000

    2026年3月21日 19:26

--- aha_101_星球旅行.cpp
+++ aha_101_星球旅行.cpp
@@ -25,11 +25,13 @@ void solve(){
 		for(int k=1;k<=n;k++){
 			for(int i=1;i<=n;i++){
 				for(int j=1;j<=n;j++){
+                    if(i==j||j==k||i==k) continue;
 					ans=min(ans,dis[i][j]+a[j][k]+a[k][i]);
 				}
 			}
 			for(int i=1;i<=n;i++){
 				for(int j=1;j<=n;j++){
+                    if(i==j||j==k||i==k) continue;
 					if(dis[i][k]+dis[k][j]<dis[i][j]){
 						dis[i][j]=dis[i][k]+dis[k][j];
 					}
