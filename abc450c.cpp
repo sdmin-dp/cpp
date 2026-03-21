41bb5656d22ff84f9385ed55f71ea3d4afc37b0a
yusiwen <3907363360@qq.com>
2026-03-21 12:36:14 +0000

    2026年3月21日 20:36

--- abc450c.cpp
+++ abc450c.cpp
@@ -12,7 +12,7 @@ void solve(){
     for(int i=1;i<=n;i++) for(int j=1;j<=n;j++) cin>>a[i][j];
     for(int i=1;i<=n;i++){
         for(int j=1;j<=n;j++){
-            if()
+            if(a[i][j]=='.')
         }
     }
 }
