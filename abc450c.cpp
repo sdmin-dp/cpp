9885598bc16e9f8c3a137c65ce14bf8ab12fa5d9
yusiwen <3907363360@qq.com>
2026-03-21 12:38:46 +0000

    2026年3月21日 20:38

--- abc450c.cpp
+++ abc450c.cpp
@@ -7,14 +7,28 @@ ll dx[]={0,1,0,-1},dy[]={1,0,-1,0};
 ll n,m;
 char a[N][N];
 bool vis[N][N];
+ll cnt;
+void dfs(ll x,ll y){
+    for(int i=0;i<4;i++){
+        ll xx=x+dx[i],yy=y+dy[i];
+        if(xx>=n||xx<=1||yy>=m||yy<=1||a[xx][yy]=='#'||vis[xx][yy]) continue;
+        vis[xx][yy]=1;
+        dfs(xx,yy);
+    }
+}
 void solve(){
     cin>>n>>m;
     for(int i=1;i<=n;i++) for(int j=1;j<=n;j++) cin>>a[i][j];
     for(int i=1;i<=n;i++){
         for(int j=1;j<=n;j++){
-            if(a[i][j]=='.')
+            if(a[i][j]=='.'&&!vis[i][j]){
+                cnt++;
+                vis[i][j]=1;
+                dfs(i,j);
+            }
         }
     }
+    cout<<cnt;
 }
 int main(){
     ios::sync_with_stdio(0);
