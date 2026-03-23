d29763bb7b41dd74f03b8f0b596c3becfbb01863
yusiwen <3907363360@qq.com>
2026-03-23 13:09:23 +0000

    2026年3月23日 21:09

--- acc_排列组合_A.cpp
+++ acc_排列组合_A.cpp
@@ -3,9 +3,28 @@
 using namespace std;
 #define el '\n'
 const ll N=1e5+5;
-
+const ll p=10007;
+ll a,b,k,n,m;
+ll qpow(ll a,ll b){
+    ll res=1;
+    while(b){
+        if(b%2) res*=a;
+        a*=a;
+        b/=2;
+    }
+    return res;
+}
+ll cnm(ll n,ll k){
+    ll res=1;
+    k=min(k,n-k);
+    for(int i=n;i>=n-k+1;i--) res*=i;
+    for(int i=k;i>=1;i--) res/=i;
+    return res;
+}
 void solve(){
-    
+    cin>>a>>b>>k>>n>>m;
+    ll r=k-n;
+    cout<<cnm(k,r)*qpow(a,k-r)*qpow(b,r)%p;
 }
 int main(){
     ios::sync_with_stdio(0);
