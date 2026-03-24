d4c315ec2b7919fab60b4378e985122b2e029d4e
yusiwen <3907363360@qq.com>
2026-03-24 11:45:31 +0000

    2026年3月24日 19:45

--- acc_欧拉路径_F.cpp
+++ acc_欧拉路径_F.cpp
@@ -58,8 +58,14 @@ void solve(){
     }
 }
 int main(){
-    ios::sync_with_stdio(0);
+    iios::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
-    solve();
+    //freopen("xxx.in","r",stdin);
+    //freopen("xxx.out","w",stdout);
+    ll T=1;
+    //cin>>T;
+    while(T--){
+        solve();
+    }
     return 0;
 }
