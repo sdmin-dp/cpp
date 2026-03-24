a60c7cb5ce0d09453235c437876831b06e68fc07
yusiwen <3907363360@qq.com>
2026-03-24 09:47:29 +0000

    2026年3月24日 17:47

--- acc_欧拉路径_C.cpp
+++ acc_欧拉路径_C.cpp
@@ -2,49 +2,18 @@
 #define ll long long
 using namespace std;
 #define el '\n'
-const ll N=1030+5;
-ll n;
-bool a[N][N];
-deque<ll> ans;
-void dfs(ll x){
-    for(int i=1;i<=n;i++){
-        if(a[x][i]){
-            a[x][i]=0;
-            a[i][x]=0;
-            dfs(i);
-        }
-    }
-    ans.push_front(x);
-}
+const ll N=1e5+5;
+
 void solve(){
-    cin>>n;
-    ll mn=1e9;
-    ll mx=-mn;
-    for(int i=1;i<=n;i++){
-        ll x,y;
-        cin>>x>>y;
-        a[x][y]=1;
-        a[y][x]=1;
-        mn=min({mn,x,y});
-        mx=max({mx,x,y});
-    }
-    ll b=1;
-    ll cnt=0;
-    for(int i=mn;i<=mx;i++){
-        cnt=0;
-        for(int j=1;j<=n;j++) cnt+=a[i][j];
-        if(cnt%2){
-            b=i;
-            break;
-        }
-    }
-    dfs(b);
-    for(auto i:ans) cout<<i<<el;
+    
 }
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
+    //freopen("xxx.in","r",stdin);
+    //freopen("xxx.out","w",stdout);
     ll T=1;
+    //cin>>T;
     while(T--){
         solve();
     }
