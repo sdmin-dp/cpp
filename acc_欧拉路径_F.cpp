2cd61d11e2da9d58b1b7df307c22d555fd9e27f5
yusiwen <3907363360@qq.com>
2026-03-24 10:45:12 +0000

    2026年3月24日 18:45

--- acc_欧拉路径_F.cpp
+++ acc_欧拉路径_F.cpp
@@ -3,9 +3,23 @@
 using namespace std;
 #define el '\n'
 const ll N=1e5+5;
-
+ll cnt=0;
+vector<ll> v;
 void solve(){
-    
+    while(1){
+        ll x,y,w;
+        cin>>x>>y;
+        if(cnt==1&&x!=0&&y!=0){
+            //初始化
+        }
+        if(x==0&&y==0){
+            cnt++;
+            if(cnt==2) break;
+            //开始计算
+        }
+        cin>>w;
+        
+    }
 }
 int main(){
     ios::sync_with_stdio(0);
