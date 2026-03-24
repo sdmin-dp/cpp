bf0c4509efefc54b9b8d2bd473ed8388211f7e8d
yusiwen <3907363360@qq.com>
2026-03-24 11:51:19 +0000

    2026年3月24日 19:51

--- acc_欧拉路径_F.cpp
+++ acc_欧拉路径_F.cpp
@@ -4,78 +4,56 @@ using namespace std;
 #define el '\n'
 const ll N=2e3+5;
 ll M;
-ll a[N][N];
+ll n,a[50][50],deg[50];
 map<pair<ll,ll>,set<ll>> id;
-ll n;
-deque<ll> ans;
+vector<ll> ans;
 bool vis[N];
 void dfs(ll x){
     for(int i=1;i<=n;i++){
         if(a[x][i]){
-            a[x][i]--;
-            a[i][x]--;
-            dfs(i);
+            for(auto it=id[{x,i}].begin();it!=id[{x,i}].end();){
+                ll w=*it;
+                if(!vis[w]){
+                    vis[w]=1;
+                    a[x][i]--;a[i][x]--;
+                    id[{x,i}].erase(it);id[{i,x}].erase(w);
+                    dfs(i);
+                    ans.push_back(w);
+                    it=id[{x,i}].begin();
+                }else it++;
+            }
         }
     }
-    ans.push_front(x);
 }
 void solve(){
+    ll x,y,w,st=0;
     while(1){
-        ll x,y,w;
         cin>>x>>y;
         if(M==1&&x!=0&&y!=0){
-            //初始化
-            M=0;
-            n=0;
-            id.clear();
+            M=0;n=0;st=0;
+            id.clear();ans.clear();
             memset(a,0,sizeof(a));
-            ans.clear();
+            memset(deg,0,sizeof(deg));
+            memset(vis,0,sizeof(vis));
         }
         if(x==0&&y==0){
-            M++;
-            if(M==2) break;
-            //开始计算
-            ll b=1;
-            ll cnt=0;
+            M++;if(M==2)break;
             ll cntou=0;
-            for(int i=1;i<=n;i++){
-                cnt=0;
-                for(int j=1;j<=n;j++) cnt+=a[i][j];
-                if(cnt%2){
-                    b=i;
-                    cntou++;
-                }
-            }
+            for(int i=1;i<=n;i++)if(deg[i]%2)cntou++;
             if(cntou!=0){
                 cout<<"Round trip does not exist.\n";
                 continue;
             }
-            dfs(b);
-            for(auto i:ans) cerr<<i<<" ";
-            cerr<<el<<el;
-            for(int i=1;i<ans.size();i++){
-                ll idx;
-                pair<ll,ll> p={ans[i-1],ans[i]};
-                for(auto i:id[p]){
-                    cerr<<i<<" ";
-                    if(!vis[i]){
-                        idx=i;
-                        vis[i]=1;
-                        break;
-                    }
-                }
-                cout<<idx<<" ";
-                cerr<<el;
-            }
+            dfs(st);
+            for(int i=ans.size()-1;i>=0;i--) cout<<ans[i]<<(i==0?"":" ");
             cout<<el;
-        }
-        else{
-            n++;
+        }else{
             cin>>w;
-            a[x][y]++;
-            a[y][x]++;
-            id[{x,y}].insert(w);
-            id[{y,x}].insert(w);
+            if(!st)st=min(x,y);
+            n=max({n,x,y});
+            a[x][y]++;a[y][x]++;
+            deg[x]++;deg[y]++;
+            id[{x,y}].insert(w);id[{y,x}].insert(w);
         }
     }
 }
