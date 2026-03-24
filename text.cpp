70151527ecf313ef6b92e12b0a4d3068b4a3357b
yusiwen <3907363360@qq.com>
2026-03-24 11:50:55 +0000

    2026年3月24日 19:50

--- text.cpp
+++ text.cpp
@@ -2,16 +2,59 @@
 #define ll long long
 using namespace std;
 #define el '\n'
-const ll N=1e5+5;
-set<ll> st;
+const ll N=2e3+5;
+ll M;
+ll n,a[50][50],deg[50];
+map<pair<ll,ll>,set<ll>> id;
+vector<ll> ans;
+bool vis[N];
+void dfs(ll x){
+    for(int i=1;i<=n;i++){
+        if(a[x][i]){
+            for(auto it=id[{x,i}].begin();it!=id[{x,i}].end();){
+                ll w=*it;
+                if(!vis[w]){
+                    vis[w]=1;
+                    a[x][i]--;a[i][x]--;
+                    id[{x,i}].erase(it);id[{i,x}].erase(w);
+                    dfs(i);
+                    ans.push_back(w);
+                    it=id[{x,i}].begin();
+                }else it++;
+            }
+        }
+    }
+}
 void solve(){
-    st.insert(5);
-    st.insert(4);
-    st.insert(3);
-    st.insert(2);
-    st.insert(1);
-    for(auto i:st){
-        cout<<i<<" ";
+    ll x,y,w,st=0;
+    while(1){
+        cin>>x>>y;
+        if(M==1&&x!=0&&y!=0){
+            M=0;n=0;st=0;
+            id.clear();ans.clear();
+            memset(a,0,sizeof(a));
+            memset(deg,0,sizeof(deg));
+            memset(vis,0,sizeof(vis));
+        }
+        if(x==0&&y==0){
+            M++;if(M==2)break;
+            ll cntou=0;
+            for(int i=1;i<=n;i++)if(deg[i]%2)cntou++;
+            if(cntou!=0){
+                cout<<"Round trip does not exist.\n";
+                continue;
+            }
+            dfs(st);
+            for(int i=ans.size()-1;i>=0;i--) cout<<ans[i]<<(i==0?"":" ");
+            cout<<el;
+        }else{
+            cin>>w;
+            if(!st)st=min(x,y);
+            n=max({n,x,y});
+            a[x][y]++;a[y][x]++;
+            deg[x]++;deg[y]++;
+            id[{x,y}].insert(w);id[{y,x}].insert(w);
+        }
     }
 }
 int main(){
