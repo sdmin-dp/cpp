4c9e1bfb785eea748ca9869b47357d93a95af709
yusiwen <3907363360@qq.com>
2026-03-24 10:00:42 +0000

    2026年3月24日 18:00

--- text.cpp
+++ text.cpp
@@ -10,7 +10,7 @@ void solve(){
     if(n==0) n=15;
     k=rand()%n;
     for(int i=1;i<=n;i++) a[i]=rand()%10;
-    cout<<n<<k<<el;
+    cout<<n<<" "<<k<<el;
     for(int i=1;i<=n;i++) cout<<a[i];
 }
 int main(){
