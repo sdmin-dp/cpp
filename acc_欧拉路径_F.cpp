517757944a6b28b11fc31c2128f0e296f4d51b9f
yusiwen <3907363360@qq.com>
2026-03-24 10:56:10 +0000

    2026年3月24日 18:56

--- acc_欧拉路径_F.cpp
+++ acc_欧拉路径_F.cpp
@@ -7,6 +7,7 @@ ll cnt=0;
 ll a[N][N];
 map<pair<ll,ll>,ll> id;
 ll n;
+deque<ll> ans;
 void solve(){
     while(1){
         ll x,y,w;
@@ -19,21 +20,19 @@ void solve(){
         }
         if(x==0&&y==0){
             cnt++;
-            if(cnt==2
-    ll b=1;
-    ll cnt=0;
-    for(int i=1;i<=n;i++){
-        cnt=0;
-        for(int j=1;j<=n;j++) cnt+=a[i][j];
-        if(cnt%2){
-            b=i;
-            break;
-        }
-    }
-    dfs(b);
-    for(auto i:ans) cout<<i<<el;) break;
+            if(cnt==2) break;
             //开始计算
-
+            ll b=1;
+            ll cnt=0;
+            for(int i=1;i<=n;i++){
+                cnt=0;
+                for(int j=1;j<=n;j++) cnt+=a[i][j];
+                if(cnt%2){
+                    b=i;
+                    break;
+                }
+            }
+            dfs(b);
         }
         n++;
         cin>>w;
