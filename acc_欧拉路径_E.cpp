85a874ce68ef715b699725af1eaf1cb5469d64a4
yusiwen <3907363360@qq.com>
2026-03-26 13:00:57 +0000

    2026年3月26日 21:00

--- acc_欧拉路径_E.cpp
+++ acc_欧拉路径_E.cpp
@@ -2,60 +2,73 @@
 #define ll long long
 using namespace std;
 #define el '\n'
-const ll N=1e5+5;
+const ll N=30;
 ll n;
-bool vis[N];
-vector<ll> g[N];
-unordered_map<char,vector<ll>> mp;
-string s[N];
+bool vis,exists;
+vector<ll> g;
+ll in,out;
 void dfs(ll x){
+    vis[x]=1;
     for(auto i:g[x]){
-        if(!vis[i]){
-            vis[i]=1;
-            dfs(i);
-        }
+        if(!vis[i]) dfs(i);
     }
 }
 void solve(){
     cin>>n;
-    for(int i=1;i<=n;i++){
-        cin>>s[i];
-        mp[s[i][0]].push_back(i);
+    for(int i=0;i<26;i++){
+        g[i].clear();
+        vis[i]=0;
+        exists[i]=0;
+        in[i]=0;
+        out[i]=0;
     }
     for(int i=1;i<=n;i++){
-        char c=s[i][s[i].size()-1];
-        for(auto j:mp[c]){
-            ll k=j;
-            g[i].push_back(k);
-            g[k].push_back(i);
+        string s;
+        cin>>s;
+        int u=s[0]-'a';
+        int v=s[s.size()-1]-'a';
+        g[u].push_back(v);
+        g[v].push_back(u);
+        out[u]++;
+        in[v]++;
+        exists[u]=exists[v]=1;
+    }
+    int st=-1;
+    for(int i=0;i<26;i++){
+        if(exists[i]){
+            st=i;
+            break;
         }
     }
-    // for(int i=1;i<=n;i++){
-    //     cerr<<i<<":";
-    //     for(auto j:g[i]){
-    //         cerr<<j<<" ";
-    //     }
-    //     cerr<<el;
-    // }
-    dfs(1);
-    bool flag=1;
-    for(int i=1;i<=n;i++) if(!vis[i]) flag=0;
-    if(flag) cout<<"Ordering is possible.\n";
-    else cout<<"The door cannot be opened.\n";
+    if(st!=-1) dfs(st);
+    for(int i=0;i<26;i++){
+        if(exists[i]&&!vis[i]){
+            cout<<"The door cannot be opened."<<el;
+            return;
+        }
+    }
+    int s_cnt=0,e_cnt=0;
+    bool ok=1;
+    for(int i=0;i<26;i++){
+        if(!exists[i]) continue;
+        if(in[i]==out[i]) continue;
+        if(out[i]-in[i]==1) s_cnt++;
+        else if(in[i]-out[i]==1) e_cnt++;
+        else ok=0;
+    }
+    if(ok&&((s_cnt==0&&e_cnt==0)||(s_cnt==1&&e_cnt==1))){
+        cout<<"Ordering is possible."<<el;
+    }else{
+        cout<<"The door cannot be opened."<<el;
+    }
 }
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
-    //freopen("xxx.in","r",stdin);
-    //freopen("xxx.out","w",stdout);
-    ll T=1;
+    ll T;
     cin>>T;
     while(T--){
-        mp.clear();
-        for(int i=1;i<=n;i++) g[i].clear();
-        memset(vis,0,sizeof(vis));
         solve();
-        // cerr<<el<<el;
     }
     return 0;
 }
