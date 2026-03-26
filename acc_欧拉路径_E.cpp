37a6ad70061f3d2f970ee73c233eb56df56e3fca
yusiwen <3907363360@qq.com>
2026-03-26 12:38:54 +0000

    2026年3月26日 20:38

--- acc_欧拉路径_E.cpp
+++ acc_欧拉路径_E.cpp
@@ -4,19 +4,25 @@ using namespace std;
 #define el '\n'
 const ll N=1e5+5;
 ll n;
-string s;
 bool vis[N];
 vector<ll> g[N];
 map<char,vector<string>> mp;
 map<string,ll> id;
+string s[N];
 void solve(){
     cin>>n;
     for(int i=1;i<=n;i++){
-        cin>>s;
-        mp[s[0]].push_back(s);
-        id[s]=i;
+        cin>>s[i];
+        mp[s[i][0]].push_back(s[i]);
+        id[s[i]]=i;
     }
-    
+    for(int i=1;i<=n;i++){
+        char c=s[i][s[i].size()-1];
+        for(auto j:mp[c]){
+            ll k=id[j];
+            
+        }
+    }   
 }
 int main(){
     ios::sync_with_stdio(0);
