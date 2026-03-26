197dfd0d21ff75b83e4bf79f324e1023ce498001
yusiwen <3907363360@qq.com>
2026-03-26 10:29:36 +0000

    2026年3月26日 18:29

--- acc_欧拉路径_F.cpp
+++ acc_欧拉路径_F.cpp
@@ -32,7 +32,7 @@ void solve(){
         }
         if(x==0&&y==0){
             M++;
-            for(int i=1;i<=n;i++) sort(g[i].begin(),g[i].end());
+            for(int i=1;i<=min(n,50ll);i++) sort(g[i].begin(),g[i].end());
             if(M==2) break;
             ll b=1;
             ll cnt=0;
@@ -47,8 +47,8 @@ void solve(){
                 cout<<"Round trip does not exist.\n";
                 continue;
             }
-            // dfs(b);
-            cerr<<el<<el;
+            dfs(b);
+            // cerr<<el<<el;
             for(int i=1;i<ans.size();i++){
                 ll idx;
                 pair<ll,ll> p={ans[i-1],ans[i]};
@@ -67,7 +67,7 @@ void solve(){
             g[y].push_back({w,x});
             if(x>y) swap(x,y);
             id[{x,y}].insert(w);
-            // id[{y,x}].insert(w);
+            id[{y,x}].insert(w);
         }
     }
 }
