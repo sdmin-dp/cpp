e62ef12761d4cbd46277cc00cbe250c1a0b68f6e
yusiwen <3907363360@qq.com>
2026-03-26 13:03:10 +0000

    2026年3月26日 21:03

--- acc_欧拉路径_E.cpp
+++ acc_欧拉路径_E.cpp
@@ -2,48 +2,35 @@
 #define ll long long
 using namespace std;
 #define el '\n'
-const ll N=30;
-ll n,in,ot;
-bool ex;
-int p;
+int p[30],in[30],ot[30];
+bool ex[30];
 int find(int x){return p[x]==x?x:p[x]=find(p[x]);}
 void solve(){
-    cin>>n;
-    for(int i=0;i<26;i++){
-        p[i]=i;
-        in[i]=ot[i]=ex[i]=0;
-    }
+    int n;cin>>n;
+    for(int i=0;i<26;i++){p[i]=i;in[i]=ot[i]=ex[i]=0;}
     for(int i=1;i<=n;i++){
-        string s;
-        cin>>s;
+        string s;cin>>s;
         int u=s[0]-'a',v=s[s.size()-1]-'a';
-        ot[u]++;in[v]++;
-        ex[u]=ex[v]=1;
+        ot[u]++;in[v]++;ex[u]=ex[v]=1;
         int fu=find(u),fv=find(v);
-        if(fu!=fv) p[fu]=fv;
+        if(fu!=fv)p[fu]=fv;
     }
-    int cnt=0;
-    for(int i=0;i<26;i++) if(ex[i]&&p[i]==i) cnt++;
-    if(cnt>1){
-        cout<<"The door cannot be opened."<<el;
-        return;
-    }
-    int s1=0,s2=0;
-    bool ok=1;
+    int c1=0,c2=0,rt=0;
     for(int i=0;i<26;i++){
-        if(!ex[i]) continue;
-        if(in[i]==ot[i]) continue;
-        if(ot[i]-in[i]==1) s1++;
-        else if(in[i]-ot[i]==1) s2++;
-        else ok=0;
+        if(!ex[i])continue;
+        if(p[i]==i)rt++;
+        if(in[i]==ot[i])continue;
+        if(ot[i]-in[i]==1)c1++;
+        else if(in[i]-ot[i]==1)c2++;
+        else{cout<<"The door cannot be opened."<<el;return;}
     }
-    if(ok&&((s1==0&&s2==0)||(s1==1&&s2==1))) cout<<"Ordering is possible."<<el;
+    if(rt==1&&((c1==0&&c2==0)||(c1==1&&c2==1)))cout<<"Ordering is possible."<<el;
     else cout<<"The door cannot be opened."<<el;
 }
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
     int T;cin>>T;
-    while(T--) solve();
+    while(T--)solve();
     return 0;
 }
