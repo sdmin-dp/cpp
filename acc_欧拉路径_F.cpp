31b580400f1d8d6b8c4e43b2ea3b5a7730bf8042
yusiwen <3907363360@qq.com>
2026-03-26 10:23:33 +0000

    2026年3月26日 18:23

--- acc_欧拉路径_F.cpp
+++ acc_欧拉路径_F.cpp
@@ -20,56 +20,56 @@ void dfs(ll x){
     ans.push_front(x);
 }
 void solve(){
-    while(1){
-        ll x,y,w;
-        cin>>x>>y;
-        if(M==1&&x!=0&&y!=0){
-            M=0;
-            id.clear();
-            for(int i=1;i<=50;i++) g[i].clear();
-            ans.clear();
-            n=0;
-        }
-        if(x==0&&y==0){
-            M++;
-            for(int i=1;i<=n;i++) sort(g[i].begin(),g[i].end());
-            if(M==2) break;
-            ll b=1;
-            ll cnt=0;
-            ll cntou=0;
-            for(ll i=1;i<=n;i++){
-                cnt=g[i].size();
-                if(cnt%2){
-                    cntou++;
-                }
-            }
-            if(cntou!=0){
-                cout<<"Round trip does not exist.\n";
-                continue;
-            }
-            // dfs(b);
-            // cerr<<el<<el;
-            for(int i=1;i<ans.size();i++){
-                ll idx;
-                pair<ll,ll> p={ans[i-1],ans[i]};
-                for(auto i:id[p]){
-                        idx=i;
-                        break;
-                }
-                cout<<idx<<" ";
-            }
-            cout<<el;
-        }
-        else{
-            n++;
-            cin>>w;
-            g[x].push_back({w,y});
-            g[y].push_back({w,x});
-            if(x>y) swap(x,y);
-            id[{x,y}].insert(w);
-            // id[{y,x}].insert(w);
-        }
-    }
+    // while(1){
+    //     ll x,y,w;
+    //     cin>>x>>y;
+    //     if(M==1&&x!=0&&y!=0){
+    //         M=0;
+    //         id.clear();
+    //         for(int i=1;i<=50;i++) g[i].clear();
+    //         ans.clear();
+    //         n=0;
+    //     }
+    //     if(x==0&&y==0){
+    //         M++;
+    //         for(int i=1;i<=n;i++) sort(g[i].begin(),g[i].end());
+    //         if(M==2) break;
+    //         ll b=1;
+    //         ll cnt=0;
+    //         ll cntou=0;
+    //         for(ll i=1;i<=n;i++){
+    //             cnt=g[i].size();
+    //             if(cnt%2){
+    //                 cntou++;
+    //             }
+    //         }
+    //         if(cntou!=0){
+    //             cout<<"Round trip does not exist.\n";
+    //             continue;
+    //         }
+    //         dfs(b);
+    //         // cerr<<el<<el;
+    //         for(int i=1;i<ans.size();i++){
+    //             ll idx;
+    //             pair<ll,ll> p={ans[i-1],ans[i]};
+    //             for(auto i:id[p]){
+    //                     idx=i;
+    //                     break;
+    //             }
+    //             cout<<idx<<" ";
+    //         }
+    //         cout<<el;
+    //     }
+    //     else{
+    //         n++;
+    //         cin>>w;
+    //         g[x].push_back({w,y});
+    //         g[y].push_back({w,x});
+    //         if(x>y) swap(x,y);
+    //         id[{x,y}].insert(w);
+    //         // id[{y,x}].insert(w);
+    //     }
+    // }
 }
 int main(){
     ios::sync_with_stdio(0);
