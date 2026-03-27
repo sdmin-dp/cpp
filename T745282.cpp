97d973b915ebc28230597134ed836ec1e688ea9e
yusiwen <3907363360@qq.com>
2026-03-27 11:28:06 +0000

    2026年3月27日 19:28

---
+++ T745282.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+#define el '\n'
+const ll N=1e5+5;
+ll n,m;
+/*
+第奇数行第奇数列的字符为 +。
+第奇数行第偶数列的字符为 -。
+第偶数行第奇数列的字符为 |。
+第偶数行第偶数列的字符为空格。
+*/
+void solve(){
+    
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    //freopen("xxx.in","r",stdin);
+    //freopen("xxx.out","w",stdout);
+    ll T=1;
+    //cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
