4fc88c05a83b84d95a027f75906cf78fe0a0a599
yusiwen <3907363360@qq.com>
2026-03-27 11:46:51 +0000

    2026年3月27日 19:46

--- T745283.cpp
+++ T745283.cpp
@@ -22,7 +22,8 @@ void solve(){
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
-    //
+    //freopen("xxx.in","r",stdin);
+    //freopen("xxx.out","w",stdout);
     int T=1;
     while(T--){
         solve();
