99da3b481d8732cc4497a8e1bbde82bf3af97732
yusiwen <3907363360@qq.com>
2026-03-27 11:14:40 +0000

    2026年3月27日 19:14

--- T745280.cpp
+++ T745280.cpp
@@ -5,10 +5,18 @@ using namespace std;
 const ll N=1e5+5;
 ll n;
 ll a[N];
+pair<ll,ll> b[N];
 void solve(){
     cin>>n;
-    for(int i=1;i<=n;i++){
+    for(int i=1;i<=2*n;i++){
         cin>>a[i];
+        if(b[a[i]].first) b[a[i]].second=i;
+        else b[a[i]].first=i;
+    }
+    for(int i=1;i<=2*n;i++){
+        if(b[a[i]].first==i){
+            cout<<b[a[i]].second<<" ";
+        }
     }
 }
 int main(){
