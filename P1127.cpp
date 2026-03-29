85e8df7c61b95f029b5d813dfcf8ce5254312189
yusiwen <3907363360@qq.com>
2026-03-29 09:29:56 +0000

    2026年3月29日 17:29

--- P1127.cpp
+++ P1127.cpp
@@ -2,58 +2,11 @@
 #define ll long long
 using namespace std;
 #define el '\n'
-const ll N=1e4+5;
-ll num(char c){return c-'a'+1;}
+const ll N=1e3+5;
 ll n;
-string s[N];
-vector<pair<string,ll>> g[35];
-ll in[N],out[N];
-ll idx[N];
-ll start;
-deque<string> ans;
-void dfs(ll x){
-    while(idx[x]<g[x].size()){
-        string v=g[x][idx[x]].first;
-        ll u=g[x][idx[x]].second;
-        idx[x]++;
-        dfs(u);
-        ans.push_front(v);
-    }
-}
+vector<pair<ll,ll>> g[N];
 void solve(){
-    cin>>n;
-    for(int i=1;i<=n;i++){
-        cin>>s[i];
-        g[num(s[i][0])].push_back({s[i],num(s[i][s[i].size()-1])});
-        in[num(s[i][s[i].size()-1])]++;
-        out[num(s[i][0])]++;
-    }
-    ll cnt1=0,cnt2=0,cnt3=0,id=0,id2=1e9+7;
-    for(int i=1;i<=26;i++){
-        if(g[i].size()==0) continue;
-        if(g[i].size()>=1&&i<id2) id2=i;
-        if(in[i]-out[i]==1) cnt1++;
-        else if(out[i]-in[i]==1){
-            cnt2++;
-            id=i;
-        }
-        else if(out[i]==in[i]) cnt3++;
-    }
-    // cerr<<cnt1<<" "<<cnt2<<" "<<cnt3<<" "<<id<<" "<<id2;
-    if(cnt1==1&&cnt2==1) start=id;
-    else if(cnt1==0&&cnt2==0) start=id2;
-    else{
-        cout<<"***";
-        return;
-    }
-    for(int i=1;i<=n;i++){
-        sort(g[i].begin(),g[i].end());
-    }
-    dfs(start);
-    cout<<ans[0];
-    for(ll i=1;i<ans.size();i++){
-        cout<<"."<<ans[i];
-    }
+    
 }
 int main(){
     ios::sync_with_stdio(0);
