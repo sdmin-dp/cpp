fc322c7302acb43c0161dc08dbeb22255e41d2d1
yusiwen <3907363360@qq.com>
2026-03-29 06:42:13 +0000

    2026年3月29日 14:42

--- text.cpp
+++ text.cpp
@@ -1,94 +1,81 @@
 #include<bits/stdc++.h>
-#define ll long long
+#include<windows.h>
 using namespace std;
-#define el '\n'
-const ll N=2e3+5;
-ll M;
-bool vis[N];
-vector<pair<ll,ll>> g[55];
-map<pair<ll,ll>,set<ll>> id;
-ll n;
-deque<ll> ans;
-bool vs[N];
-void dfs(ll x){
-    for(auto i:g[x]){
-        if(!vis[i.first]){
-            vis[i.first]=1;
-            dfs(i.second);
-        }
-    }
-    ans.push_front(x);
+typedef unsigned long long ull;
+#define N 100005
+#define For(i,x,y)for(i=x;i<=(y);i++)
+bool bo[N];
+queue<int>p,q;
+ull R=GetTickCount();
+int deg[N][2],dep[N],fa[N];
+inline int Rand(int _,int __)
+{
+	R^=R<<13;
+	R^=R>>7;
+	R^=R<<17;
+	return R%(__-_+1)+_;
 }
-void solve(){
-    while(1){
-        ll x,y,w;
-        cin>>x>>y;
-        if(M==1&&x!=0&&y!=0){
-            //初始化
-            M=0;
-             
-            id.clear();
-            for(int i=1;i<=50;i++) g[i].clear();
-            ans.clear();
-            n=0;
-        }
-        if(x==0&&y==0){
-            M++;
-            for(int i=1;i<=min(n,50ll);i++) sort(g[i].begin(),g[i].end());
-            if(M==2) break;
-            //开始计算
-            ll b=1;
-            ll cnt=0;
-            ll cntou=0;
-            for(ll i=1;i<=n;i++){
-                cnt=g[i].size();
-                if(cnt%2){
-                    b=i;
-                    cntou++;
-                }
-            }
-            if(cntou!=0){
-                cout<<"Round trip does not exist.\n";
-                continue;
-            }
-            dfs(b);
-            // for(auto i:ans) cerr<<i<<" ";
-            // cerr<<el<<el;
-            for(int i=1;i<ans.size();i++){
-                ll idx;
-                pair<ll,ll> p={ans[i-1],ans[i]};
-                for(auto i:id[p]){
-                    // cerr<<i<<" ";
-                    if(!vs[i]){
-                        idx=i;
-                        vs[i]=1;
-                        break;
-                    }
-                }
-                cout<<idx<<" ";
-                // cerr<<el;
-            }
-            cout<<el;
-        }
-        else{
-            n++;
-            cin>>w;
-            g[x].push_back({w,y});
-            g[y].push_back({w,x});
-            id[{x,y}].insert(w);
-            id[{y,x}].insert(w);
-        }
-    }
+int find(int p)
+{
+	if(p!=fa[p])fa[p]=find(fa[p]);
+	return fa[p];
 }
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);cout.tie(0);
-    //freopen("xxx.in","r",stdin);
-    //freopen("xxx.out","w",stdout);
-    ll T=1;
-    //cin>>T;
-    while(T--){
-        solve();
-    }
-    return 0;
+inline void unite(int p,int q)
+{
+	p=find(p),q=find(q);
+	if(p==q)return;
+	if(dep[p]<dep[q])fa[p]=q;
+	else fa[q]=p;
+	if(dep[p]==dep[q])dep[p]++;
+}
+int main()
+{
+	freopen("P7771.in","w",stdout);
+	int n=Rand(1,100000),m=Rand(190000,200000),i,u,v;
+	cout<<n<<' '<<m;
+	For(i,1,n)fa[i]=i;
+	For(i,1,m>>1)
+	{
+		u=Rand(1,n),v=Rand(1,n);
+		cout<<endl<<u<<' '<<v;
+		unite(u,v);
+		deg[u][0]++;
+		deg[v][1]++;
+	}
+	m-=m>>1;
+	For(i,1,n)
+	if(deg[i][0]<deg[i][1])p.push(i);
+	else if(deg[i][0]>deg[i][1])q.push(i);
+	while(m)
+	{
+		if(p.empty()||q.empty())break;
+		u=p.front();
+		v=q.front();
+		p.pop();
+		q.pop();
+		unite(u,v);
+		cout<<endl<<u<<' '<<v;
+		deg[u][0]++;
+		deg[v][1]++;
+		if(deg[u][0]<deg[u][1])p.push(u);
+		if(deg[v][0]>deg[v][1])q.push(v);
+		m--;
+	}
+	For(i,1,n-1)
+	if(find(i)!=find(n))
+	{
+		cout<<endl<<n<<' '<<i<<endl<<i<<' '<<n;
+		m-=2;
+		unite(n,i);
+	}
+	if(m<2)
+	while(1);
+	while(m>Rand(0,1))
+	{
+		u=Rand(1,n);
+		cout<<endl<<u<<' '<<u;
+		m--;
+	}
+	if(m)cout<<endl<<Rand(1,n)<<' '<<Rand(1,n);
+	return 0;
 }
