f567f0a72c66437ef5ebef50e1f4ff50d735828e
yusiwen <3907363360@qq.com>
2026-03-29 07:19:21 +0000

    2026年3月29日 15:19

--- text.cpp
+++ text.cpp
@@ -3,9 +3,17 @@
 using namespace std;
 #define el '\n'
 const ll N=1e5+5;
-
+ll n,m;
 void solve(){
-    cout<<rand();
+    n=rand()%15;if(n==0) n=15;
+    m=rand()%15;if(m==0) m=15;
+    cout<<n<<" "<<m<<el;
+    for(int i=1;i<=m;i++){
+        ll x,y;
+        x=rand()%n;if(x==0) x=n;
+        y=rand()%n;if(y==0) y=n;
+        cout<<x<<" "<<y<<el;
+    }
 }
 int main(){
     ios::sync_with_stdio(0);
