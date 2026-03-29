24774db0c3e6d86f3bf93056ceffdf8740e4cab9
yusiwen <3907363360@qq.com>
2026-03-29 06:51:09 +0000

    2026年3月29日 14:51

--- P7771.cpp
+++ P7771.cpp
@@ -6,7 +6,12 @@ const ll N=1e5+5;
 ll n,m;
 vector<pair<ll,ll>> g[N];
 void solve(){
-    
+    cin>>n>>m;
+    for(int i=1;i<=n;i++){
+        ll x,y;
+        cin>>x>>y;
+        g[x].push_back(y);
+    }
 }
 int main(){
     ios::sync_with_stdio(0);
