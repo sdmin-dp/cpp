e5f38dc92b8f5fd36724890d368f9ec1c9dd6a06
yusiwen <3907363360@qq.com>
2026-03-29 11:26:43 +0000

    2026年3月29日 19:26

--- 模板及重要代码/ST表.cpp
+++ 模板及重要代码/ST表.cpp
@@ -4,10 +4,16 @@ using namespace std;
 #define el '\n'
 const ll N=1e5+5;
 const ll maxlog=17;
+ll n,q;
+ll a[N];
 ll lg2[N];
 ll st[N][17];
 void init(){
-    
+    for(int i=1;i<=n;i++) lg2[i]=lg2[i/2]+1;
+    for(int i=1;i<=n;i++) st[i][0]=a[i];
+    for(int j=1;j<=maxlog;j++){
+        for(int i=1;i<=)
+    }
 }
 void solve(){
     
