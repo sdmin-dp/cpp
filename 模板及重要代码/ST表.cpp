decaa9408f224e8826fef35b2747012a611d0268
yusiwen <3907363360@qq.com>
2026-03-29 11:24:24 +0000

    2026年3月29日 19:24

---
+++ 模板及重要代码/ST表.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+#define el '\n'
+const ll N=1e5+5;
+const ll maxlog=17;
+ll lg2[N];
+ll st[N][17];
+void init(){
+    
+}
+void solve(){
+    
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    //freopen("xxx.in","r",stdin);
+    //freopen("xxx.out","w",stdout);
+    ll T=1;
+    //cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
