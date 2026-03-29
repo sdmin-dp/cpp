d5af7f0e7e319a6323e8aa95a476e71ad88048e9
yusiwen <3907363360@qq.com>
2026-03-29 07:52:09 +0000

    2026年3月29日 15:52

--- text.cpp
+++ text.cpp
@@ -7,8 +7,20 @@ ll n,m;
 vector<ll> g[N];
 ll in[N],out[N];
 ll idx[N];
+ll s;
+deque<ll> ans;
 void solve(){
-    
+    cin>>n>>m;
+    for(int i=1;i<=n;i++){
+        ll x,y;
+        cin>>x>>y;
+        g[x].push_back(y);
+        in[y]++;
+        out[x]++;
+    }
+    for(int i=1;i<=n;i++){
+        if(in[i])
+    }
 }
 int main(){
     ios::sync_with_stdio(0);
