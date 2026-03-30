82099611dbc1eb2559ac9ef3ee52958b1699baaa
yusiwen <3907363360@qq.com>
2026-03-30 13:57:17 +0000

    2026年3月30日 21:57

--- acc_1581_消息的传递.cpp
+++ acc_1581_消息的传递.cpp
@@ -1,19 +1,61 @@
 #include<bits/stdc++.h>
-#define ll long long
 using namespace std;
+#define ll long long
 #define el '\n'
-const ll N=1e5+5;
-
+const ll N=1e4+5;
+ll n,m;
+vector<ll> g[N];
+ll dfn[N],low[N];
+ll ins[N];
+stack<ll> s;
+ll idx;
+ll cnt=0,len[N],color[N];
+vector<ll> ng[N];
+ll in[N];
+void tarjan(ll x){
+    dfn[x]=low[x]=++idx;
+    s.push(x);
+    ins[x]=1;
+    for(auto i:g[x]){
+        if(!dfn[i]){
+            tarjan(i);
+            low[x]=min(low[x],low[i]);
+        }else if(ins[i]) low[x]=min(low[x],dfn[i]);
+    }
+    if(low[x]==dfn[x]){
+        cnt++;
+        ll top;
+        do{
+            top=s.top();
+            s.pop();
+            ins[top]=0;
+            color[top]=cnt;
+            len[cnt]++;
+        }while(top!=x);
+    }
+}
 void solve(){
-    
+    cin>>n>>m;
+    for(int i=1;i<=m;i++){
+        ll x,y;
+        cin>>x>>y;
+        g[x].push_back(y);
+    }
+    for(int i=1;i<=n;i++) if(!color[i]) tarjan(i);
+    for(int u=1;u<=n;u++){
+        for(auto v:g[u]){
+            if(color[u]!=color[v]) in[color[v]]++;
+        }
+    }
+    ll ans=0;
+    for(int i=1;i<=cnt;i++){
+        if(!in[i]) ans++;
+    }
+    cout<<ans;
 }
 int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);cout.tie(0);
-    //freopen("xxx.in","r",stdin);
-    //freopen("xxx.out","w",stdout);
     ll T=1;
-    //cin>>T;
+    // cin>>T;
     while(T--){
         solve();
     }
