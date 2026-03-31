3dc72762bf70486ed2a2cbb433b02cfe7904e6ad
yusiwen <3907363360@qq.com>
2026-03-31 11:43:33 +0000

    2026年3月31日 19:43

--- acc_RMQ_F.cpp
+++ acc_RMQ_F.cpp
@@ -11,24 +11,34 @@ ll lg2[N];
 void init(){
     for(int i=2;i<N;i++) lg2[i]=lg2[i/2]+1;
     for(int i=1;i<=n;i++) for(ll j=1;j<=m;j++) st[i][j][0][0]=a[i][j];
-    // for(int i=1;i<=n;i++){
-    //     for(int k=1;k<=mxlog;k++){
-    //         for(int j=1;j+(1<<k-1)-1<=m;j++){
-    //             st[i][j][0][k]=max(st[i][j+(1<<k-1)-1][0][k-1],st[i][j][0][k-1]);
+    // for(int k=1;k<=mxlog;k++){
+    //     for(int i=1;i+(1<<k-1)<=n;i++){
+    //         for(int j=1;j<=m;j++){
+    //             st[i][j][k][0]=max(st[i][j+(1<<k-1)][k-1][0],st[i][j][k-1][0]);
     //         }
     //     }
     // }
-    
-    for(int k=1;k<=mxlog;k++){
-        for(int i=1;i+(1<<k-1)<=n;i++){
-            for(int j=1;j<=m;j++){
+
+    for(int i=1;i<=n;i++){
+        for(int k=1;k<=mxlog;k++){
+            for(int j=1;j+(1<<k-1)-1<=m;j++){
                 st[i][j][0][k]=max(st[i][j+(1<<k-1)][0][k-1],st[i][j][0][k-1]);
             }
         }
     }
+
+    for(int j=1;j<=m;j++){
+        for(int k=1;k<=mxlog;k++){
+            for(int i=1;i+(1<<k-1)-1<=n;i++){
+                st[i][j][k][0]=max(st[i+(1<<k-1)][j][k-1][0],st[i][j][k-1][0]);
+            }
+        }
+    }
+
+
     for(int k=1;k<=mxlog;k++){
         for(int l=1;l<=mxlog;l++){
-            for(int i=0;i+(1<<k-1)-1<=n;i++){
+            for(int i=1;i+(1<<k-1)-1<=n;i++){
                 for(int j=1;j+(1<<l-1)-1<=m;j++){
                     st[i][j][k][l]=max({
                         st[i][j][k-1][l-1],
@@ -36,11 +46,6 @@ void init(){
                         st[i+(1<<k-1)][j][k-1][l-1],
                         st[i+(1<<k-1)][j+(1<<l-1)][k-1][l-1]
                     });
-                    cerr<<i<<" "<<j<<" "<<k<<" "<<l<<"   "<<st[i][j][k][l]<<"     ";
-                    cerr<<st[i][j][k-1][l-1]<<" "<<
-                        st[i][j+(1<<l-1)][k-1][l-1]<<" "<<
-                        st[i+(1<<k-1)][j][k-1][l-1]<<" "<<
-                        st[i+(1<<k-1)][j+(1<<l-1)][k-1][l-1]<<el;
                 }
             }
         }
