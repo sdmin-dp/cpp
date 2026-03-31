48833ac6346250e582b6d25283dbb4a260b8c1ca
yusiwen <3907363360@qq.com>
2026-03-31 10:00:08 +0000

    2026年3月31日 18:00

--- acc_RMQ_F.cpp
+++ acc_RMQ_F.cpp
@@ -25,7 +25,11 @@ void init(){
     }
     for(int k=1;k<=mxlog;k++){
         for(int l=1;l<=mxlog;l++){
-            for(int )
+            for(int i=1;i+(1<<k-1)-1<=n;i++){
+                for(int j=1;j+(1<<l-1)-1<=m;j++){
+                    
+                }
+            }
         }
     }
 }
