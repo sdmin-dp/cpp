57170f85bffe44580a2cf851551c31f1267a520d
yusiwen <3907363360@qq.com>
2026-03-31 10:06:52 +0000

    2026年3月31日 18:06

--- acc_RMQ_F.cpp
+++ acc_RMQ_F.cpp
@@ -27,15 +27,25 @@ void init(){
         for(int l=1;l<=mxlog;l++){
             for(int i=1;i+(1<<k-1)-1<=n;i++){
                 for(int j=1;j+(1<<l-1)-1<=m;j++){
-                    
+                    st[i][j][k][l]=max({
+                        st[i][j][n-1][m-1],
+                        st[i][j+(1<<m-1)-1][n-1][m-1],
+                        st[i+(1<<m-1)][j][n-1][m-1],
+                        st[i+(1<<m-1)-1][j+(1<<m-1)-1][n-1][m-1]
+                    });
                 }
             }
         }
     }
 }
+
 void solve(){
     cin>>n>>m;
-    for(int i=1;i<=n;i++) for(int j=1;j<=m;j++) cin>>a[i][j]; 
+    for(int i=1;i<=n;i++) for(int j=1;j<=m;j++) cin>>a[i][j];
+    init();
+    for(int i=1;i<=q;i++){
+        cout<<query()<<el;
+    }
 }
 int main(){
     ios::sync_with_stdio(0);
