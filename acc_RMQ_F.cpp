6063ca9480ddfe3aa6ad767fdc9daa9373897ea2
yusiwen <3907363360@qq.com>
2026-03-31 11:14:15 +0000

    2026年3月31日 19:14

--- acc_RMQ_F.cpp
+++ acc_RMQ_F.cpp
@@ -18,12 +18,12 @@ void init(){
     //         }
     //     }
     // }
-    for(int  j=1;j<=m;j++){
-        for(int k=1;k<=mxlog;k++){
-            for(int i=1;i+(1<<k-1)<=n;i++){
+    
+    for(int k=1;k<=mxlog;k++){
+        for(int i=1;i+(1<<k-1)<=n;i++){
+            for(int j=1;j<=m;j++){
                 st[i][j][k][0]=max(st[i][j+(1<<k-1)][k-1][0],st[i][j][k-1][0]);
             }
-
         }
     }
     for(int k=1;k<=mxlog;k++){
