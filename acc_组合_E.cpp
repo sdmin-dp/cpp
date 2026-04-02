1a2f36096a868618c0306a06dd5c89d20162dbb8
yusiwen <3907363360@qq.com>
2026-04-02 11:17:24 +0000

    2026年4月2日 19:17

---
+++ acc_组合_E.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+#define el '\n'
+const ll N=1e5+5;
+ll n,m,p;
+ll qpow(ll a,ll b,ll p){
+    ll res=1;a=a%p;
+    while(b){
+        if(b&1) res=res*a%p;
+        b>>=1;a=a*a%p;
+    }
+    return res;
+}
+ll C(ll n,ll m,ll p){
+    if(m>n) return 0;
+    m=min(m,n-m);
+    ll a1=1,a2=1;
+    for(int i=1;i<=m;i++){
+        a1=a1*(n-i+1)%p;
+        a2=a2*i%p;
+    }
+    return a1*qpow(a2,p-2,p)%p;
+}
+ll Ls(ll n,ll m,ll p){
+    if(m==0) return 1;
+    return C(n%p,m%p,p)*Ls(n/p,m/p,p)%p;
+}
+void solve(){
+    
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    //freopen("xxx.in","r",stdin);
+    //freopen("xxx.out","w",stdout);
+    ll T=1;
+    //cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
