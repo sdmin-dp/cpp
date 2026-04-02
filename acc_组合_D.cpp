7c7617dc3fc31f176b4c6a5dbc8a30e68bde7ea0
yusiwen <3907363360@qq.com>
2026-04-02 11:15:29 +0000

    2026年4月2日 19:15

---
+++ acc_组合_D.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+#define el '\n'
+const ll N=1e5+5;
+
+void solve(){
+    cin>>n>>p;
+    ny[1]=1;cout<<1<<'\n';
+    for(int i=2;i<=n;i++){
+        ny[i]=(p-p/i)*ny[p%i]%p;
+        cout<<ny[i]<<'\n';
+    }
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    //freopen("xxx.in","r",stdin);
+    //freopen("xxx.out","w",stdout);
+    ll T=1;
+    //cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
