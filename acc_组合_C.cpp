6e029afcb4d44cec6c4b7c830f97da914da4e4e7
yusiwen <3907363360@qq.com>
2026-04-02 10:45:51 +0000

    2026年4月2日 18:45

--- acc_组合_C.cpp
+++ acc_组合_C.cpp
@@ -17,7 +17,8 @@ ll cnm(ll n,ll k) {
 void solve(){
     cin>>n>>m;
     ll a=n/mod,b=m/mod;
-    ll ans=cnm(a,b)*cnm(n%mod,m%mod);
+    ll ans=cnm(a,b)*cnm(n%mod,m%mod)%mod;
+    cout<<ans;
 }
 int main(){
     ios::sync_with_stdio(0);
