7220b8b0e027bd73aa39be4a9c81636e63eafcd9
yusiwen <3907363360@qq.com>
2026-04-04 04:17:39 +0000

    2026年4月4日 12:17

--- P3304.cpp
+++ P3304.cpp
@@ -3,14 +3,13 @@
 using namespace std;
 #define el '\n'
 const ll N=4e4+5;
-ll n,ans,len;
+ll n,ans;
 vector<pair<ll,ll>> g[N];
-ll dfs(ll x,ll fa,ll ){
+ll dfs(ll x,ll fa){
     ll dist=0,d1=0,d2=0;
     for(auto i:g[x]){
         if(i.first==fa) continue;
         ll d=dfs(i.first,x)+i.second;
-        len++;
         dist=max(dist,d);
         if(d>d1) d2=d1,d1=d;
         else if(d>d2) d2=d;
@@ -27,7 +26,7 @@ void solve(){
         g[y].push_back({x,z});
     }
     dfs(1,-1);
-    cout<<ans<<el<<len;
+    cout<<ans;
 }
 int main(){
     ios::sync_with_stdio(0);
