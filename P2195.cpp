c099be3012032dff79a12c604c3e51d80e352f19
yusiwen <3907363360@qq.com>
2026-04-04 13:06:37 +0000

    2026年4月4日 21:06

--- P2195.cpp
+++ P2195.cpp
@@ -1,16 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define ll long long
+#define el '\n'
 const int N=3e5+5;
 vector<int>g[N];
-int n,m,q,f[N],d[N],v[N],L,P;
-int find(int x){return f[x]==x?x:f[x]=find(f[x]);}
+ll n,m,q;
+ll f[N],d[N],v[N];
+ll L,P;
+int find(int x){return f[x]==(x?x:f[x]=find(f[x]));}
 void dfs(int u,int p,int s){
-    if(s>L)L=s,P=u;
-    for(int v:g[u])if(v!=p)dfs(v,u,s+1);
+    if(s>L) L=s,P=u;
+    for(int v:g[u]) if(v!=p) dfs(v,u,s+1);
 }
 int get_d(int u){
-    L=-1;dfs(u,-1,0);
-    L=-1;dfs(P,-1,0);
+    L=-1;
+    dfs(u,-1,0);
+    L=-1;
+    dfs(P,-1,0);
     return L;
 }
 void solve(){
@@ -18,17 +24,21 @@ void solve(){
     for(int i=1;i<=n;i++)f[i]=i;
     while(m--){
         int u,v;cin>>u>>v;
-        g[u].push_back(v);g[v].push_back(u);
-        int fu=find(u),fv=find(v);
-        if(fu!=fv)f[fu]=fv;
+        g[u].push_back(v);
+        g[v].push_back(u);
+        ll fu=find(u),fv=find(v);
+        if(fu!=fv) f[fu]=fv;
     }
     for(int i=1;i<=n;i++){
         int r=find(i);
-        if(!v[r])d[r]=get_d(i),v[r]=1;
+        if(!v[r]){
+            d[r]=get_d(i);
+            v[r]=1;
+        }
     }
     while(q--){
         int o,x,y;cin>>o>>x;
-        if(o==1)cout<<d[find(x)]<<"\n";
+        if(o==1) cout<<d[find(x)]<<"\n";
         else{
             cin>>y;
             int fx=find(x),fy=find(y);
