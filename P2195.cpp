fa0e916eca0353e847a3e3094ab0a40acdb5103b
yusiwen <3907363360@qq.com>
2026-04-04 13:04:56 +0000

    2026年4月4日 21:04

--- P2195.cpp
+++ P2195.cpp
@@ -41,7 +41,13 @@ void solve(){
 }
 int main(){
     ios::sync_with_stdio(0);
-    cin.tie(0);
-    
+    cin.tie(0);cout.tie(0);
+    //freopen("xxx.in","r",stdin);
+    //freopen("xxx.out","w",stdout);
+    ll T=1;
+    //cin>>T;
+    while(T--){
+        solve();
+    }
     return 0;
 }
