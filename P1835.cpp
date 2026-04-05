8a9c320a145b2563ec1794c00cd083602bf952f1
yusiwen <3907363360@qq.com>
2026-04-05 09:15:11 +0000

    2026年4月5日 17:15

--- P1835.cpp
+++ P1835.cpp
@@ -20,10 +20,7 @@ void init(){
 void solve(){
     cin>>l>>r;
     n=sqrt(r);
-    if(l==1&&r==1){
-        cout<<0;
-        return;
-    }
+    vis[1]=1;
     init();
     for(auto i:prime){
         for(int j=(l/i);i*j<=r;j++){
