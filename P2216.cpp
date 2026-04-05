3e056094ab639ca2c10e25d96593453658ce904e
yusiwen <3907363360@qq.com>
2026-04-05 07:44:13 +0000

    2026年4月5日 15:44

--- P2216.cpp
+++ P2216.cpp
@@ -2,13 +2,63 @@
 #define ll long long
 using namespace std;
 #define el '\n'
-const ll N=1e3+5;
+const ll N=100+5;
+const ll mxlog=8;
 ll n,m,k;
 ll a[N][N];
-
+ll st[N][N][11][11];
+ll lg2[N];
+void init(){
+    for(int i=2;i<N;i++) lg2[i]=lg2[i/2]+1;
+    for(int i=1;i<=n;i++) for(ll j=1;j<=m;j++) st[i][j][0][0]=a[i][j];
+    for(int i=1;i<=n;i++){
+        for(int k=1;k<=mxlog;k++){
+            for(int j=1;j+(1<<k-1)-1<=m;j++){
+                st[i][j][0][k]=max(st[i][j+(1<<k-1)][0][k-1],st[i][j][0][k-1]);
+            }
+        }
+    }
+    for(int j=1;j<=m;j++){
+        for(int k=1;k<=mxlog;k++){
+            for(int i=1;i+(1<<k-1)-1<=n;i++){
+                st[i][j][k][0]=max(st[i+(1<<k-1)][j][k-1][0],st[i][j][k-1][0]);
+            }
+        }
+    }
+    for(int k=1;k<=mxlog;k++){
+        for(int l=1;l<=mxlog;l++){
+            for(int i=1;i+(1<<k-1)-1<=n;i++){
+                for(int j=1;j+(1<<l-1)-1<=m;j++){
+                    st[i][j][k][l]=max({
+                        st[i][j][k-1][l-1],
+                        st[i][j+(1<<l-1)][k-1][l-1],
+                        st[i+(1<<k-1)][j][k-1][l-1],
+                        st[i+(1<<k-1)][j+(1<<l-1)][k-1][l-1]
+                    });
+                }
+            }
+        }
+    }
+}
+ll query(ll x1,ll y1,ll x2,ll y2){
+    ll k1=lg2[x2-x1+1];
+    ll k2=lg2[y2-y1+1];
+    return max({
+        st[x1][y1][k1][k2],
+        st[x2-(1<<k1)+1][y1][k1][k2],
+        st[x1][y2-(1<<k2)+1][k1][k2],
+        st[x2-(1<<k1)+1][y2-(1<<k2)+1][k1][k2]
+    });
+}
 void solve(){
     cin>>n>>m>>k;
     for(int i=1;i<=n;i++) for(int j=1;j<=m;j++) cin>>a[i][j];
+    init();
+    for(int i=1;i+k-1<=n;i++){
+        for(int j=1;j+k-1<=m;i++){
+            ll x1=i,y1=j,x2=i+k-1,y2=j+k-1;
+        }
+    }
 }
 int main(){
     ios::sync_with_stdio(0);
