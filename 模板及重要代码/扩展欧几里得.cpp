6c51781a6fb418be09cf890038456bc669869fae
yusiwen <3907363360@qq.com>
2026-04-05 11:00:39 +0000

    2026年4月5日 19:00

--- 模板及重要代码/扩展欧几里得.cpp
+++ 模板及重要代码/扩展欧几里得.cpp
@@ -3,26 +3,20 @@
 using namespace std;
 #define el '\n'
 const ll N=1e5+5;
-int ext_gcd(ll a,ll b,ll &x,ll &y){
-    //递归边界：b==0
+ll exgcd(ll a,ll b,ll &x,ll &y){
     if(b==0){
         x=1;
         y=0;
         return a;
     }
-    //递归调用gcd(b,a%b)
-    ll x1,y1;
-    ll gcd=ext_gcd(b,a%b,x1,y1);
-    //回溯更新x,y
-    x=y1;
-    y=x1-(a/b)*y1;
-    return gcd;
+    ll d=exgcd(b,a%b,y,x);
+    y-=(a/b)*x;
+    return d;
 }
 void solve(){
     
 }
-
-int main() {
+int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
     //freopen("xxx.in","r",stdin);
