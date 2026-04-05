a60c8f069cc9a247588b09d936fa547a253beabf
yusiwen <3907363360@qq.com>
2026-04-05 07:35:14 +0000

    2026年4月5日 15:35

--- P2216.cpp
+++ P2216.cpp
@@ -2,11 +2,16 @@
 #define ll long long
 using namespace std;
 #define el '\n'
-const ll N=1e5+5;
-ll n,m,s;
-
+const ll N=1e2+5;
+ll n,m,k;
+ll a[N][N];
 void solve(){
-    
+    cin>>n>>m>>k;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            cin>>a[i][j];
+        }
+    }
 }
 int main(){
     ios::sync_with_stdio(0);
