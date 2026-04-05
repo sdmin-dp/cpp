8e5c002a1822f3202632e997f49dc91077a1447a
yusiwen <3907363360@qq.com>
2026-04-05 07:47:41 +0000

    2026年4月5日 15:47

--- P2216.cpp
+++ P2216.cpp
@@ -6,22 +6,22 @@ const ll N=100+5;
 const ll mxlog=8;
 ll n,m,k;
 ll a[N][N];
-ll st[N][N][11][11];
+pair<ll,ll> st[N][N][11][11];
 ll lg2[N];
 void init(){
     for(int i=2;i<N;i++) lg2[i]=lg2[i/2]+1;
-    for(int i=1;i<=n;i++) for(ll j=1;j<=m;j++) st[i][j][0][0]=a[i][j];
+    for(int i=1;i<=n;i++) for(ll j=1;j<=m;j++) st[i][j][0][0]={a[i][j],a[i][j]};
     for(int i=1;i<=n;i++){
         for(int k=1;k<=mxlog;k++){
             for(int j=1;j+(1<<k-1)-1<=m;j++){
-                st[i][j][0][k]=max(st[i][j+(1<<k-1)][0][k-1],st[i][j][0][k-1]);
+                st[i][j][0][k]={max(st[i][j+(1<<k-1)][0][k-1].first,st[i][j][0][k-1].first),min(st[i][j+(1<<k-1)][0][k-1].second,st[i][j][0][k-1].second)};
             }
         }
     }
     for(int j=1;j<=m;j++){
         for(int k=1;k<=mxlog;k++){
             for(int i=1;i+(1<<k-1)-1<=n;i++){
-                st[i][j][k][0]=max(st[i+(1<<k-1)][j][k-1][0],st[i][j][k-1][0]);
+                st[i][j][k][0]={max(st[i+(1<<k-1)][j][k-1][0].first,st[i][j][k-1][0].first);
             }
         }
     }
@@ -54,9 +54,11 @@ void solve(){
     cin>>n>>m>>k;
     for(int i=1;i<=n;i++) for(int j=1;j<=m;j++) cin>>a[i][j];
     init();
+    ll ans=10000;
     for(int i=1;i+k-1<=n;i++){
         for(int j=1;j+k-1<=m;i++){
             ll x1=i,y1=j,x2=i+k-1,y2=j+k-1;
+            cout<<query(x1,y1,x2,y2)<<el;
         }
     }
 }
