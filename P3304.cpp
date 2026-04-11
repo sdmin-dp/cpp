1f0b258dbbaa85e19298434eb9e9be2dafbf33bf
yusiwen <3907363360@qq.com>
2026-04-11 13:18:19 +0000

    2026年4月11日 21:18

--- P3304.cpp
+++ P3304.cpp
@@ -5,9 +5,9 @@ using namespace std;
 const ll N=2e5+5;
 ll n;
 vector<pair<ll,ll>> g[N];
-bool book[N];
 ll ans=-1e12,k;
 ll pre[N];
+ll b,e;
 void dfs(ll x,ll fa,ll dis){
     if(dis>ans){
         k=x;
@@ -20,6 +20,7 @@ void dfs(ll x,ll fa,ll dis){
         dfs(v,x,dis+i.second);
     }
 }
+vector<ll> path;
 void solve(){
     cin>>n;
     for(int i=1;i<n;i++){
@@ -29,9 +30,16 @@ void solve(){
         g[y].push_back({x,z});
     }
     dfs(1,0,0);
+    b=k;
     ans=-1e12;
     dfs(k,0,0);
-    
+    e=k;
+    ll cur=e;
+    while(cur!=b){
+        path.push_back(cur);
+        cur=pre[cur];
+    }
+    path.push_back(b);
 }
 int main(){
     ios::sync_with_stdio(0);
