66f675fd64135b28d504dce69587ed02d18014aa
yusiwen <3907363360@qq.com>
2026-04-12 07:24:12 +0000

    2026年4月12日 15:24

--- T738676.cpp
+++ T738676.cpp
@@ -4,7 +4,7 @@ using namespace std;
 #define el '\n'
 const ll N=1e2+5;
 ll n;
-ll m
+ll a[N];
 void solve(){
     
 }
@@ -13,7 +13,10 @@ int main(){
     cin.tie(0);cout.tie(0);
     //freopen("xxx.in","r",stdin);
     //freopen("xxx.out","w",stdout);
-    
+    a[0]=1;
+    for(ll i=1;i<=n;i++){
+        a[i]=a[i-1]*2;
+    }
     ll T=1;
     cin>>T;
     while(T--){
