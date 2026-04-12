b8e02b6fe32756c37a434cec233a70c88583a813
yusiwen <3907363360@qq.com>
2026-04-12 06:43:26 +0000

    2026年4月12日 14:43

--- 模板及重要代码/扩展欧几里得.cpp
+++ 模板及重要代码/扩展欧几里得.cpp
@@ -14,10 +14,7 @@ ll exgcd(ll a,ll b,ll &x,ll &y){
     return d;
 }
 void solve(){
-    ll x=0,y=0;
-    ll gcd=exgcd(2,11,x,y);
-    // 2 11 100
-    cout<<"x:"<<x<<" "<<"y:"<<<<" gcd:"<<gcd;
+    
 }
 int main(){
     ios::sync_with_stdio(0);
