568befe274a17acb49d97311e96efe55821ee1d1
yusiwen <3907363360@qq.com>
2026-04-12 11:13:53 +0000

    2026年4月12日 19:13

--- df_1439.cpp
+++ df_1439.cpp
@@ -2,32 +2,10 @@
 #define ll long long
 using namespace std;
 #define el '\n'
-const ll N=20+5;
-ll n;
-bool vis[N];
-bool isprime(ll x){
-    if(x<2) return 0;
-    for(ll i=2;i*i<=x;i++) if(x%i==0) return 0;
-    return 1;
-}
-void dfs(ll x,string s){
-    if(x==n+1){
-        
-    }
-    if(x==1){
-        vis[1]=1;
-        dfs(x+1,s+'1');
-        vis[1]=0;
-    }
-    for(ll i=1;i<=n;i++){
-        if(!vis[i]){
-            
-        }
-    }
-}
+const ll N=1e5+5;
+
 void solve(){
-    cin>>n;
-    dfs(1,"");
+
 }
 int main(){
     ios::sync_with_stdio(0);
