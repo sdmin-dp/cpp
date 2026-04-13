1c0c912290ba13ca6c096135d722fcefec56ddff
yusiwen <3907363360@qq.com>
2026-04-13 06:29:50 +0000

    2026年4月13日 14:29

--- lgcb 3月 div.3/密室逃脱.cpp
+++ lgcb 3月 div.3/密室逃脱.cpp
@@ -7,9 +7,9 @@ ll n,m;
 ll a[N];
 void solve(){
     cin>>n>>m;
-    for(int i=1;i<=n;i++) cin>>a[i];
+    for(int i=1;i<n;i++) cin>>a[i];
     ll cnt=1;
-    for(int i=1;i<=n;i++){
+    for(int i=1;i<n;i++){
         cerr<<cnt<<" "<<m<<el;
         if(cnt==m){
             cout<<"YES\n";
