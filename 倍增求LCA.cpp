40ce9dad5ca312514aefea4d00ca8829dd3957ad
yusiwen <3907363360@qq.com>
2026-04-13 02:10:43 +0000

    2026年4月13日 10:10

---
+++ 倍增求LCA.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define el '\n'
+const ll N=1e5+5;
+ll n,m;
+ll fa[N];
+ll dep[N],up[15][N];
+void dfs(ll x,ll f){
+    dep[x]=dep[f]+1;up[0][x]=f;
+    for(int i=1;i<10;i++) up[i][x]=up[i-1][up[i-1][x]];
+    for(int i=2;i<=n;i++) if(fa[i]==x) dfs(i,x);
+}
+ll LCA(ll u,ll v){
+    if(dep[u]<dep[v]) swap(v,u);
+    for(int i=9;i>=0;i--){
+        if(dep[up[i][u]]>=dep[v]){
+            u=up[i][u];
+        }
+    }
+    if(u==v) return u;
+    for(int k=9;k>=0;k--){
+        if(up[k][u]!=up[k][v]){
+            u=up[k][u];
+            v=up[k][v];
+        }
+    }
+    return up[0][u];
+}
+void solve(){
+    cin>>n>>m;
+    for(int i=2;i<=n;i++) cin>>fa[i];
+    fa[1]=0;
+    dfs(1,0);
+    for(int i=0;i<m;i++){
+        ll x,y;
+        cin>>x>>y;
+        cout<<LCA(x,y)<<el;
+    }
+}
+int main(){
+    ll T=1;
+    // cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
