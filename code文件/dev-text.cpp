2719b3322a535ea1eee97d9c8cfd1ceebe53cc52
yusiwen <3907363360@qq.com>
2026-04-13 02:36:13 +0000

    2026年4月13日 10:36

--- code文件/dev-text.cpp
+++ code文件/dev-text.cpp
@@ -2,76 +2,51 @@
 #define ll long long
 using namespace std;
 #define el '\n'
-const ll N=100+5;
-const ll mxlog=8;
-ll n,m,k;
-ll a[N][N];
-pair<ll,ll> st[N][N][11][11];
-ll lg2[N];
-void init(){
-    for(int i=2;i<N;i++) lg2[i]=lg2[i/2]+1;
-    for(int i=1;i<=n;i++) for(ll j=1;j<=m;j++) st[i][j][0][0]={a[i][j],a[i][j]};
-    for(int i=1;i<=n;i++){
-        for(int k=1;k<=mxlog;k++){
-            for(int j=1;j+(1<<k-1)-1<=m;j++){
-                st[i][j][0][k]={max(st[i][j+(1<<k-1)][0][k-1].first,st[i][j][0][k-1].first),min(st[i][j+(1<<k-1)][0][k-1].second,st[i][j][0][k-1].second)};
-            }
-        }
-    }
-    for(int j=1;j<=m;j++){
-        for(int k=1;k<=mxlog;k++){
-            for(int i=1;i+(1<<k-1)-1<=n;i++){
-                st[i][j][k][0]={max(st[i+(1<<k-1)][j][k-1][0].first,st[i][j][k-1][0].first),min(st[i+(1<<k-1)][j][k-1][0].second,st[i][j][k-1][0].second)};
-            }
-        }
-    }
-    for(int k=1;k<=mxlog;k++){
-        for(int l=1;l<=mxlog;l++){
-            for(int i=1;i+(1<<k-1)-1<=n;i++){
-                for(int j=1;j+(1<<l-1)-1<=m;j++){
-                    st[i][j][k][l]={max({
-                        st[i][j][k-1][l-1].first,
-                        st[i][j+(1<<l-1)][k-1][l-1].first,
-                        st[i+(1<<k-1)][j][k-1][l-1].first,
-                        st[i+(1<<k-1)][j+(1<<l-1)][k-1][l-1].first
-                    }),min({
-                        st[i][j][k-1][l-1].second,
-                        st[i][j+(1<<l-1)][k-1][l-1].second,
-                        st[i+(1<<k-1)][j][k-1][l-1].second,
-                        st[i+(1<<k-1)][j+(1<<l-1)][k-1][l-1].second
-                    })};
-                }
-            }
-        }
-    }
+const ll N=5e5+5;
+const ll mxlog=20;
+ll n,m,b;
+vector<ll> g[N];
+ll up[mxlog][N],dep[N];
+void dfs(ll x,ll fa){
+    dep[x]=dep[fa]+1;
+    up[0][x]=fa;
+    for(int i=1;i<=mxlog;i++) up[i][x]=up[i-1][up[i-1][x]];
+    for(auto i:g[x]) if(i!=fa) dfs(i,x);
 }
-pair<ll,ll> query(ll x1,ll y1,ll x2,ll y2){
-    ll k1=lg2[x2-x1+1];
-    ll k2=lg2[y2-y1+1];
-    return {max({
-        st[x1][y1][k1][k2].first,
-        st[x2-(1<<k1)+1][y1][k1][k2].first,
-        st[x1][y2-(1<<k2)+1][k1][k2].first,
-        st[x2-(1<<k1)+1][y2-(1<<k2)+1][k1][k2].first
-    }),min({
-        st[x1][y1][k1][k2].second,
-        st[x2-(1<<k1)+1][y1][k1][k2].second,
-        st[x1][y2-(1<<k2)+1][k1][k2].second,
-        st[x2-(1<<k1)+1][y2-(1<<k2)+1][k1][k2].second
-    })};
+ll LCA(ll u,ll v){
+    if(dep[u]<dep[v]) swap(u,v);
+    for(int i=mxlog;i>=0;i--){
+        if(dep[up[i][u]]>=dep[v]){
+            u=up[i][u];
+        }
+    }
+    if(u==v) return u;
+    for(int i=mxlog;i>=0;i--){
+        if(up[i][u]!=up[i][v]){
+            u=up[i][u];
+            v=up[i][v];
+        }
+    }
+    return up[0][u];
 }
 void solve(){
-    cin>>n>>m>>k;
-    for(int i=1;i<=n;i++) for(int j=1;j<=m;j++) cin>>a[i][j];
-    init();
-    ll ans=10000;
-    for(int i=1;i+k-1<=n;i++){
-        for(int j=1;j+k-1<=m;i++){
-            ll x1=i,y1=j,x2=i+k-1,y2=j+k-1;
-            pair<ll,ll> p=query(x1,y1,x2,y2);
-            cout<<p.first-p.second<<el;
-        }
+    cin>>n>>m>>b;
+    for(int i=1;i<n;i++){
+        ll x,y;
+        g[x].push_back(y);
+        g[y].push_back(x);
     }
+    dfs(1,0);
+    ll last=b,ans=0;
+    for(int i=1;i<=m;i++){
+        ll x;
+        cin>>x;
+        ll fa=LCA(last,x);
+        ll t=abs(dep[fa]-dep[x])+abs(dep[fa]-dep[last]);
+        ans+=t;
+        last=x;
+    }
+    cout<<ans;
 }
 int main(){
     ios::sync_with_stdio(0);
