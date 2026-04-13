2168dc19f0b7568c3ab07443565726c39376af6b
yusiwen <3907363360@qq.com>
2026-04-13 09:17:20 +0000

    2026年4月13日 17:17

--- text.cpp
+++ text.cpp
@@ -3,19 +3,18 @@
 using namespace std;
 #define el '\n'
 const ll N=1e5+5;
-ll n;
-void solve(){
-    scanf("%lld",&n);
-    for(ll i=sqrt(n);i>=1;i--){
-        if(n%i) printf("%lld不是%lld的因数\n",i,n);
-        else if(__gcd(i,n/i)!=1) printf("%lld和%lld的最小公倍数不是%lld\n",i,n/i,n); 
-        else printf("%lld和%lld是的\n",i,n/i);
-    }
 
+void solve(){
+    ll sqr=sqrt(n);
+    ll ans;
+    for(ll i=1;i<=min(sqr,m);i++){
+        if(n%i==0) ans=max({ans,i,n/i});
+    }
+    cout<<n/ans;
 }
 int main(){
-    // ios::sync_with_stdio(0);
-    // cin.tie(0);cout.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
     //freopen("xxx.in","r",stdin);
     //freopen("xxx.out","w",stdout);
     ll T=1;
