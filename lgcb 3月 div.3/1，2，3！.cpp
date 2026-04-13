d2b7b6f3150993564fc36e893079dbc1f185586e
yusiwen <3907363360@qq.com>
2026-04-13 10:58:09 +0000

    2026年4月13日 18:58

--- lgcb 3月 div.3/1，2，3！.cpp
+++ lgcb 3月 div.3/1，2，3！.cpp
@@ -18,7 +18,7 @@ void solve(){
         }
         if(cnt[1]>=1&&cnt[2]>=1&&cnt[3]>=1) ans=min(ans,r-l+1);
         
-        cerr<<r<<":"<<cnt[1]<<" "<<cnt[2]<<" "<<cnt[3]<<" "<<l<<" "<<r<<el;
+        // cerr<<r<<":"<<cnt[1]<<" "<<cnt[2]<<" "<<cnt[3]<<" "<<l<<" "<<r<<el;
     }
     // if(cnt[1]>=1&&cnt[2]>=1&&cnt[3]>=1) ans=min(ans,r-l+1);
     if(ans==1e18) cout<<0<<el;
