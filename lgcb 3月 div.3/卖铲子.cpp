fd55dd58e560b432ee0deca7cf71af453724fd9b
yusiwen <3907363360@qq.com>
2026-04-13 06:38:24 +0000

    2026年4月13日 14:38

--- lgcb 3月 div.3/卖铲子.cpp
+++ lgcb 3月 div.3/卖铲子.cpp
@@ -18,8 +18,8 @@ void solve(){
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
-    // freopen("xxx.in","r",stdin);
-    // freopen("xxx.out","w",stdout);
+    freopen("shovel.in","r",stdin);
+    freopen("shovel.out","w",stdout);
     ll T=1;
     cin>>T;
     while(T--){
