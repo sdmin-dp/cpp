0b559476c08c7c51fb7867ac032c4377c406d0fa
yusiwen <3907363360@qq.com>
2026-04-13 03:09:14 +0000

    2026年4月13日 11:09

--- text.cpp
+++ text.cpp
@@ -1,31 +1,19 @@
 #include<bits/stdc++.h>
-using namespace std;
 #define ll long long
+using namespace std;
 #define el '\n'
 const ll N=1e5+5;
-string cmp,s;
-ll mp[30];
-ll mp2[30];
-ll cnt[30];
+
 void solve(){
-    cin>>cmp;
-    for(size_t i=0;i<cmp.size();i++){
-        mp[cmp[i]-'a']=i;
-        mp2[i]=cmp[i]-'a';
-    }
-    char c;
-    while(cin>>c) cnt[mp[c-'a']]++;
     
-    for(int i=0;i<=25;i++){
-        // cerr<<cnt[i]<<" ";
-        if(cnt[i]==0) continue;
-        for(int j=1;j<=cnt[i];j++) printf("%c",mp2[i]+'a');
-        // cerr<<mp2[i];
-    }
 }
 int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    //freopen("xxx.in","r",stdin);
+    //freopen("xxx.out","w",stdout);
     ll T=1;
-    // cin>>T;
+    //cin>>T;
     while(T--){
         solve();
     }
