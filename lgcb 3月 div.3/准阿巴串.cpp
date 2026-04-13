042da11630cba419e83329c57acde830fefea9c5
yusiwen <3907363360@qq.com>
2026-04-13 10:37:58 +0000

    2026年4月13日 18:37

--- lgcb 3月 div.3/准阿巴串.cpp
+++ lgcb 3月 div.3/准阿巴串.cpp
@@ -7,18 +7,21 @@ const ll N=1e5+5;
 void solve(){
     string s;
     cin>>s;
-    ll last1=0,last2=0;
+    ll last1=0,last2=0,ans=0;
     //1:奇a偶b
     //2:奇b偶a
     for(int i=0;i<s.size();i++){
         if(i%2==1&&s[i]=='a'||i%2==0&&s[i]=='b'){
-
+            last2=i+1;
+            ans+=i-last1+1;
         }else if(i%2==1&&s[i]=='b'||i%2==0&&s[i]=='a'){
-
+            last1=i+1;
+            ans+=i-last2+1;
         }else{
-            
+            ans+=i-min(last1,last2)+1;
         }
     }
+    cout<<ans;
 }
 int main(){
     ios::sync_with_stdio(0);
