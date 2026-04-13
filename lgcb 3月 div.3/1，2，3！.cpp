4bf423fec7d3b92a1c9080c42f61c71e4046e165
yusiwen <3907363360@qq.com>
2026-04-13 08:59:22 +0000

    2026年4月13日 16:59

--- lgcb 3月 div.3/1，2，3！.cpp
+++ lgcb 3月 div.3/1，2，3！.cpp
@@ -43,7 +43,7 @@ void solve(){
         ans=min(ans,e-b+1);
         printf("i1:%lld,i2:%lld,k1:%lld,k2:%lld,j:%lld\n",i1,i2,k1,k2,j);
     }
-    // cout<<ans<<el;
+    cout<<ans<<el;
     // cerr<<"num1:";for(int i:num1) cerr<<i<<" ";cerr<<el;
     // cerr<<"num2:";for(int i:num2) cerr<<i<<" ";cerr<<el;
     // cerr<<"num3:";for(int i:num3) cerr<<i<<" ";cerr<<el;
