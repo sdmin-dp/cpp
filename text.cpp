ae89554b04e039efb3b2086b4136f67959a93559
yusiwen <3907363360@qq.com>
2026-04-13 03:10:09 +0000

    2026年4月13日 11:10

--- text.cpp
+++ text.cpp
@@ -2,15 +2,50 @@
 #define ll long long
 using namespace std;
 #define el '\n'
-const ll N=1e5+5;
+const ll N=5e5+5;
+const ll mxlog=20;
+ll n,m,b;
+vector<ll> g[N];
+ll up[mxlog+5][N],dep[N];
+bool vis[N];
 void dfs(ll x,ll fa){
     dep[x]=dep[fa]+1;
     up[0][x]=fa;
     for(int i=1;i<=mxlog;i++) up[i][x]=up[i-1][up[i-1][x]];
     for(auto i:g[x]) if(i!=fa) dfs(i,x);
 }
+void dfs2(ll x,ll lca){
+    vis[x]=1;
+    cout<<x<<" "<<lca;
+    if(x==lca) return;
+    dfs2(up[0][x],lca);
+}
+ll LCA(ll u,ll v){
+    if(dep[u]<dep[v]) swap(u,v);
+    for(int i=mxlog;i>=0;i--){
+        if(dep[up[i][u]]>=dep[v]){
+            u=up[i][u];
+        }
+    }
+    if(u==v) return u;
+    for(int i=mxlog;i>=0;i--){
+        if(up[i][u]!=up[i][v]){
+            u=up[i][u];
+            v=up[i][v];
+        }
+    }
+    return up[0][u];
+}
 void solve(){
-    
+    cin>>n>>m>>b;
+    for(int i=1;i<n;i++){
+        ll x,y;
+        cin>>x>>y;
+        g[x].push_back(y);
+        g[y].push_back(x);
+    }
+    dfs(1,0);
+    dfs2(1,1);
 }
 int main(){
     ios::sync_with_stdio(0);
