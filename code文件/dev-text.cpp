91d2615f35909662ae3c4000b111894d80d893fb
yusiwen <3907363360@qq.com>
2026-04-13 08:13:21 +0000

    2026年4月13日 16:13

--- code文件/dev-text.cpp
+++ code文件/dev-text.cpp
@@ -2,51 +2,28 @@
 #define ll long long
 using namespace std;
 #define el '\n'
-const ll N=5e5+5;
-const ll mxlog=20;
-ll n,m,b;
-vector<ll> g[N];
-ll up[mxlog][N],dep[N];
-void dfs(ll x,ll fa){
-    dep[x]=dep[fa]+1;
-    up[0][x]=fa;
-    for(int i=1;i<=mxlog;i++) up[i][x]=up[i-1][up[i-1][x]];
-    for(auto i:g[x]) if(i!=fa) dfs(i,x);
-}
-ll LCA(ll u,ll v){
-    if(dep[u]<dep[v]) swap(u,v);
-    for(int i=mxlog;i>=0;i--){
-        if(dep[up[i][u]]>=dep[v]){
-            u=up[i][u];
-        }
-    }
-    if(u==v) return u;
-    for(int i=mxlog;i>=0;i--){
-        if(up[i][u]!=up[i][v]){
-            u=up[i][u];
-            v=up[i][v];
-        }
-    }
-    return up[0][u];
-}
+const ll N=1e5+5;
+string s;
+vector<ll> num1,num2,num3;
 void solve(){
-    cin>>n>>m>>b;
-    for(int i=1;i<n;i++){
-        ll x,y;
-        g[x].push_back(y);
-        g[y].push_back(x);
+    cin>>s;
+    for(int i=0;i<s.size();i++){
+        if(s[i]=='1') num1.push_back(i);
+        else if(s[i]=='2') num2.push_back(i);
+        else num3.push_back(i);
     }
-    dfs(1,0);
-    ll last=b,ans=0;
-    for(int i=1;i<=m;i++){
-        ll x;
-        cin>>x;
-        ll fa=LCA(last,x);
-        ll t=abs(dep[fa]-dep[x])+abs(dep[fa]-dep[last]);
-        ans+=t;
-        last=x;
+    if(num1.size()==0||num2.size()==0||num3.size()==0){
+        cout<<0<<el;
+        return;
     }
-    cout<<ans;
+    ll ans=1e18;
+    for(auto j:num2){
+        ll i=lower_bound(num1.begin(),num1.end(),j)-num1.begin();
+        ll k=lower_bound(num3.begin(),num3.end(),j)-num1.begin();
+        ll b=min(i,j,k),e=max(i,j,k);
+        ans=min(ans,e-b+1);
+    }
+    cout<<ans<<el;
 }
 int main(){
     ios::sync_with_stdio(0);
@@ -54,7 +31,7 @@ int main(){
     //freopen("xxx.in","r",stdin);
     //freopen("xxx.out","w",stdout);
     ll T=1;
-    //cin>>T;
+    cin>>T;
     while(T--){
         solve();
     }
