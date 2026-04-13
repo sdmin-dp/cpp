6269460eafea51482214d318472bfcb789541265
yusiwen <3907363360@qq.com>
2026-04-13 06:09:59 +0000

    2026年4月13日 14:09

--- lgcb 3月 div.3/word.cpp
+++ lgcb 3月 div.3/word.cpp
@@ -3,15 +3,17 @@
 using namespace std;
 #define el '\n'
 const ll N=1e5+5;
-ll n;
+string s,t;
 void solve(){
-    
+    cin>>s>>t;
+    swap(s[0],t[0]);
+    cout<<s<<" "<<t<<el;
 }
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
-    //freopen("xxx.in","r",stdin);
-    //freopen("xxx.out","w",stdout);
+    freopen("word.in","r",stdin);
+    freopen("word.out","w",stdout);
     ll T=1;
     cin>>T;
     while(T--){
