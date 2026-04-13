7feb4a9376683bfe81e7052326d1deecc553ba19
yusiwen <3907363360@qq.com>
2026-04-13 10:38:27 +0000

    2026年4月13日 18:38

--- lgcb 3月 div.3/准阿巴串.cpp
+++ lgcb 3月 div.3/准阿巴串.cpp
@@ -26,8 +26,8 @@ void solve(){
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
-    // freopen("xxx.in","r",stdin);
-    // freopen("xxx.out","w",stdout);
+    freopen("xxx.in","r",stdin);
+    freopen("xxx.out","w",stdout);
     ll T=1;
     cin>>T;
     while(T--){
