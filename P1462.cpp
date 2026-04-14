3f5e171c42a0bf1c40509535c564c19122574d3f
yusiwen <3907363360@qq.com>
2026-04-14 03:23:46 +0000

    2026年4月14日 11:23

--- P1462.cpp
+++ P1462.cpp
@@ -6,6 +6,23 @@ const ll N=1e4+5;
 ll n,m,bod;
 ll mon[N];
 vector<pair<ll,ll>> g[N];
+bool check(){
+    
+}
+ll binary_answer(){
+    ll l=1,r=1e9,mid,ans;
+    while(l<=r){
+        mid=(l+r)/2;
+        if(check(mid)){
+            //活着
+            r=mid-1;
+            ans=mid;
+        }else{
+            l=mid+1;
+        }
+    }
+    return ans;
+}
 void solve(){
     cin>>n>>m>>bod;
     for(int i=1;i<=n;i++) cin>>mon[i];
@@ -15,7 +32,7 @@ void solve(){
         g[u].push_back({v,w});
         g[v].push_back({u,w});
     }
-    
+    cout<<binary_answer();
 }
 int main(){
     ios::sync_with_stdio(0);
