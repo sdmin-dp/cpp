dab343d837b83bed20804e31ce9b6548f6423418
yusiwen <3907363360@qq.com>
2026-04-14 23:53:34 +0000

    2026年4月15日 07:53

--- 东方博宜2026.4 B组/C.cpp
+++ 东方博宜2026.4 B组/C.cpp
@@ -3,28 +3,10 @@
 using namespace std;
 #define el '\n'
 const ll N=1e5+5;
-ll a,b,c;
-ll exgcd(ll a,ll b,ll &x,ll &y){
-    if(b==0){
-        x=1;
-        y=0;
-        return a;
-    }
-    ll d=exgcd(b,a%b,y,x);
-    y-=(a/b)*x;
-    return d;
-}
+
 void solve(){
-    cin>>a>>b>>c;
-    if(c-1==a||c-1==b){
-        cout<<1<<" "<<1;
-        return;
-    }else if(c-1==a+b){
-        cout<<1<<" "<<2;
-        return;
-    }
     
-}
+}   
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
@@ -36,4 +18,4 @@ int main(){
         solve();
     }
     return 0;
-}
+}   
