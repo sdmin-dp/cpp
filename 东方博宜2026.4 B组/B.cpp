509916ce0c2096b8737f944e391e715610c882dc
yusiwen <3907363360@qq.com>
2026-04-14 08:21:47 +0000

    2026年4月14日 16:21

--- 东方博宜2026.4 B组/B.cpp
+++ 东方博宜2026.4 B组/B.cpp
@@ -5,10 +5,19 @@ using namespace std;
 const ll N=1e5+5;
 ll n,k;
 pair<ll,ll> a[N];
+pair<ll,ll> cnt[N];
+
 void solve(){
     cin>>n>>k;
     for(int i=1;i<=n;i++){
-        cin>>a[i].first>>a[i].second;
+        cin>>a[i].second>>a[i].first;
+        cnt[a[i].second].first++;
+        cnt[a[i].second].second=a[i].second;
+    }
+    // sort(a+1,a+n+1,cmp);
+    sort(cnt+1,cnt+n+1,greater<ll>());
+    for(int i=1;i<=n;i++){
+
     }
 }
 int main(){
