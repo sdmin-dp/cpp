fe6acee5e499f35932a43e2985efbd0cf77d8991
yusiwen <3907363360@qq.com>
2026-04-14 06:24:20 +0000

    2026年4月14日 14:24

--- 东方博宜2026.4 B组/A.cpp
+++ 东方博宜2026.4 B组/A.cpp
@@ -5,6 +5,28 @@ using namespace std;
 const ll N=2e3+5;
 ll n;
 ll a[N];
+ll binary_sear(ll mn,ll mx){
+    //返回满足条件的数量
+    //二分两次，一个是小于mn+mx的，一个是大于mx-mn的
+    //first
+    ll x=mn+mx;
+    ll res=0,tmp1=0,tmp2=0;
+    ll l=1,r=n,mid=0,ans=0;
+    while(l<=r){
+        mid=(l+r)/2;
+        if(a[mid]>x){
+            r=mid-1;
+        }else{
+            ans=mid;
+            l=mid+1;
+        }
+    }
+    tmp1=ans;
+    x=mx-mn;
+    ans=upper_bound(a+1,a+n+1,x)-a;
+    res+=n-ans+1;
+    return res;
+}
 void solve(){
     cin>>n;
     for(int i=1;i<=n;i++) cin>>a[i];
