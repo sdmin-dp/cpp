9b4076757f7f2cf5b229f6977c4b29ec78e5fc5c
yusiwen <3907363360@qq.com>
2026-04-14 06:07:46 +0000

    2026年4月14日 14:07

--- 东方博宜2026.4 B组/A.cpp
+++ 东方博宜2026.4 B组/A.cpp
@@ -2,11 +2,33 @@
 #define ll long long
 using namespace std;
 #define el '\n'
-const ll N=1e5+5;
+const ll N=2e3+5;
 ll n;
 ll a[N];
+ll binary_search(ll mn,ll mx){
+    //返回满足条件的数量
+    //二分两次，一个是小于mn+mx的，一个是大于mx-mn的
+    //first
+    ll x=mn+mx;
+    ll l=1,r=n,mid=0,ans=0;
+    while(l<=r){
+        mid=(l+r)/2;
+        if(mid>=x){
+            
+        }
+    }
+}
 void solve(){
-    
+    cin>>n;
+    for(int i=1;i<=n;i++) cin>>a[i];
+    sort(a+1,a+n+1);
+    ll ans=0;
+    for(int i=1;i<n;i++){
+        for(int j=i+1;j<=n;j++){
+            ll x=min(i,j),y=max(i,j);
+            ans+=binary_search(x,y);
+        }
+    }
 }
 int main(){
     ios::sync_with_stdio(0);
