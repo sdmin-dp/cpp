f47bd7f912efccd1cd11f112ba134cb246ea4475
yusiwen <3907363360@qq.com>
2026-04-14 08:15:22 +0000

    2026年4月14日 16:15

--- 东方博宜2026.4 B组/B.cpp
+++ 东方博宜2026.4 B组/B.cpp
@@ -3,9 +3,13 @@
 using namespace std;
 #define el '\n'
 const ll N=1e5+5;
-
+ll n,k;
+pair<ll,ll> a[N];
 void solve(){
-    
+    cin>>n>>k;
+    for(int i=1;i<=n;i++){
+        cin>>a[i].first>>a[i].second;
+    }
 }
 int main(){
     ios::sync_with_stdio(0);
