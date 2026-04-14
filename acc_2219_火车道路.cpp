c35aa1439e7e85556f3785d8707d11ec3af8483e
yusiwen <3907363360@qq.com>
2026-04-14 01:36:12 +0000

    2026年4月14日 09:36

--- acc_2219_火车道路.cpp
+++ acc_2219_火车道路.cpp
@@ -4,13 +4,13 @@ using namespace std;
 #define el '\n'
 const ll N=5e5;
 const ll M=4e5;
-vector<vector<int>>e(N+2);
+vector<vector<ll>>e(N+2);
 ll pos[N+2],a[M+2];
-lld[N+2];
-lllca[22][N+2];
-void dfs(int u,int f){
+ll d[N+2];
+ll lca[22][N+2];
+void dfs(ll u,ll f){
     lca[0][u]=f;
-    for(int i=1;i<=20;i++){
+    for(ll i=1;i<=20;i++){
         lca[i][u]=lca[i-1][lca[i-1][u]];
     }
     for(auto v:e[u]){
@@ -20,11 +20,11 @@ void dfs(int u,int f){
         pos[u]=min(pos[u],pos[v]);
     }
 }
-int LCA(int u,ll  v){
+ll LCA(ll u,ll  v){
     if(d[u]<d[v]){
         swap(u,v);
     }
-    for(int i=20;i>=0;i--){
+    for(ll i=20;i>=0;i--){
         if(d[lca[i][u]]>=d[v]){
             u=lca[i][u];
         }
@@ -33,7 +33,7 @@ int LCA(int u,ll  v){
         return u;
     }
 
-    for(int i=20;i>=0;i--){
+    for(ll i=20;i>=0;i--){
         if(lca[i][u]!=lca[i][v]){
             u=lca[i][u];
             v=lca[i][v];
@@ -42,33 +42,35 @@ int LCA(int u,ll  v){
 
     return lca[0][u];
 }
-int dis(int u,int v){
+ll dis(ll u,ll v){
     return d[u]+d[v]-2*d[LCA(u,v)];
+}
+void solve(){
+
 }
 int main(){
     // ios::sync_with_stdio(0);
     // cin.tie(0);cout.tie(0);
-    int n,m,r;
+    ll n,m,r;
     cin>>n>>m>>r;
-    for(int i=1;i<n;i++){
+    for(ll i=1;i<n;i++){
         pos[i]=m+2;
-        int u,v;
+        ll u,v;
         cin>>u>>v;
         e[u].push_back(v);
         e[v].push_back(u);
     }
     pos[n]=m+2;
-
-    for(int i=1;i<=m;i++){
+    for(ll i=1;i<=m;i++){
         cin>>a[i];
         pos[a[i]]=min(pos[a[i]],i);
     }
 
     dfs(r,0);
 
-    int last=r;
-    long long ans=0;
-    for(int i=1;i<=m;i++){
+    ll last=r;
+    ll ans=0;
+    for(ll i=1;i<=m;i++){
         if(pos[a[i]]==i){
             ans+=dis(last,a[i]);
             last=a[i];
