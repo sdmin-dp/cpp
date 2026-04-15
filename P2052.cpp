3a359efaa7000d2aee9a2fd70579e3130c513e23
yusiwen <3907363360@qq.com>
2026-04-15 03:40:38 +0000

    2026年4月15日 11:40

--- P2052.cpp
+++ P2052.cpp
@@ -2,34 +2,39 @@
 #define ll long long
 using namespace std;
 #define el '\n'
-const ll N=1e5+5;
-struct bcj{
-    vector<ll> fa;
-    bcj(ll len){
-        fa.reserve(len+5);
-        fa.resize(len+5);
-        for(int i=1;i<=len;i++) fa[i]=i;
-    }
-    ll find(ll x){
-        if(fa[x]==x) return x;
-        return fa[x]=find(fa[x]);
-    }
-};
-ll len[N];
+const ll N=5e4+5;
 ll n;
+vector<pair<ll,ll>> g[N];
+ll len[N];
+ll ans=0;
+void dfs(ll x,ll fa){
+    for(auto i:g[x]){
+        ll v=i.first,w=i.second;
+        if(v==fa) continue;
+        dfs(v,x);
+        len[x]+=len[v];
+    }
+}
+void dfs2(ll u,ll fa){
+    for(auto i:g[u]){
+        ll v=i.first,w=i.second;
+        if(v==fa) return;
+        ll lenu=len[u],lenv=len[v];
+        len[u]-=len[v];
+        ans+=abs(len[u]-len[v])*w;
+        dfs2(v,u);
+        len[u]=lenu;
+        len[v]=lenv;
+    }
+}
 void solve(){
     cin>>n;
-    bcj a(n);
-    ll ans=0;
-    for(int i=1;i<=n;i++) len[i]=1;
     for(int i=1;i<n;i++){
-        ll x,y,z;cin>>x>>y>>z;
-        int fx=a.find(x),fy=a.find(y);
-        ans+=llabs(len[fx]-len[fy])*z;
-        a.fa[fy]=fx;
-        len[fx]+=len[fy];
+        ll x,y,z;
+        cin>>x>>y>>z;
+        g[x].push_back({x,z});
+        g[y].push_back({x,z});
     }
-    cout<<ans;
 }
 int main(){
     ios::sync_with_stdio(0);
