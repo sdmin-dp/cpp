d2812b983acf3047e4a44bd6a6bc1f73eef7a351
yusiwen <3907363360@qq.com>
2026-04-15 02:15:42 +0000

    2026年4月15日 10:15

--- P1395.cpp
+++ P1395.cpp
@@ -2,10 +2,23 @@
 #define ll long long
 using namespace std;
 #define el '\n'
-const ll N=1e5+5;
+const ll N=5e4+5;
 ll n;
+vector<ll> g[N];
+void dfs(ll x,ll fa){
+    for(auto i:g[x]){
+        if(i==fa) continue;
+        
+    }
+}
 void solve(){
-    
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        ll x,y;cin>>x>>y;
+        g[x].push_back(y);
+        g[y].push_back(x);
+    }
+
 }
 int main(){
     ios::sync_with_stdio(0);
