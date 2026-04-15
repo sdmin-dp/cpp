59bf19e236661065987d14ee01ca715e50da813d
yusiwen <3907363360@qq.com>
2026-04-15 01:14:31 +0000

    2026年4月15日 09:14

--- P5908.cpp
+++ P5908.cpp
@@ -4,9 +4,14 @@ using namespace std;
 #define el '\n'
 const ll N=1e5+5;
 ll n,m;
-vecotr<ll> g[N];
+vector<ll> g[N];
 void solve(){
-    
+    cin>>n>>m;
+    for(int i=1;i<n;i++){
+        ll x,y;cin>>x>>y;
+        g[x].push_back(y);
+        g[y].push_back(x);
+    }
 }
 int main(){
     ios::sync_with_stdio(0);
