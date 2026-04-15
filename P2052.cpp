f2b0df46ff8fb7c3f2d4d387ff39af750c4424c4
yusiwen <3907363360@qq.com>
2026-04-15 03:01:32 +0000

    2026年4月15日 11:01

--- P2052.cpp
+++ P2052.cpp
@@ -3,10 +3,27 @@
 using namespace std;
 #define el '\n'
 const ll N=1e5+5;
+struct bcj{
+    vector<ll> fa;
+    bcj(ll len){
+        fa.reserve(len+5);
+        fa.resize(len+5);
+        for(int i=1;i<=len;i++) fa[i]=i;
+    }
+    ll find(ll x){
+        if(fa[x]==x) return x;
+        return fa[x]=find(fa[x]);
+    }
+};
 ll n,m;
 vector<pair<ll,ll>> g[N];
 void solve(){
-    
+    cin>>n>>m;
+    for(int i=1;i<=m;i++){
+        ll x,y;cin>>x>>y;
+        g[x].push_back(y);
+        g[y].push_back(x);
+    }
 }
 int main(){
     ios::sync_with_stdio(0);
