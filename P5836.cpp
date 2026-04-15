ba9d16be6f5b1cda98222ed75f45e9928a8c71aa
yusiwen <3907363360@qq.com>
2026-04-15 04:24:05 +0000

    2026年4月15日 12:24

--- P5836.cpp
+++ P5836.cpp
@@ -5,7 +5,9 @@ using namespace std;
 const ll N=1e5+5;
 ll n,m;
 ll dep[N],up[15][N];
+char a[N];
 vector<ll> g[N];
+pair<ll,ll> cnt[N];//pair<H,G>
 void dfs(ll x,ll f){
     dep[x]=dep[f]+1;up[0][x]=f;
     for(int i=1;i<10;i++) up[i][x]=up[i-1][up[i-1][x]];
@@ -27,10 +29,28 @@ ll LCA(ll u,ll v){
     }
     return up[0][u];
 }
+void dfs2(ll x,ll fa){
+    a[x]=a[fa];
+    if(a[x]=='H') cnt[x].first++;
+    else cnt[x].second++;
+    for(auto i:g[x]) if(i!=fa) dfs2(i,x);
+}
 void solve(){
     cin>>n>>m;
-    
+    for(int i=1;i<=n;i++) cin>>a[i];
+    for(int i=1;i<n;i++){
+        ll x,y;cin>>x>>y;
+        g[x].push_back(y);
+        g[y].push_back(x);
+    }
     dfs(1,0);
+    dfs2(1,0);
+    for(int i=1;i<=m;i++){
+        ll b,e;
+        char c;
+        cin>>b>>e>>c;
+        
+    }
 }
 int main(){
     ll T=1;
