352d582166fc11e0ce7f8fdc669e995e5e50475e
yusiwen <3907363360@qq.com>
2026-04-16 10:14:33 +0000

    2026年4月16日 18:14

--- 东方博宜2026.4 A/C.cpp
+++ 东方博宜2026.4 A/C.cpp
@@ -10,7 +10,9 @@ bool vis[N][N];
 void solve(){
     cin>>n>>m;
     for(int i=1;i<=m;i++) cin>>a[i].first>>a[i].second;
-    
+    for(int i=1;i<=n;i++){
+        
+    }
 }
 int main(){
     ios::sync_with_stdio(0);
