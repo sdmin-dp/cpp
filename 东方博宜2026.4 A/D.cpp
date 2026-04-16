c8c76c9c5d563667ae080a2606fd142ffb9f72e5
yusiwen <3907363360@qq.com>
2026-04-16 08:14:35 +0000

    2026年4月16日 16:14

--- 东方博宜2026.4 A/D.cpp
+++ 东方博宜2026.4 A/D.cpp
@@ -26,7 +26,10 @@ void solve(){
     for(int i=6;i<=9;i++) y=y*10+(s[i]-48);
     for(int i=10;i<=11;i++) m=m*10+(s[i]-48);
     for(int i=12;i<=13;i++) d=d*10+(s[i]-48);
-    if(y==1900&&m==1&&d>=1||y==){
+    if(y==1900){
+        if(m==1){
+            
+        }
     }
 }
 int main(){
