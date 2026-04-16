3a3e2c6dff9d5275403bec40da596fcebe9bbad1
yusiwen <3907363360@qq.com>
2026-04-16 08:12:01 +0000

    2026年4月16日 16:12

--- 东方博宜2026.4 A/D.cpp
+++ 东方博宜2026.4 A/D.cpp
@@ -6,6 +6,9 @@ const ll N=1e5+5;
 const ll mod=11;
 string s;
 bool f1=0,f2=0;
+bool isrunnian(ll a){
+    return ((a%4==0&&a%100!=0)||(a%400==0));
+}
 void solve(){
     cin>>s;
     f1=0,f2=0;
@@ -20,13 +23,10 @@ void solve(){
     }
     if(sum%mod==1) f1=1;
     ll y=0,m=0,d=0;
-    for(int i=6;i<=9;i++){
-        y=y*10+(s[i]-48);
-    }
-    for(int i=10;i<=12;i++){
-        m=m*10+(s[i]-48);
-    }
-    for(int i=13)
+    for(int i=6;i<=9;i++) y=y*10+(s[i]-48);
+    for(int i=10;i<=11;i++) m=m*10+(s[i]-48);
+    for(int i=12;i<=13;i++) d=d*10+(s[i]-48);
+    if(y)
 }
 int main(){
     ios::sync_with_stdio(0);
