40bddae8d236a029a1710b0485d2ee049627ba8f
yusiwen <3907363360@qq.com>
2026-04-16 08:17:57 +0000

    2026年4月16日 16:17

--- 东方博宜2026.4 A/D.cpp
+++ 东方博宜2026.4 A/D.cpp
@@ -26,11 +26,7 @@ void solve(){
     for(int i=6;i<=9;i++) y=y*10+(s[i]-48);
     for(int i=10;i<=11;i++) m=m*10+(s[i]-48);
     for(int i=12;i<=13;i++) d=d*10+(s[i]-48);
-    if(y==1900){
-        if(m==1){
-            
-        }
-    }
+    if(y==19)
 }
 int main(){
     ios::sync_with_stdio(0);
