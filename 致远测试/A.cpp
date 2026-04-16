41668f8c97c9d5e8861995273c23797e937c6e18
yusiwen <3907363360@qq.com>
2026-04-16 10:27:54 +0000

    2026年4月16日 18:27

--- 致远测试/A.cpp
+++ 致远测试/A.cpp
@@ -10,10 +10,16 @@ void solve(){
     for(int i=1;i<=n;i++) cin>>a[i];
     ll mn=1e12,k=0;
     for(int i=l;i<=r;i++){
+        ll sum=0;
         for(int j=1;j<=n;j++){
-
+            sum+=(a[j]%i);
+        }
+        if(sum<mn){
+            mn=sum;
+            k=i;
         }
     }
+    cout<<k;
 }
 int main(){
     ios::sync_with_stdio(0);
