92ac5273649d991d1bb232a8defedd03bd600f39
yusiwen <3907363360@qq.com>
2026-04-16 09:32:56 +0000

    2026年4月16日 17:32

--- 东方博宜2026.4 A/D.cpp
+++ 东方博宜2026.4 A/D.cpp
@@ -30,23 +30,23 @@ void solve(){
     // cout<<y<<" "<<m<<" "<<d<<el;
     if(y==2025){
         if(m==5){
-            if(d<=10){
+            if(d>=1&&d<=10){
                 f2=1;
             }
         }else if(m<5){
             if(m==1||m==3||m==5||m==7||m==8||m==10||m==12){
-                    if(d>=1&&d<=31){
-                        f2=1;
-                    }else if(m==2){
-                        if(isrunnian(y)){
-                            if(d>=1&&d<=29) f2=1;
-                        }else if(!isrunnian(y)){
-                            if(d>=1&&d<=28) f2=1;
-                        }
-                    }else{
-                        if(d>=1&&d<=30) f2=1;
-                    }
+                if(d>=1&&d<=31){
+                    f2=1;
                 }
+            }else if(m==2){
+                if(isrunnian(y)){
+                    if(d>=1&&d<=29) f2=1;
+                }else if(!isrunnian(y)){
+                    if(d>=1&&d<=28) f2=1;
+                }
+            }else{
+                if(d>=1&&d<=30) f2=1;
+            }
         }
     }else if(y>=1900&&y<2025){
         if(m>=1&&m<=12){
