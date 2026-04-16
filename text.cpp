82f5c3039ecf68a37922efbfdce37ec5fc312dce
yusiwen <3907363360@qq.com>
2026-04-16 08:33:40 +0000

    2026年4月16日 16:33

--- text.cpp
+++ text.cpp
@@ -2,17 +2,14 @@
 #define ll long long
 using namespace std;
 #define el '\n'
-const ll N=100+5;
-char str[N],str1[N];
+const ll N=1e5+5;
+
 void solve(){
-    string s="13r ef";
-    // scanf("%s",&s[0]);
-    // cout<<s;
-    printf("%s",s);
+    
 }
 int main(){
-    // ios::sync_with_stdio(0);
-    // cin.tie(0);cout.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
     //freopen("xxx.in","r",stdin);
     //freopen("xxx.out","w",stdout);
     ll T=1;
