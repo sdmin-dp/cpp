1d9fb7b64b642065a0ad7e2241b276c69364c62f
yusiwen <3907363360@qq.com>
2026-04-16 06:42:26 +0000

    2026年4月16日 14:42

--- P1351.cpp
+++ P1351.cpp
@@ -3,9 +3,27 @@
 using namespace std;
 #define el '\n'
 const ll N=1e2+5;
-const ll M=1e5
+const ll M=1e3+5;
+ll n,m,k;
+ll dp[M],dp2[M];
+pair<ll,ll> a[N];//pair<高度,价值>
 void solve(){
-    
+    cin>>n>>m>>k;
+    for(int i=1;i<=n;i++) cin>>a[i].first>>a[i].second;
+    for(int i=1;i<=n;i++){
+        for(int j=a[i].first;j<=m;j++){
+            dp[j]=max(dp[j],dp[j-(ll)(a[i].first*0.8)]+);
+        }
+    }
+    for(int i=1;i<=n;i++){
+        for(int j=a[i].first;j<=m;j++){
+            dp2[j]=max(dp2[j],dp2[j-a[i].first]+a[i].second);
+        }
+    }
+    ll ans=dp2[m];
+    for(int i=1;i<=n;i++){
+        if(a[i].first)
+    }
 }
 int main(){
     ios::sync_with_stdio(0);
