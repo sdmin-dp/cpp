0c7cc93e196b712da3efa418f832d74090216294
yusiwen <3907363360@qq.com>
2026-04-16 01:40:33 +0000

    2026年4月16日 09:40

--- P3379.cpp
+++ P3379.cpp
@@ -3,9 +3,15 @@
 using namespace std;
 #define el '\n'
 const ll N=1e5+5;
-ll n,q,m;
+ll n,q,b;
+vector<ll> g[N];
 void solve(){
-    
+    cin>>n>>q>>b;
+    for(int i=1;i<n;i++){
+        ll x,y;cin>>x>>y;
+        g[x].push_back(y);
+        g[y].push_back(x);
+    }
 }
 int main(){
     ios::sync_with_stdio(0);
