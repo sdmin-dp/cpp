08249fdcd7b367d3c99409f5bc7b9e0b3a1e8f29
yusiwen <3907363360@qq.com>
2026-04-16 12:32:46 +0000

    2026年4月16日 20:32

---
+++ 致远测试/D.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+#define el '\n'
+const ll N=1e5+5;
+
+void solve(){
+    ll n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        ll x=rand()%n;
+        if(x==0) x==n;
+    }
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    //freopen("xxx.in","r",stdin);
+    //freopen("xxx.out","w",stdout);
+    ll T=1;
+    //cin>>T;
+    srand(time(0));
+    while(T--){
+        solve();
+    }
+    return 0;
+}
