259d0be1ee065fba6cad72071718392f96298864
yusiwen <3907363360@qq.com>
2026-04-16 08:36:57 +0000

    2026年4月16日 16:36

--- text.cpp
+++ text.cpp
@@ -5,7 +5,10 @@ using namespace std;
 const ll N=1e5+5;
 
 void solve(){
-    
+    /*
+    140213201412039372
+    2^17 2^16 2^15 2^14 2^13 2^12 2^11 2^10 1^9 2^8 2^7 2^6 2^5 2^4 2^3 2^2 2^1
+    */
 }
 int main(){
     ios::sync_with_stdio(0);
