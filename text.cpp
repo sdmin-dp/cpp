72d0e292c830c9db7c158ec7d6f9e96c40eee82e
yusiwen <3907363360@qq.com>
2026-04-16 09:35:27 +0000

    2026年4月16日 17:35

--- text.cpp
+++ text.cpp
@@ -35,18 +35,18 @@ void solve(){
             }
         }else if(m<5){
             if(m==1||m==3||m==5||m==7||m==8||m==10||m==12){
-                    if(d>=1&&d<=31){
-                        f2=1;
-                    }else if(m==2){
-                        if(isrunnian(y)){
-                            if(d>=1&&d<=29) f2=1;
-                        }else if(!isrunnian(y)){
-                            if(d>=1&&d<=28) f2=1;
-                        }
-                    }else{
-                        if(d>=1&&d<=29) f2=1;
-                    }
+                if(d>=1&&d<=31){
+                    f2=1;
                 }
+            }else if(m==2){
+                if(isrunnian(y)){
+                    if(d>=1&&d<=29) f2=1;
+                }else if(!isrunnian(y)){
+                    if(d>=1&&d<=28) f2=1;
+                }
+            }else{
+                if(d>=1&&d<=29) f2=1;
+            }
         }
     }else if(y>=1900&&y<2025){
         if(m>=1&&m<=12){
