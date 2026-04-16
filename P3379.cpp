e0275a895949fa0f85a4ef16e5344553b73f7c6f
yusiwen <3907363360@qq.com>
2026-04-16 01:43:06 +0000

    2026年4月16日 09:43

--- P3379.cpp
+++ P3379.cpp
@@ -7,18 +7,18 @@ ll n,q,b;
 ll dep[N],up[20][N];
 vector<ll> g[N];void dfs(ll x,ll f){
     dep[x]=dep[f]+1;up[0][x]=f;
-    for(int i=1;i<17;i++) up[i][x]=up[i-1][up[i-1][x]];
+    for(int i=1;i<20;i++) up[i][x]=up[i-1][up[i-1][x]];
     for(auto i:g[x]) if(i!=f) dfs(i,x);
 }
 ll LCA(ll u,ll v){
     if(dep[u]<dep[v]) swap(v,u);
-    for(int i=17;i>=0;i--){
+    for(int i=20;i>=0;i--){
         if(dep[up[i][u]]>=dep[v]){
             u=up[i][u];
         }
     }
     if(u==v) return u;
-    for(int k=17;k>=0;k--){
+    for(int k=20;k>=0;k--){
         if(up[k][u]!=up[k][v]){
             u=up[k][u];
             v=up[k][v];
