77bca432546d0c6f6170a3ffd44478ede31e5412
yusiwen <3907363360@qq.com>
2026-04-16 08:45:01 +0000

    2026年4月16日 16:45

--- 东方博宜2026.4 A/D.cpp
+++ 东方博宜2026.4 A/D.cpp
@@ -17,7 +17,7 @@ void solve(){
         ll x=0;
         if(s[i]=='X') x=10;
         else x=s[i]-48;
-        sum+=(x*(1<<(i+1)));
+        sum+=(x*pow(2,(18-i-1)));
     }
     if(sum%mod==1) f1=1;
     cout<<sum%mod<<" ";
