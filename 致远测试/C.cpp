a3904d1b9b5d5750fe5fee21fa9336152c9389b4
yusiwen <3907363360@qq.com>
2026-04-16 12:04:07 +0000

    2026年4月16日 20:04

--- 致远测试/C.cpp
+++ 致远测试/C.cpp
@@ -11,7 +11,7 @@ void solve(){
     for(int i=1;i<=n;i++){
         cin>>a[i];
     }    
-    sort(a+1,a+n+1);
+    sort(a+1,a+n+1,greater<ll>());
     ll ans=1;win[1]=1;
     for(int i=2;i<=n;i++){
         if(a[i-1]-a[i]<=k&&win[i-1]){
