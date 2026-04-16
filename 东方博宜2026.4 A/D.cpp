609cc9f464928a22c9c4af90205ac99bccafe8a8
yusiwen <3907363360@qq.com>
2026-04-16 08:27:01 +0000

    2026年4月16日 16:27

--- 东方博宜2026.4 A/D.cpp
+++ 东方博宜2026.4 A/D.cpp
@@ -25,48 +25,49 @@ void solve(){
     ll y=0,m=0,d=0;
     for(int i=6;i<=9;i++) y=y*10+(s[i]-48);
     for(int i=10;i<=11;i++) m=m*10+(s[i]-48);
-    for(int i=12;i<=13;i++) d=d*10+(s[i]-48);
-    if(y==2025){
-        if(m==5){
-            if(d<=10){
-                f2=1;
-            }
-        }else if(m<5){
-            if(m==1||m==3||m==5||m==7||m==8||m==10||m==12){
-                    if(d>=1&&d<=31){
-                        f2=1;
-                    }else if(m==2){
-                        if(isrunnian(y)){
-                            if(d>=1&&d<=29) f2=1;
-                        }else if(!isrunnian(y)){
-                            if(d>=1&&d<=28) f2=1;
-                        }
-                    }else{
-                        if(d>=1&&d<=29) f2=1;
-                    }
-                }
-        }
-    }else{
-        if(y>=1900){
-            if(m>=1&&m<=12){
-                if(m==1||m==3||m==5||m==7||m==8||m==10||m==12){
-                    if(d>=1&&d<=31){
-                        f2=1;
-                    }else if(m==2){
-                        if(isrunnian(y)){
-                            if(d>=1&&d<=29) f2=1;
-                        }else if(!isrunnian(y)){
-                            if(d>=1&&d<=28) f2=1;
-                        }
-                    }else{
-                        if(d>=1&&d<=29) f2=1;
-                    }
-                }
-            }
-        }
-    }
-    if(f1&&f2) cout<<"PASS\n";
-    else cout<<"FAIL\n";
+    cout<<y<<" "<<m<<" "<<d<<el;
+    // for(int i=12;i<=13;i++) d=d*10+(s[i]-48);
+    // if(y==2025){
+    //     if(m==5){
+    //         if(d<=10){
+    //             f2=1;
+    //         }
+    //     }else if(m<5){
+    //         if(m==1||m==3||m==5||m==7||m==8||m==10||m==12){
+    //                 if(d>=1&&d<=31){
+    //                     f2=1;
+    //                 }else if(m==2){
+    //                     if(isrunnian(y)){
+    //                         if(d>=1&&d<=29) f2=1;
+    //                     }else if(!isrunnian(y)){
+    //                         if(d>=1&&d<=28) f2=1;
+    //                     }
+    //                 }else{
+    //                     if(d>=1&&d<=29) f2=1;
+    //                 }
+    //             }
+    //     }
+    // }else{
+    //     if(y>=1900){
+    //         if(m>=1&&m<=12){
+    //             if(m==1||m==3||m==5||m==7||m==8||m==10||m==12){
+    //                 if(d>=1&&d<=31){
+    //                     f2=1;
+    //                 }else if(m==2){
+    //                     if(isrunnian(y)){
+    //                         if(d>=1&&d<=29) f2=1;
+    //                     }else if(!isrunnian(y)){
+    //                         if(d>=1&&d<=28) f2=1;
+    //                     }
+    //                 }else{
+    //                     if(d>=1&&d<=29) f2=1;
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+    // if(f1&&f2) cout<<"PASS\n";
+    // else cout<<"FAIL\n";
 }
 int main(){
     ios::sync_with_stdio(0);
