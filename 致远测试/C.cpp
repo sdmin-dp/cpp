33c924902393950a2c289909df9ac23bc8e36778
yusiwen <3907363360@qq.com>
2026-04-16 12:03:02 +0000

    2026年4月16日 20:03

--- 致远测试/C.cpp
+++ 致远测试/C.cpp
@@ -5,12 +5,19 @@ using namespace std;
 const ll N=1e5+5;
 ll n,k;
 ll a[N];
+bool win[N];
 void solve(){
     cin>>n>>k;
     for(int i=1;i<=n;i++){
         cin>>a[i];
     }    
-    
+    ll ans=1;win[1]=1;
+    for(int i=2;i<=n;i++){
+        if(a[i-1]-a[i]<=k&&win[i-1]){
+            win[i]=1;
+            ans++;
+        }
+    }
 }
 int main(){
     ios::sync_with_stdio(0);
