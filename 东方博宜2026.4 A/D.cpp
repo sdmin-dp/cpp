50b8373ddaf28627daabcc0c39ceb8e9d6f73865
yusiwen <3907363360@qq.com>
2026-04-16 09:34:34 +0000

    2026年4月16日 17:34

--- 东方博宜2026.4 A/D.cpp
+++ 东方博宜2026.4 A/D.cpp
@@ -34,7 +34,7 @@ void solve(){
                 f2=1;
             }
         }else if(m<5){
-            if(m==1||m==3||m==5||m==7||m==8||m==10||m==12){
+            if(m==1||m==3){
                 if(d>=1&&d<=31){
                     f2=1;
                 }
@@ -44,7 +44,7 @@ void solve(){
                 }else if(!isrunnian(y)){
                     if(d>=1&&d<=28) f2=1;
                 }
-            }else{
+            }else if(m==4){
                 if(d>=1&&d<=30) f2=1;
             }
         }
