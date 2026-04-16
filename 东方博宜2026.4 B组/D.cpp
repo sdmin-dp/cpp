af7e9fea23582bb0191fe71ab0c48a7aa5377d6e
yusiwen <3907363360@qq.com>
2026-04-16 07:06:55 +0000

    2026年4月16日 15:06

--- 东方博宜2026.4 B组/D.cpp
+++ 东方博宜2026.4 B组/D.cpp
@@ -3,21 +3,21 @@
 using namespace std;
 #define el '\n'
 const ll N=1e2+5;
-const ll M=(1e3)*1.25+5;
+const ll M=1e3+5;
 ll n,m,k;
-ll dp2[M];
+ll dp[M],dp2[M];
 pair<ll,ll> a[N];//pair<价值,高度>
 void solve(){
     cin>>n>>m>>k;
     for(int i=1;i<=n;i++) cin>>a[i].first>>a[i].second;
-    // for(int i=1;i<=n;i++){
-    //     for(int j=a[i].second*0.8;j<=m;j++){
-    //         dp[j]=max(dp[j],dp[j-(ll)(a[i].second*0.8)]+a[i].first);
-    //         // cerr<<(ll)(a[i].second*0.8)<<" "<<dp[j]<<el;
-    //     }
-    // }
     for(int i=1;i<=n;i++){
-        for(int j=a[i].second;j<=m*1.25;j++){
+        for(int j=a[i].second*0.8;j<=m;j++){
+            dp[j]=max(dp[j],dp[j-(ll)(a[i].second*0.8)]+a[i].first);
+            // cerr<<(ll)(a[i].second*0.8)<<" "<<dp[j]<<el;
+        }
+    }
+    for(int i=1;i<=n;i++){
+        for(int j=a[i].second;j<=m;j++){
             dp2[j]=max(dp2[j],dp2[j-a[i].second]+a[i].first);
         }
     }
@@ -27,7 +27,7 @@ void solve(){
     ll ans=dp2[m];
     for(int i=1;i<=n;i++){
         if(a[i].second<k) continue;
-        ans=max(ans,dp2[m-a[i].second*5/4]+a[i].first);
+        ans=max(ans,dp[m-a[i].second]+a[i].first);
         // cerr<<i<<" "<<dp[m-a[i].second]+a[i].first<<" "<<m-a[i].second<<" "<<ans<<" "<<dp[28]<<el;
     }
     cout<<ans;
