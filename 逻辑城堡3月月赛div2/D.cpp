c6e643201e0ec8efe5cd353b2559ce3cdcbf0259
yusiwen <3907363360@qq.com>
2026-04-17 07:31:12 +0000

    2026年4月17日 15:31

--- 逻辑城堡3月月赛div2/D.cpp
+++ 逻辑城堡3月月赛div2/D.cpp
@@ -5,7 +5,7 @@ using namespace std;
 const ll N=1e5+5;
 
 void solve(){
-    
+    ll n=rand();
 }
 int main(){
     ios::sync_with_stdio(0);
@@ -14,6 +14,7 @@ int main(){
     //freopen("xxx.out","w",stdout);
     ll T=1;
     //cin>>T;
+    srand(time(0));
     while(T--){
         solve();
     }
