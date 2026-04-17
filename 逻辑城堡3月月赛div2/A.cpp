6b4f866278df8c339e2297f553c418742fd2f4c5
yusiwen <3907363360@qq.com>
2026-04-17 06:12:48 +0000

    2026年4月17日 14:12

--- 逻辑城堡3月月赛div2/A.cpp
+++ 逻辑城堡3月月赛div2/A.cpp
@@ -4,8 +4,17 @@ using namespace std;
 #define el '\n'
 const ll N=1e5+5;
 string s;
+ll cnt[30];
 void solve(){
-    
+    cin>>s;
+    for(auto &i:s) cnt[i-'a']++;
+    ll sum1=0;char c;
+    for(auto i=0;i<26;i++){
+        if(cnt[i]==1){
+            sum1++;
+            c=char(i+'a');
+        }
+    }
 }
 int main(){
     ios::sync_with_stdio(0);
