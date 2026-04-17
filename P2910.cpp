bb4aeff224b4db6a398a2fe4bbb2b70cb7cace5d
yusiwen <3907363360@qq.com>
2026-04-17 03:55:30 +0000

    2026年4月17日 11:55

--- P2910.cpp
+++ P2910.cpp
@@ -23,12 +23,7 @@ void solve(){
                 if(dis[i][k]+dis[k][j]<dis[i][j]) dis[i][j]=dis[i][k]+dis[k][j];
             }
         }
-    }
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++){
-            
-        }
-    }
+    }=
     ll sum=0;
     for(int i=2;i<=m;i++){
         sum+=dis[a[i-1]][a[i]];
