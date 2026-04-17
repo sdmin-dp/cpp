5216defa8a7ddae5c924ed2d30371eb82ff13db1
yusiwen <3907363360@qq.com>
2026-04-17 07:46:56 +0000

    2026年4月17日 15:46

--- text.cpp
+++ text.cpp
@@ -1,29 +1,10 @@
-#include<bits/stdc++.h>
-#define ll long long
-using namespace std;
-#define el '\n'
-const ll N=1e5+5;
-ll n,k;
-ll a[N];
-void solve(){
-    n=rand()%20,k=rand()%20;
-    cout<<n<<" "<<k<<el;
-    for(int i=1;i<=n;i++){
-        a[i]=rand()%100;
-        // cout<<a[i]<<" ";
-    }
+#include <vector>
 
-}
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);cout.tie(0);
-    //freopen("xxx.in","r",stdin);
-    //freopen("xxx.out","w",stdout);
-    ll T=1;
-    //cin>>T;
-    srand(time(0));
-    while(T--){
-        solve();
+int main() {
+    std::vector<long long*> v;
+    while (true) {
+        // 每次申请 100MB 左右的内存
+        v.push_back(new long long[12500000]); 
     }
     return 0;
 }
