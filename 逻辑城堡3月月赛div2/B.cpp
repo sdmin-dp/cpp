69cef026755089e5e2ff056b35d67ae9b4f7e4ed
yusiwen <3907363360@qq.com>
2026-04-17 06:57:35 +0000

    2026年4月17日 14:57

--- 逻辑城堡3月月赛div2/B.cpp
+++ 逻辑城堡3月月赛div2/B.cpp
@@ -12,7 +12,7 @@ void solve(){
     for(int i=1;i<=n;i++) cin>>a[i];
     for(int i=1;i<=n;i++){
         for(int j=i+1;j<=n;j++){
-            if(i>j){
+            if(a[i]>a[j]){
                 res.push_back({i,j});
                 swap(a[i],a[j]);
                 ans++;
