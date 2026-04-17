c5545a6d7f3c09bb7d604822fe34c43b939b839f
yusiwen <3907363360@qq.com>
2026-04-17 06:24:32 +0000

    2026年4月17日 14:24

--- 逻辑城堡3月月赛div2/A.cpp
+++ 逻辑城堡3月月赛div2/A.cpp
@@ -19,8 +19,8 @@ void solve(){
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
-    // freopen("pair.in","r",stdin);
-    // freopen("pair.out","w",stdout);
+    freopen("pair.in","r",stdin);
+    freopen("pair.out","w",stdout);
     ll T=1;
     //cin>>T;
     while(T--){
