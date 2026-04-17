2ee17a2c0075a637af9194f6a06881cd016ea6a3
yusiwen <3907363360@qq.com>
2026-04-17 04:34:02 +0000

    2026年4月17日 12:34

--- B3611.cpp
+++ B3611.cpp
@@ -2,10 +2,19 @@
 #define ll long long
 using namespace std;
 #define el '\n'
-const ll N=1e5+5;
+const ll N=1e2+5;
 ll n;
 ll dis[N][N];
 void solve(){
+    cin>>n;
+    memset(dis,0x3f,sizeof(dis));
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            bool x;
+            cin>>x;
+            if(x!=0) dis[i][j]=x;
+        }
+    }
     
 }
 int main(){
