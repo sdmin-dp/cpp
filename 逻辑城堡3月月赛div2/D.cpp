991115640a709e16bb2911327670aa8a4bc25eb2
yusiwen <3907363360@qq.com>
2026-04-17 08:57:18 +0000

    2026年4月17日 16:57

--- 逻辑城堡3月月赛div2/D.cpp
+++ 逻辑城堡3月月赛div2/D.cpp
@@ -25,7 +25,7 @@ void solve(){
         double C=c/((n-i)*1.0);
         if(C>li&&C<ri&&C>lI&&C<rI) ans--;
         c+=x[i];
-        cerr<<li<<" "<<ri<<" "<<lI<<" "<<rI<<" "<<i<<" "<<x[i]<<el;
+        // cerr<<li<<" "<<ri<<" "<<lI<<" "<<rI<<" "<<i<<" "<<x[i]<<el;
     }
     cout<<ans;
 }
