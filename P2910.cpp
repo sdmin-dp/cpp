50161ee1223a7c346f1270625b98594362c1d93f
yusiwen <3907363360@qq.com>
2026-04-17 04:02:02 +0000

    2026年4月17日 12:02

--- P2910.cpp
+++ P2910.cpp
@@ -14,7 +14,7 @@ void solve(){
             ll x;cin>>x;
             dis[i][j]=x;
         }
-        dis[i][i]=0;z
+        dis[i][i]=0;
     }
     for(int k=1;k<=n;k++){
         for(int i=1;i<=n;i++){
