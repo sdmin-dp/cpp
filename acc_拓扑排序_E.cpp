57f6d595de4f515dcf0b735033c232bd5340a139
yusiwen <3907363360@qq.com>
2026-04-19 09:03:44 +0000

    2026年4月19日 17:03

--- acc_拓扑排序_E.cpp
+++ acc_拓扑排序_E.cpp
@@ -3,28 +3,44 @@
 using namespace std;
 #define el '\n'
 const ll N=1e5+5;
-ll ctoll(char c){
-    return (c-'a'+1);
-}
-void sort(ll n){
-    for(int i=1;i<=n;i++){
-        
-    }
+char no(){
+    cout<<0;
+    exit(0);
 }
 ll n;
 ll in[N];
 string s,last,t;
 vector<ll> g[N];
+ll dep[N];
+ll ctoll(char c){
+    return (c-'a'+1);
+}
+void sort(){
+    queue<ll> q;
+    for(int i=1;i<=26;i++){
+        if(in[i]==0){
+            q.push(i);
+        }
+    }
+    while(!q.empty()){
+        if(q.size()>=2) no();
+        ll x=q.front();
+        q.pop();
+        for(auto i:g[x]){
+            if(!--in[i]){
+                q.push(i);
+                dep[i]=dep[x]+1;
+            }
+        }       
+    }
+}
 void solve(){
     cin>>n;
     cin>>last;
     for(int i=1;i<=26;i++) in[i]=-1;
     for(int i=2;i<=n;i++){
         cin>>s;
-        if(s<last){
-            cout<<0;
-            return;
-        }
+        if(s<last) no();
         for(ll i=0;i<last.size();i++){
             if(s[i]>last[i]){
                 g[ctoll(last[i])].push_back(ctoll(s[i]));
@@ -36,6 +52,10 @@ void solve(){
         last=s;
     }
     cin>>t;
+    for(auto i:t) if(in[ctoll(i)]==-1) no();
+    for(auto i:t){
+        cout<<char(dep[ctoll(t[i])]-'a'+1);
+    }
 }
 int main(){
     ios::sync_with_stdio(0);
