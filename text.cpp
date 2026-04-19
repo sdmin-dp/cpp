08a64d160bbe29a65ed90e00c24abaa8666afd07
yusiwen <3907363360@qq.com>
2026-04-19 07:29:14 +0000

    2026年4月19日 15:29

--- text.cpp
+++ text.cpp
@@ -1,61 +1,19 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+#define ll long long
 using namespace std;
-#define ll long long 
 #define el '\n'
-const ll N=1e4+5;
-bool isprime[N];
-ll dist[N];
-void sieve(){
-    isprime[0]=isprime[1]=1;
-    for(ll i=1;i<=N;i++){
-        if(!isprime[i]){
-            for(ll j=2;i*j<=N;j++){
-                isprime[i*j]=1;
-            }
-        }
-    }
-}
-ll bfs(ll b,ll e) {
-    memset(dist,-1,sizeof dist);
-    queue<ll> q;
-    q.push(b);
-    dist[b]=0;
-    while(!q.empty()){
-        ll t=q.front();
-        q.pop();
-        if(t==e) return dist[t];
-        ll p[4];
-        p[0]=t/1000;
-        p[1]=t/100%10;
-        p[2]=t/10%10;
-        p[3]=t%10;
-        for(ll i=0;i<4;i++){
-            ll old=p[i];
-            for(ll j=0;j<=9;j++) {
-                if (i==0&&j==0) continue;
-                p[i]=j;
-                ll next_num=p[0]*1000+p[1]*100+p[2]*10+p[3];
-                if (!isprime[next_num]&&dist[next_num]==-1) {
-                    dist[next_num]=dist[t]+1;
-                    q.push(next_num);
-                }
-            }
-            p[i]=old;
-        }
-    }
-    return -1;
-}
+const ll N=1e5+5;
+
 void solve(){
-    ll a,b;
-    cin>>a>>b;
-    ll res=bfs(a,b);
-    if(res==-1) cout<<"Impossible"<<el;
-    else cout<<res<<el;
+    
 }
-int main() {
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    //freopen("xxx.in","r",stdin);
+    //freopen("xxx.out","w",stdout);
     ll T=1;
-    cin>>T;
-    sieve();
+    //cin>>T;
     while(T--){
         solve();
     }
