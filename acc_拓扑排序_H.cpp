3a2f08c011e5b18ef43bb22dea57d8bb2106e521
yusiwen <3907363360@qq.com>
2026-04-19 09:58:27 +0000

    2026年4月19日 17:58

--- acc_拓扑排序_H.cpp
+++ acc_拓扑排序_H.cpp
@@ -3,33 +3,9 @@
 using namespace std;
 #define el '\n'
 const ll N=1e5+5;
-ll n,m;
-vector<pair<ll,ll>> g[N];
-ll in[N];
-void bfs(){
-    queue<pair<ll,ll>> q;
-    for(int i=1;i<=n;i++) if(!in[i]) q.push({i,0});
-    ll ans=0;
-    while(!q.empty()){
-        auto x=q.front();
-        ll u=x.first,step=x.second;
-        ans=max(ans,step);
-        for(auto i:g[u]){
-            ll v=i.first,w=i.second;
-            q.push({v,step+w});
-        }
-    }
-    cout<<ans;
-}
+
 void solve(){
-    cin>>n>>m;
-    for(int i=1;i<=m;i++){
-        ll u,v,w;
-        cin>>u>>v>>w;
-        g[u].push_back({v,w});
-        in[v]++;
-    }
-    bfs();
+    
 }
 int main(){
     ios::sync_with_stdio(0);
