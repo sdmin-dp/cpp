5ad0e427c5356f3e0b1672eae55318a8bd52dd17
yusiwen <3907363360@qq.com>
2026-04-19 09:22:39 +0000

    2026年4月19日 17:22

--- text.cpp
+++ text.cpp
@@ -3,18 +3,9 @@
 using namespace std;
 #define el '\n'
 const ll N=1e5+5;
-ll n,k,s,ans;
+
 void solve(){
-    cin>>n>>k;
-    char c,last;
-    cin>>last;
-    for(int i=2;i<=n;i++){
-        cin>>c;
-        if(c!=last) s++;
-        last=c;
-    }
-    ans=n-1-s+(s%2&&s/2<k)+min(s/2,k)*2;
-    cout<<ans;
+    
 }
 int main(){
     ios::sync_with_stdio(0);
