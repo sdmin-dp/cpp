25f50be85793968ed7c267a85a5deaa57d61511a
yusiwen <3907363360@qq.com>
2026-04-20 03:46:39 +0000

    2026年4月20日 11:46

--- P8436.cpp
+++ P8436.cpp
@@ -50,8 +50,13 @@ void solve(){
             change(i);
         }
     }
-    for(int i=1;i<=n;i++){
+    for(int i=1;i<=idx;i++){
         
+        for(int j=1;j<=n;j++){
+            if(color[j]==i){
+
+            }
+        }
     }
 }
 int main(){
