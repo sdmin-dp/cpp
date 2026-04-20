55ee8e5709b3ec20cf5f9d76a112638bd58cee49
yusiwen <3907363360@qq.com>
2026-04-20 03:05:24 +0000

    2026年4月20日 11:05

--- 模板及重要代码/tarjan求点双连通分量.cpp
+++ 模板及重要代码/tarjan求点双连通分量.cpp
@@ -5,50 +5,93 @@
 
 using namespace std;
 
-const int MAXN = 100005;
-vector<int> adj[MAXN];      // 邻接表
+const int MAXN = 500005; // 假设原图有 N 个点
+// 圆方树里，总点数 = 原图圆点数 + 新造出来的方点数
+// 最坏情况下，方点数也能达到 N 个，所以数组开 2 倍！
+vector<int> adj[MAXN];      // 原图邻接表
+vector<int> tree[MAXN * 2]; // 缩点后的“圆方树”邻接表
 int dfn[MAXN], low[MAXN], timer;
-stack<pair<int, int>> st;   // 存储边的栈，用于提取点双
-vector<vector<int>> bccs;   // 存储最终找出的所有点双
+stack<int> st;
+int n, m, square_cnt; // square_cnt 记录方点编号
 
-void tarjan(int u, int p = -1) {
+void tarjan(int u) {
     dfn[u] = low[u] = ++timer;
-    
+    st.push(u);
+
     for (int v : adj[u]) {
-        if (v == p) continue; // 不走回头路去父节点
-
-        if (!dfn[v]) { // 如果 v 没被访问过（树枝边）
-            st.push({u, v}); // 把这条边压入栈
-            tarjan(v, u);
-            low[u] = min(low[u], low[v]); // 更新能回溯到的最小值
-
-            // 【关键判定】：说明 u 是割点或其上方的连接点
+        if (!dfn[v]) {
+            tarjan(v);
+            low[u] = min(low[u], low[v]);
+            
+            // 找到了一个点双！
             if (low[v] >= dfn[u]) {
-                vector<int> bcc;
-                // 从栈中弹出边，直到弹出 (u, v) 为止
+                // 1. 每发现一个点双，就新造一个“方点”
+                // 方点的编号从 n+1 开始往后排
+                square_cnt++; 
+                int cur_square = n + square_cnt; 
+
+                // 2. 把栈里的点（这个点双里的成员）拿出来连向方点
                 while (true) {
-                    pair<int, int> edge = st.top();
+                    int node = st.top();
                     st.pop();
-                    bcc.push_back(edge.first);
-                    bcc.push_back(edge.second);
-                    if (edge.first == u && edge.second == v) break;
+                    // 圆点 node 与 方点 cur_square 连边
+                    tree[node].push_back(cur_square);
+                    tree[cur_square].push_back(node);
+                    if (node == v) break;
                 }
-                // 去重：因为一个点会在多条边中出现
-                sort(bcc.begin(), bcc.end());
-                bcc.erase(unique(bcc.begin(), bcc.end()), bcc.end());
-                bccs.push_back(bcc);
+                
+                // 3. 特别注意：u 也是这个点双的一员，也要和方点连边
+                // 但 u 不能弹出，因为它可能还要和别的方点连边
+                tree[u].push_back(cur_square);
+                tree[cur_square].push_back(u);
             }
-        } 
-        else if (dfn[v] < dfn[u]) { // 如果 v 已经访问过（回边/返祖边）
-            st.push({u, v});
-            low[u] = min(low[u], dfn[v]); // 更新 low 值
+        } else {
+            low[u] = min(low[u], dfn[v]);
         }
     }
 }
 
 int main() {
-    // 假设有 n 个点，m 条边...
-    // 遍历所有点防止图不连通
-    // if (!dfn[i]) tarjan(i);
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    cin >> n >> m;
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+        if (u == v) continue;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    // 初始化方点计数，原图有 n 个点，方点从 n+1 开始
+    square_cnt = 0;
+
+    for (int i = 1; i <= n; i++) {
+        if (!dfn[i]) {
+            // 处理孤立点的情况：
+            // 如果这个点没边，Tarjan 进去直接就退出了
+            // 但在圆方树里，它通常作为一个独立的圆点存在
+            tarjan(i);
+            // 弹掉根节点（可选，为了逻辑严密）
+            if (!st.empty()) st.pop(); 
+        }
+    }
+
+    // 此时圆方树已经建好了！
+    // 树的总节点数是 n + square_cnt
+    cout << "原图点数: " << n << "，缩点后方点数: " << square_cnt << endl;
+    cout << "缩点后的圆方树连边情况如下：" << endl;
+    for (int i = 1; i <= n + square_cnt; i++) {
+        for (int neighbor : tree[i]) {
+            // 因为是无向边，为了不重复打印，只打印从小到大的
+            if (i < neighbor) {
+                // 打印格式：(圆/方)点 -> (圆/方)点
+                cout << (i <= n ? "圆" : "方") << i << " -- " 
+                     << (neighbor <= n ? "圆" : "方") << neighbor << endl;
+            }
+        }
+    }
+
     return 0;
 }
