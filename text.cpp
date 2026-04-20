a686610a4e5166d5ae8232f8a010ae243ffff7b8
yusiwen <3907363360@qq.com>
2026-04-20 07:34:29 +0000

    2026年4月20日 15:34

--- text.cpp
+++ text.cpp
@@ -3,20 +3,22 @@
 using namespace std;
 #define el '\n'
 const ll N=1e5+5;
-
 void solve(){
     ll n;
     cin>>n;
     for(int i=2;i<=n;i++){
+        if(n%i!=0) continue;
         for(int j=i+1;j<=n;j++){
-            ll k=n/i/j;
-            if(i*j*k==n){
-                cout<<i<<" "<<j<<" "<<k<<el;
-                return;
+            if(n%(i*j)==0){
+                ll k=n/(i*j);
+                if(i!=j&&j!=k&&i!=k){
+                    cout<<i<<" "<<j<<" "<<k<<el;
+                    return;
+                }
             }
         }
     }
-    cout<<"NO"<<el;
+    cout<<"NO";
 }
 int main(){
     ios::sync_with_stdio(0);
