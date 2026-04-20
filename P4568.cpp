4d4ea878fa04e14ae2ae7c99b754777fd563a67d
yusiwen <3907363360@qq.com>
2026-04-20 02:07:19 +0000

    2026年4月20日 10:07

--- P4568.cpp
+++ P4568.cpp
@@ -2,24 +2,44 @@
 #define ll long long
 using namespace std;
 #define el '\n'
-const ll N=1e4+5;
+const ll N=1e5+5;
 ll n,m,k;
-ll b,e;
 vector<pair<ll,ll>> g[N];
+ll dis[N][20];
+ll b,e;
 void dijkstra(){
     priority_queue<pair<ll,ll>,vector<pair<ll,ll>>,greater<pair<ll,ll>>> q;
-    
-}
-void solve(){
-    cin>>n>>m>>k>>b>>e;
-    for(int i=1;i<=n;i++){
-        ll u,v,w;
-        cin>>u>>v>>w;
-        g[u+1].push_back({v+1,w});
-        g[v+1].push_back({u+1,w});
+    q.push({0,b});
+    memset(dis,0x3f,sizeof(dis));
+    for(ll i=1;i<=k;i++) dis[1][0]=0;
+    while(!q.empty()){
+        auto x=q.top();
+        q.pop();
+        for(int i=1;i<=k;i++){
+            if(x.first>dis[x.second]) continue;
+            for(auto i:g[x.second]){
+                if(dis[i.first]>x.second+i.second){
+                    dis[i.first]=x.second+i.second;
+                    q.push({i.first,dis[i.first]});
+                }
+            }
+        }
+        
     }
 }
-int main(){
+void solve(){
+    cin>>n>>m>>k;
+    for(int i=1;i<=m;i++){
+        ll u,v,w;
+        cin>>u>>v>>w;
+        g[u].push_back({v,w});
+        g[v].push_back({u,w});
+    }
+    dijkstra();
+    
+}
+
+int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
     //freopen("xxx.in","r",stdin);
