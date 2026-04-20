61c5d7c7daefe7ddb7142bc01607919892c0c396
yusiwen <3907363360@qq.com>
2026-04-20 04:13:41 +0000

    2026年4月20日 12:13

--- code文件/dev-text.cpp
+++ code文件/dev-text.cpp
@@ -2,68 +2,37 @@
 #define ll long long
 using namespace std;
 #define el '\n'
-const ll N=1e5+5;
-char no(){
-    cout<<0;
-    exit(0);
-}
-ll n;
-ll in[N];
-string s,last,t;
+const ll N=1e3+5;
+ll n1,n2,m;
 vector<ll> g[N];
-ll dep[N];
-ll ctoll(char c){
-    return (c-'a'+1);
-}
-void sort(){
-    queue<ll> q;
-    for(int i=1;i<=26;i++){
-        if(in[i]==0){
-            q.push(i);
-        }
-    }
-    while(!q.empty()){
-        if(q.size()>=2) no();
-        ll x=q.front();
-        q.pop();
-        for(auto i:g[x]){
-            if(!--in[i]){
-                q.push(i);
-                dep[i]=dep[x]+1;
-            }
-        }       
-    }
+ll match[N];
+ll vis[N];
+bool dfs(ll x){
+	for(auto i:g[x]){
+		if(!vis[i]){
+			vis[i]=1;
+			if(!match[i]||dfs(match[i])){
+				match[i]=x;
+				return 1;
+			}
+		}
+	}
+	return 0;
 }
 void solve(){
-    cin>>n;
-    cin>>last;
-    for(int i=1;i<=26;i++) in[i]=-1;
-    for(int i=2;i<=n;i++){
-        cin>>s;
-        for(ll i=0;i<last.size();i++){
-            if(s[i]!=last[i]){
-                g[ctoll(last[i])].push_back(ctoll(s[i]));
-                if(in[ctoll(last[i])]==-1) in[ctoll(last[i])]=0;
-                if(in[ctoll(s[i])]==-1) in[ctoll(s[i])]=0;
-                in[ctoll(s[i])]++;
-                break;
-            }
-        }
-        last=s;
+    cin>>n1>>n2>>m;
+    for(int i=1;i<=m;i++){
+        ll x,y;
+        y+=n1;
+        g[x].push_back(y);
+        g[y].push_back(x);
     }
-    // for(int i=1;i<=26;i++){
-    //     cerr<<char(i-1+'a')<<":";
-    //     for(auto j:g[i]){
-    //         cerr<<char(j-1+'a')<<" ";
-    //     }
-    //     cerr<<el;
-    // }
-    sort();
-    cin>>t;
-    for(auto i:t) if(in[ctoll(i)]==-1) no();
-    for(auto i:t){
-        cout<<char(dep[ctoll(t[i])]-'a'+1);
-    }
+    ll cnt=0;
+    for(int i=1;i<=n1;i++){
+		memset(vis,0,sizeof(vis));
+		if(dfs(i)) cnt++;
+	}
+	cout<<cnt;
 }
 int main(){
     ios::sync_with_stdio(0);
