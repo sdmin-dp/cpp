26eea5620dac1f784ce852b724ef5fa4833d00e0
yusiwen <3907363360@qq.com>
2026-04-20 08:30:12 +0000

    2026年4月20日 16:30

--- text.cpp
+++ text.cpp
@@ -4,9 +4,13 @@ using namespace std;
 #define el '\n'
 const ll N=1e5+5;
 ll n;
-
+ll a[N];
 void solve(){
-    
+    cin>>n;
+    for(int i=1;i<=n;i++) cin>>a[i];
+    for(int i=1;i<=n;i++){
+        
+    }
 }
 int main(){
     ios::sync_with_stdio(0);
