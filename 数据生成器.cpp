a3070c287049742338813b870bd212d5768ef699
yusiwen <3907363360@qq.com>
2026-04-20 07:22:30 +0000

    2026年4月20日 15:22

--- 数据生成器.cpp
+++ 数据生成器.cpp
@@ -3,19 +3,14 @@
 using namespace std;
 #define el '\n'
 const ll N=1e5+5;
-string ans;
-void solve(){
-    ll n=rand()%10;
-    ll m=rand()%10;
-    cout<<n<<" "<<m<<el;
-    for(int i=1;i<=m;i++){
-        ll x=rand()%n,y=rand()%n,w=rand()%20;
-        if(x==0) x=n;
-        if(y==0) y=n;
-        if(y==x) y=x-1;
-        cout<<x<<" "<<y<<" "<<w<<el;
-    }
 
+void solve(){
+    ll t=rand()%101;
+    cout<<t<<el;
+    for(int i=1;i<=t;i++){
+        ll n=rand()%1000000001;
+        cout<<n<<el;
+    }
 }
 int main(){
     ios::sync_with_stdio(0);
