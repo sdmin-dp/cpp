3f802ecebcbe69f31c20440dd2490c8c2f8ceae8
yusiwen <3907363360@qq.com>
2026-04-20 06:24:23 +0000

    2026年4月20日 14:24

--- 逻辑城堡1月div3/B.cpp
+++ 逻辑城堡1月div3/B.cpp
@@ -4,9 +4,23 @@ using namespace std;
 #define el '\n'
 const ll N=1e5+5;
 ll n;
+ll a[N];
 void solve(){
     cin>>n;
-    
+    for(int i=1;a[i-1]*2<=n;i++){
+        a[i]=a[i-1]*2;
+    }
+    cout<<n<<"=";
+    bool flag=1;
+    for(int i=1;i<=n;i++){
+        if(n>=a[i]){
+            if(flag){cout<<a[i];flag=0;}
+            else cout<<'+'<<a[i];
+        }
+        else{
+            return;
+        }
+    }
 }
 int main(){
     ios::sync_with_stdio(0);
