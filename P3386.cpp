9add4a803ee13444f64e957ea98c72cdbf37ee76
yusiwen <3907363360@qq.com>
2026-04-20 04:11:52 +0000

    2026年4月20日 12:11

--- P3386.cpp
+++ P3386.cpp
@@ -8,14 +8,26 @@ vector<ll> g[N];
 ll match[N];
 ll vis[N];
 bool dfs(ll x){
-    
+	for(auto i:g[x]){
+		if(!vis[i]){
+			vis[i]=1;
+			if(!match[i]||dfs(match[i])){
+				match[i]=x;
+				return 1;
+			}
+		}
+	}
+	return 0;
 }
 void solve(){
     cin>>n1>>n2>>m;
     for(int i=1;i<=m;i++){
         ll x,y;
+        y+=n1;
         g[x].push_back(y);
+        g[y].push_back(x);
     }
+    dfs();
 }
 int main(){
     ios::sync_with_stdio(0);
