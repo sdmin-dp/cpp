8fa84876453f48faab1ee0475ebd517b92fff6cb
yusiwen <3907363360@qq.com>
2026-04-20 06:44:21 +0000

    2026年4月20日 14:44

---
+++ 逻辑城堡1月div3/C.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+#define el '\n'
+const ll N=1e5+5;
+ll n;
+vector<ll> prime(){
+    vector<ll> ans;
+    for(ll i=2;i*i<=n;i++){
+        while(n%i==0){
+            ans.push_back(i);
+            n/=i;
+        }
+    }
+    return ans;
+}
+void solve(){
+    cin>>n;
+    vector<ll> ans=prime();
+    if(ans.size()<=2){
+        cout<<"NO";
+        return;
+    } 
+    
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    //freopen("xxx.in","r",stdin);
+    //freopen("xxx.out","w",stdout);
+    ll T=1;
+    cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
