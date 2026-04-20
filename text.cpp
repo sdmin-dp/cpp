5656dbe65a85f8790c6d6159163bc531fec8810b
yusiwen <3907363360@qq.com>
2026-04-20 07:13:25 +0000

    2026年4月20日 15:13

--- text.cpp
+++ text.cpp
@@ -3,10 +3,14 @@
 using namespace std;
 #define el '\n'
 const ll N=1e5+5;
-
+bool isprime(ll x){
+    if(x<2) return 0;
+    for(int i=2;i*i<=x;i++) if(x%i==0) return 0;
+    return 1;
+}
 void solve(){
-    for(int i=1;i<=100;i++){
-        cout<<1000000000<<el;
+    for(int i=1000000000;i>=1;i--){
+        
     }
 }
 int main(){
