91f9cb38cd19ef421fe4a58c2b44d75f5437cc23
yusiwen <3907363360@qq.com>
2026-04-20 07:29:52 +0000

    2026年4月20日 15:29

--- text.cpp
+++ text.cpp
@@ -9,11 +9,10 @@ void solve(){
     cin>>n;
     for(int i=2;i<=n;i++){
         for(int j=i+1;j<=n;j++){
-            for(int k=j+1;k<=n;k++){
-                if(i*j*k==n){
-                    cout<<i<<" "<<j<<" "<<k<<el;
-                    return;
-                }
+            k=n/i/j;
+            if(i*j*k==n){
+                cout<<i<<" "<<j<<" "<<k<<el;
+                return;
             }
         }
     }
