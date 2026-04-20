c4d1fdc914fb07bf62432d473a01feeff0883394
yusiwen <3907363360@qq.com>
2026-04-20 07:10:00 +0000

    2026年4月20日 15:10

--- 逻辑城堡1月div3/C.cpp
+++ 逻辑城堡1月div3/C.cpp
@@ -16,7 +16,7 @@ void solve(){
             cerr<<"dog";
             ll j=find(n/i);
             if(j==0) continue;
-            cout<<i<<" "<<j<<" "<<(n/i)/j;
+            cout<<i<<" "<<j<<" "<<(n/i)/j<<el;
             return;
         }
     }
