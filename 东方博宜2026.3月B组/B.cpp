164469b47901f73a5e721e68ac9dfb868286d4b7
yusiwen <3907363360@qq.com>
2026-04-21 06:47:28 +0000

    2026年4月21日 14:47

--- 东方博宜2026.3月B组/B.cpp
+++ 东方博宜2026.3月B组/B.cpp
@@ -4,28 +4,19 @@ using namespace std;
 #define el '\n'
 const ll N=1e5+5;
 ll n;
-priority_queue<pair<ll,ll>,vector<pair<ll,ll>>,greater<pair<ll,ll>>> q;
+pair<ll,ll> a[N];
+bool cmp(pair<ll,ll> x,pair<ll,ll> y){
+    return (x.second-x.first)<(y.second-y.first);
+}
 void solve(){
     cin>>n;
+    for(int i=1;i<=n;i++) cin>>a[i].first>>a[i].second;
+    sort(a+1,a+n+1,cmp);
+    ll cnt=0,time=0;
     for(int i=1;i<=n;i++){
-        ll x,y;cin>>x>>y;
-        if(x<=y){
-            q.push({y-x,x});
-        }
-    }
-    ll cnt=0;
-    ll time=0;
-    while(!q.empty()){
-        auto x=q.top();
-        q.pop();
+        if(a[i].first>a[i].second) continue;
         
-        if(time<=x.first){
-            cnt++;
-            time+=x.second;
-            cerr<<x.first<<" "<<x.second<<" "<<time<<el;
-        }
     }
-    cout<<cnt;
 }
 int main(){
     ios::sync_with_stdio(0);
