44504eef4a178a570856517cb6e71e7968ff1716
yusiwen <3907363360@qq.com>
2026-04-21 06:34:47 +0000

    2026年4月21日 14:34

--- 东方博宜2026.3月B组/B.cpp
+++ 东方博宜2026.3月B组/B.cpp
@@ -4,16 +4,27 @@ using namespace std;
 #define el '\n'
 const ll N=1e5+5;
 ll n;
-pair<ll,ll> a[N];
 vector<ll> v;
+priority_queue<ll,vector<ll>,greater<ll>> q;
 void solve(){
     cin>>n;
     for(int i=1;i<=n;i++){
-        cin>>a[i].first>>a[i].second;
-        v.push_back(a[i].second-a[i].first);
+        ll x,y;cin>>x>>y;
+        if(x<=y){
+            v.push_back(x);
+            q.push(y-x);
+        }
     }
-    sort(v.begin(),v.end());
-    for(auto i:v) cout<<i<<" ";
+    ll cnt=0;
+    ll time=0;
+    while(!q.empty()){
+        ll x=q.top();
+        q.pop();
+        if(time<x){
+            cnt++;
+            
+        }
+    }
 }
 int main(){
     ios::sync_with_stdio(0);
