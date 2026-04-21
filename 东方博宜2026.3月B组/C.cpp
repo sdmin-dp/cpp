a205776ff0f296cd4527af060cd8d27c535466c2
yusiwen <3907363360@qq.com>
2026-04-21 08:50:22 +0000

    2026年4月21日 16:50

--- 东方博宜2026.3月B组/C.cpp
+++ 东方博宜2026.3月B组/C.cpp
@@ -26,7 +26,7 @@ void solve(){
             }
         }
         if(!f){
-            if(ans%s.size()!=0) ans=ans%s.size();
+            if(ans%s.size()!=0) ans=(ans/s.size()+1)*s.size();
             ans+=mp[i][0];
         }
     }
