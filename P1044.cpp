a3cc62900716bbd876f851f518a1f62cadb6094e
yusiwen2014 <3907363360@qq.com>
2026-04-23 06:31:34 +0000

    A54014:31113-34PMve: {23PM14:31e}

--- P1044.cpp
+++ P1044.cpp
@@ -10,7 +10,7 @@ void solve(){
     dp[0]=dp[1]=1;
     for(int i=2;i<=n;i++){
         for(int j=1;j<=i;j++){
-            dp[i]+=dp[j-1]+dp[i-j];
+            dp[i]+=dp[j-1]*dp[i-j];
         }
     }
     cout<<dp[n];
