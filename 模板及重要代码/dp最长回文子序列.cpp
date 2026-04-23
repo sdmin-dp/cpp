8a5f7bce98fff99df798f7d66cfef8b35a20a40f
yusiwen2014 <3907363360@qq.com>
2026-04-23 08:03:00 +0000

    A83316:03113-0PMve: {23PM16:03e}

---
+++ 模板及重要代码/dp最长回文子序列.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+#define el '\n'
+const ll N=2e3+5;
+ll n;
+ll a[N];
+ll dp[N][N];
+void solve(){
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        cin>>a[i];
+        dp[i][i]=1;
+    }
+    for(int len=1;len<=n;len++){
+        for(int i=n-len+1;i>=1;i--){
+            ll j=i+len-1;
+            dp[i][j]=min(dp[i+1][j],dp[i][j-1]);
+            
+        }
+    }
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    //freopen("xxx.in","r",stdin);
+    //freopen("xxx.out","w",stdout);
+    ll T=1;
+    //cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
