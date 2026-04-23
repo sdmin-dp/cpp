e1af78dc53824ab05261e03878e16ce02e0e0cbf
yusiwen2014 <3907363360@qq.com>
2026-04-23 06:40:05 +0000

    A91214:40113-5PMve: {23PM14:40e}

--- P1115.cpp
+++ P1115.cpp
@@ -11,7 +11,7 @@ void solve(){
     for(int i=1;i<=n;i++) cin>>a[i];
     dp[1]=a[1];
     for(int i=2;i<=n;i++){
-        if(dp[i-1]+a[i]<=0) dp[i]=dp[i-1];
+        if(dp[i-1]+a[i]<=0) dp[i]=0;
         else dp[i]=dp[i-1]+a[i];
     }
     for(int i=1;i<=n;i++) cerr<<dp[i]<<" ";
