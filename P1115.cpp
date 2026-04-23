63c4b0361122ab0e015d307136bab6599089f685
yusiwen2014 <3907363360@qq.com>
2026-04-23 06:37:30 +0000

    A47514:37113-30PMve: {23PM14:37e}

--- P1115.cpp
+++ P1115.cpp
@@ -2,12 +2,19 @@
 #define ll long long
 using namespace std;
 #define el '\n'
-const ll N=1e5+5;
+const ll N=2e5+5;
 ll n;
 ll a[N];
+ll dp[N];
 void solve(){
     cin>>n;
-    
+    for(int i=1;i<=n;i++) cin>>a[i];
+    dp[1]=a[1];
+    for(int i=2;i<=n;i++){
+        if(dp[i-1]+a[i]<0) dp[i]=dp[i-1];
+        else dp[i]=dp[i-1]+a[i];
+    }
+    cout<<dp[n];
 }
 int main(){
     ios::sync_with_stdio(0);
