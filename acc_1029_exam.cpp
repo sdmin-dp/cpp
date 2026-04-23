9427a522ebfede767b0086345d57b69db71aec6b
yusiwen2014 <3907363360@qq.com>
2026-04-23 05:54:52 +0000

    A72413:54113-52PMve: {23PM13:54e}

--- acc_1029_exam.cpp
+++ acc_1029_exam.cpp
@@ -4,25 +4,51 @@ using namespace std;
 #define el '\n'
 const ll N=1e5+5;
 ll n;
+set<ll>s;
 unordered_map<ll,ll> mp;
 unordered_map<ll,ll> remp;
 pair<ll,ll> a[N];
-vector<ll> sorted;
 vector<ll> g[N];
+ll vis[N];
+ll match[N];
+bool dfs(ll u,ll id){
+    for(auto i:g[u]){
+        if(vis[i-2*n]!=id){
+            vis[i-2*n]=id;
+            if(!match[i-2*n]||dfs(match[i-2*n],id)){
+                match[i-2*n]=u;
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
 void solve(){
     cin>>n;
     for(int i=1;i<=n;i++){
         cin>>a[i].first>>a[i].second;
-        sorted.push_back(a[i].first);
-        sorted.push_back(a[i].second);
+        s.insert(a[i].first);
+        s.insert(a[i].second);
     }
-    sort(sorted.begin(),sorted.end());
-    for(){
-
+    
+    ll I=0;
+    for(auto &j:s){
+        remp[++I]=j;
+        mp[j]=I;
     }
     for(int i=1;i<=n;i++){
-        
+        g[mp[a[i].first]].push_back(i+2*n);
+        g[mp[a[i].second]].push_back(i+2*n);
     }
+    ll cnt=0;
+    for(int i=1;i<=s.size();i++){
+        if(dfs(i,i)) cnt++;
+        if(cnt==n){
+            cout<<remp[i];
+            return;
+        }
+    }
+    cout<<-1;
 }
 int main(){
     ios::sync_with_stdio(0);
