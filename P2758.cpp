45c85a856aef82678745b419444ad0ae33c5848f
yusiwen2014 <3907363360@qq.com>
2026-04-23 08:30:38 +0000

    A60516:30113-38PMve: {23PM16:30e}

--- P2758.cpp
+++ P2758.cpp
@@ -2,18 +2,22 @@
 #define ll long long
 using namespace std;
 #define el '\n'
-const ll N=1e5+5;
+const ll N=2e3+5;
 string s,t;
 ll dp[N][N];
 void solve(){
     cin>>s>>t;
     s=' '+s;
     t=' '+t;
+    for(int i=1;i<s.size();i++) dp[0][i]=i;
+    for(int i=1;i<t.size();i++) dp[i][0]=i;
     for(int i=1;i<s.size();i++){
         for(int j=1;j<=t.size();j++){
-            if(s[i]==t[j])  
+            if(s[i]==t[j]) dp[i][j]=dp[i-1][j-1];
+            else dp[i][j]=min({dp[i-1][j-1],dp[i][j-1],dp[i-1][j]})+1;
         }
     }
+    cout<<dp[s.size()-1][t.size()-1];
 }
 int main(){
     ios::sync_with_stdio(0);
