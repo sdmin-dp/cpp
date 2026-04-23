6c158bd4a944507395e2cad47a81f575b8609d85
yusiwen2014 <3907363360@qq.com>
2026-04-23 09:00:54 +0000

    A82817:00113-54PMve: {23PM17:00e}

--- P1040.cpp
+++ P1040.cpp
@@ -6,12 +6,13 @@ const ll N=30+5;
 ll n;
 ll dis[N];
 ll dp[N][N];
-ll root[N][N];
+ll rot[N][N];
+ll root;
 void solve(){
     cin>>n;
     for(int i=1;i<=n;i++){
         cin>>dis[i];
-        root[i][i]=i;
+        rot[i][i]=i;
         dp[i][i]=dis[i];
     }
     for(int len=2;len<=n;len++){
@@ -19,13 +20,14 @@ void solve(){
             ll j=i+len-1;
             for(int k=i;k<=j;k++){
                 if(max(dp[i][k-1],1ll)*max(dp[k+1][j],1ll)+dis[k]>dp[i][j]){
-                    root[i][j]=k;
+                    rot[i][j]=k;
                     dp[i][j]=max(dp[i][k-1],1ll)*max(dp[k+1][j],1ll)+dis[k];
                 }
             }
         }
     }
-    cout<<dp[1][n];
+    cout<<dp[1][n]<<el;
+    root=rot[1][n];
 }
 int main(){
     ios::sync_with_stdio(0);
