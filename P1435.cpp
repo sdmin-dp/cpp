257651e688bae355574ab74809c538d125334859
yusiwen2014 <3907363360@qq.com>
2026-04-23 18:00:49 +0800

    new fille:   P1435.cpp 	new fie:   P14920.cpp

---
+++ P1435.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+#define el '\n'
+const ll N=1e3+5;
+ll n;
+string s;
+ll dp[N][N];
+void solve(){
+    cin>>s;
+    n=s.size();
+    s=' '+s;
+    for(int len=2;len<=n;len++){
+        for(int i=1;i+len-1<=n;i++){
+            ll j=i+len-1;
+            if(s[i]==s[j]) dp[i][j]=dp[i+1][j-1];
+            else dp[i][j]=min(dp[i+1][j],dp[i][j-1])+1;
+        }
+    }
+    cout<<dp[1][n];
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    //freopen("xxx.in","r",stdin);
+    //freopen("xxx.out","w",stdout);
+    ll T=1;
+    //cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
---
+++ P14920.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+#define el '\n'
+const ll N=5e2+5;
+ll n,m;
+pair<ll,ll> a[N];
+ll dp[250005];
+void solve(){
+    cin>>n>>m;
+    for(int i=1;i<=n;i++) cin>>a[i].first>>a[i].second;
+    memset(dp,0x3f,sizeof(dp));
+    dp[0]=0;
+    for(int i=1;i<=n;i++)
+        for(int j=250000;j>=a[i].first;j--)
+            dp[j]=min(dp[j-a[i].first]+a[i].second,dp[j]);
+    for(int j=250000;j>=0;j--){
+        if(dp[j]<=m){
+            cout<<j;
+            return;
+        }
+    }
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    //freopen("xxx.in","r",stdin);
+    //freopen("xxx.out","w",stdout);
+    ll T=1;
+    //cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
