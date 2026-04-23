66dbc0b44204f22792c76562fc0d84c2a5bbb3a1
yusiwen2014 <3907363360@qq.com>
2026-04-23 08:28:37 +0000

    A14716:28113-37PMve: {23PM16:28e}

--- P2758.cpp
+++ P2758.cpp
@@ -3,9 +3,17 @@
 using namespace std;
 #define el '\n'
 const ll N=1e5+5;
-ll n;
+string s,t;
+ll dp[N][N];
 void solve(){
-    
+    cin>>s>>t;
+    s=' '+s;
+    t=' '+t;
+    for(int i=1;i<s.size();i++){
+        for(int j=1;j<=t.size();j++){
+            if(s[i]==t[j])  
+        }
+    }
 }
 int main(){
     ios::sync_with_stdio(0);
