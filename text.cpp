0e365d35cd169b65f6819e479bd615df375512b2
yusiwen <3907363360@qq.com>
2026-04-25 07:24:27 +0000

    2026年4月25日 15:24

--- text.cpp
+++ text.cpp
@@ -3,44 +3,11 @@ using namespace std;
 #define ll long long 
 #define el '\n'
 const ll N=1e5+5;
-ll n;
-map<ll,string> mp;
-bool isnum(char c){
-    return (c>='0'&&c<='9');
-}
-vector<pair<ll,ll>> v;
+
 void solve(){
-    cin>>n;
-    for(int i=1;i<=n;i++){
-        ll x;string s;
-        cin>>x;
-        cin>>s;
-        mp[x]=s;
-    }
     string s;
     getline(cin,s);
     cout<<s;
-    for(int i=0;i<s.size();i++){
-        if(!isnum(s[i])) continue;
-        ll x=0;
-        ll len=0;
-        while(i<s.size()&&isnum(s[i])){
-            x=x*10+int(s[i]-48);
-            i++;
-            len++;
-        }
-        v.push_back({x,len});
-    }
-    ll cnt=0;
-    for(int i=0;i<s.size();i++){
-        if(i=='#'){
-            cout<<v[cnt++].first;
-            i+=v[cnt].second;
-        }else{
-            cout<<s[i];
-        }
-    }
-    
 }
 int main(){
     ios::sync_with_stdio(0);
