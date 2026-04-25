4a46380648888fe262380593b7ff0af663a34dcd
yusiwen <3907363360@qq.com>
2026-04-25 07:35:45 +0000

    2026年4月25日 15:35

--- code文件/dev-text.cpp
+++ code文件/dev-text.cpp
@@ -14,37 +14,42 @@ void solve(){
     for(int i=1;i<=n;i++){
         ll x;string s;
         cin>>x;
-        cin>>s;
+        getline(cin,s);
         mp[x]=s;
     }
-    
     string s;
 //    cin>>s;
 	getline(cin,s);
 //    getline(cin,s);
-    cout<<s;
+//    cout<<s;
 //    cout<<"dog";
-//    for(int i=0;i<s.size();i++){
-//        if(!isnum(s[i])) continue;
-//        ll x=0;
-//        ll len=0;
-//        while(i<s.size()&&isnum(s[i])){
-//            x=x*10+int(s[i]-48);
-//            i++;
-//            len++;
-//        }
-//        v.push_back({x,len});
-//    }
-//    ll cnt=0;
-//    for(int i=0;i<s.size();i++){
-//        if(i=='#'){
-//            cout<<v[cnt++].first;
-//            i+=v[cnt].second;
-//        }else{
-//            cout<<s[i];
-//        }
-//    }
-    
+    for(int i=0;i<s.size();i++){
+//    	cout<<i<<" ";
+        if(!isnum(s[i])) continue;
+        ll x=0;
+        ll len=0;
+        while(i<s.size()&&isnum(s[i])){
+            x=x*10+int(s[i]-48);
+            i++;
+            len++;
+        }
+        v.push_back({x,len});
+    }
+//    for(auto i:v){
+//    	cout<<i.first<<" "<<i.second<<el;
+//	}
+    ll cnt=0;
+    for(int i=0;i<s.size();i++){
+    	
+        if(s[i]=='#'){
+//        	cnt++;
+            cout<<mp[v[cnt++].first];
+            i+=v[cnt].second+1;
+        }else{
+            cout<<s[i];
+        }
+        cerr<<i<<" ";
+    }
 }
 int main(){
     ios::sync_with_stdio(0);
