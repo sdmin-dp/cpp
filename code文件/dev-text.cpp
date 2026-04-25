b3231fde5d79aea92c66e03a4d6a432bf881f3c2
yusiwen <3907363360@qq.com>
2026-04-25 07:23:12 +0000

    2026年4月25日 15:23

--- code文件/dev-text.cpp
+++ code文件/dev-text.cpp
@@ -1,29 +1,57 @@
 #include<bits/stdc++.h>
-#define ll long long
 using namespace std;
+#define ll long long 
 #define el '\n'
 const ll N=1e5+5;
 ll n;
-pair<ll,ll> a[N];
-bool cmp(pair<ll,ll> x,pair<ll,ll> y){
-	return x.first*y.second<y.first*x.second;
+map<ll,string> mp;
+bool isnum(char c){
+    return (c>='0'&&c<='9');
 }
+vector<pair<ll,ll>> v;
 void solve(){
-	cin>>n;
-	for(int i=1;i<=n;i++) cin>>a[i].first>>a[i].second;
-	sort(a+1,a+n+1,cmp);
-	ll time=0,sum=0;
-	for(int i=1;i<=n;i++){
-		time+=a[i].first;
-		sum+=time*a[i].second;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        ll x;string s;
+        cin>>x;
+        cin>>s;
+        mp[x]=s;
+    }
+    
+    string s;
+//    cin>>s;
+	char c;
+	while(c=getchar()){
+		s.push_back(c);
 	}
-	cout<<sum;
+//    getline(cin,s);
+    cout<<s;
+    cout<<"dog";
+    for(int i=0;i<s.size();i++){
+        if(!isnum(s[i])) continue;
+        ll x=0;
+        ll len=0;
+        while(i<s.size()&&isnum(s[i])){
+            x=x*10+int(s[i]-48);
+            i++;
+            len++;
+        }
+        v.push_back({x,len});
+    }
+    ll cnt=0;
+//    for(int i=0;i<s.size();i++){
+//        if(i=='#'){
+//            cout<<v[cnt++].first;
+//            i+=v[cnt].second;
+//        }else{
+//            cout<<s[i];
+//        }
+//    }
+    
 }
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
-    //freopen("xxx.in","r",stdin);
-    //freopen("xxx.out","w",stdout);
     ll T=1;
     //cin>>T;
     while(T--){
@@ -31,4 +59,3 @@ int main(){
     }
     return 0;
 }
-
