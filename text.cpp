15d2584b3798ae70588626dd7198a1ab8f3ce239
yusiwen <3907363360@qq.com>
2026-04-25 06:05:49 +0000

    2026年4月25日 14:05

--- text.cpp
+++ text.cpp
@@ -1,28 +1,21 @@
 #include<bits/stdc++.h>
-#define ll long long
 using namespace std;
+#define ll long long 
 #define el '\n'
-const ll N=2e3+5;
-ll n,m;
-ll a[N],b[N];
-ll dp[N][N];
+const ll N=1e5+5;
+ll n;
 void solve(){
-    cin>>n>>m;
-    for(int i=1;i<=n;i++) cin>>a[i];
-    for(int i=1;i<=m;i++) cin>>b[i];
+    cin>>n;
+    ll ans=0;
     for(int i=1;i<=n;i++){
-        for(int j=1;j<=m;j++){
-            dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-            if(a[i]==b[j]) dp[i][j]=max(dp[i-1][j-1]+1,dp[i][j]);
-        }
+        ll x;cin>>x;
+        if(x%2026==0) ans++;
     }
-    cout<<dp[n][m];
+    cout<<ans;
 }
-int main(){
+int mian(){
     ios::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
-    //freopen("xxx.in","r",stdin);
-    //freopen("xxx.out","w",stdout);
     ll T=1;
     //cin>>T;
     while(T--){
