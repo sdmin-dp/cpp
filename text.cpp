6c5201666a770ab8f62e011dc040bd4e1a037f6e
yusiwen2014 <3907363360@qq.com>
2026-04-26 11:31:33 +0000

    A90819:31116-33PMve: {26PM19:31e}

--- text.cpp
+++ text.cpp
@@ -3,66 +3,18 @@
 using namespace std;
 #define el '\n'
 const ll N=1e5+5;
-ll A,B,C,a,b,c;
-ll cnt1,cnt2,id1,id2,mn1,mn2;
-unordered_set<ll> st;
-ll rehash(ll x){
-    ll p1=x/10000,p2=x%10000,p3=C-p1-p2;
-    return min({p1,p2,p3});
-}
+ll a,b,c,ax,bx,cx,an1,ans,mx,type=0;
 void solve(){
-    cin>>A>>B>>C;
-    a=b=0;c=C;
-    while(st.find(a*10000+b)==st.end()){
-        if(b==0){
-            if(c<B) b=c,c=0;
-            else if(c>=B) b=B,c-=B;
-        }else if(a==A){
-            c+=a;
-            a=0;
-        }else{
-            if(A-a>=b) a+=b,b=0;
-            else if(A-a<b) b-=(A-a),a=A;
-        }
-        st.insert(a*10000+b);
-    }
-    ll j=0;
-    for(auto i:st){
-        j++;
-        if(rehash(i)<mn1){
-            id1=j;
-            mn1=i;
+    cin>>a>>b>>c;
+    cx=mx=c;
+    while(type<2){
+        if(type==0){
+            ll k=min(b,cx);
+            bx
         }
     }
-    swap(A,B);
-    a=0,b=0,c=C;
-    st.clear();
-    while(st.find(a*10000+b)==st.end()){
-        if(b==0){
-            if(c<B) b=c,c=0;
-            else if(c>=B) b=B,c-=B;
-        }else if(a==A){
-            c+=a;
-            a=0;
-        }else{
-            if(A-a>=b) a+=b,b=0;
-            else if(A-a<b) b-=(A-a),a=A;
-        }
-        st.insert(a*10000+b);
-    }
-    j=0;
-    for(auto i:st){
-        j++;
-        if(rehash(i)<mn2){
-            id2=j;
-            mn2=i;
-        }
-    }
-    if(mn1<mn2) cout<<mn1<<el<<id1;
-    else if(mn1>mn2) cout<<mn2<<" "<<id2;
-    else cout<<mn1<<" "<<min(id1,id2);
 }
-int main(){ 
+int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
     //freopen("xxx.in","r",stdin);
