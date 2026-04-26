d9fdd558a9a4011c91695611e413300b583c5404
yusiwen2014 <3907363360@qq.com>
2026-04-26 11:42:49 +0000

    A84019:42116-49PMve: {26PM19:42e}

--- 东方博宜2026.4 B组/C.cpp
+++ 东方博宜2026.4 B组/C.cpp
@@ -3,9 +3,24 @@
 using namespace std;
 #define el '\n'
 const ll N=1e5+5;
-ll a,b,c,ax=0,
+ll a,b,c,ax,bx,cx,ans1,ans,mx,type=0;
 void solve(){
-    
+    cin>>a>>b>>c;
+    cx=mx=c;
+    if(a==b&&a==c){
+        cout<<a<<"\n0";
+        return;
+    }
+    while(type<2){
+        if(type==0){
+            ll k=min(b,cx);
+            bx+=k;
+            cx-=k;
+            ans1++;
+            if(bx&&(bx<mx||bx==mx&&ans1<ans))
+            
+        }
+    }
 }
 int main(){
     ios::sync_with_stdio(0);
