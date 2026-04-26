3919e84083b72285549752169f3299486f648022
yusiwen2014 <3907363360@qq.com>
2026-04-26 07:31:12 +0000

    A26615:31116-12PMve: {26PM15:31e}

--- text.cpp
+++ text.cpp
@@ -6,7 +6,7 @@ const ll N=1e5+5;
 ll n,k;
 //m->厂家个数
 pair<ll,ll> a[N];
-priority_queue<pair<ll,ll>,vector<pair<ll,ll>>,greater<pair<ll,ll>>> q;
+set<pair<ll,ll>> st;
 //由于要排序,q的pair是个反的
 ll ans=0;
 ll vis[N];
@@ -23,34 +23,11 @@ void solve(){
     for(int i=1;i<=k;i++){
         ans+=a[i].second;
         if(!vis[a[i].first]) x++;
-        q.push({a[i].second,a[i].first});
         vis[a[i].first]++;
+        
     }
     ans+=x*x;
     ll res=ans;
-    for(int i=k+1;i<=n;i++){
-        if(vis[i]) continue;
-        auto t=q.top();
-        swap(t.first,t.second);
-        ll new_ans=ans-t.second;
-        cerr<<t.first<<t.second<<el;
-        if(vis[t.first]==1){
-            new_ans-=x*x;
-            x--;
-            new_ans+=x*x;
-        }
-        vis[t.first]--;
-        new_ans+=a[i].second;
-        vis[a[i].first]++;
-        new_ans-=x*x;
-        x++;
-        new_ans+=x*x;
-        q.pop();
-        q.push({a[i].second,a[i].first});
-        ans=new_ans;
-        res=max(res,ans);
-        // cerr<<res<<" "<<ans<<" "<<a[i].first<<" "<<a[i].second<<el;
-    }
     cout<<res;
 }
 int main(){
