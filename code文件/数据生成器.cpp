512b05efd65228be658fc1a4ce59761ea5acd186
yusiwen2014 <3907363360@qq.com>
2026-04-26 10:10:05 +0000

    A72918:10116-5PMve: {26PM18:10e}

--- code文件/数据生成器.cpp
+++ code文件/数据生成器.cpp
@@ -3,26 +3,13 @@
 using namespace std;
 #define el '\n'
 const ll N=1e5+5;
-ll sum=200000;
+
 void solve(){
-    ll t=10000;
-    cout<<t<<el;
-    for(int i=1;i<=t;i++){
-        ll n=rand()%200001;
-        if(n>sum) n=sum;
-        sum-=n;
-        cout<<n<<el;
-        for(int i=1;i<=n;i++){
-            ll a=rand()%(n+1);
-            cout<<a<<" ";
-        }
-        cout<<el;
-    }
+    
 }
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
-    srand(time(0));
     //freopen("xxx.in","r",stdin);
     //freopen("xxx.out","w",stdout);
     ll T=1;
