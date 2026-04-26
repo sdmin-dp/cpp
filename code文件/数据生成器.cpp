1bc24cd8df640f9686d026b04b9a98f43f2a41af
yusiwen2014 <3907363360@qq.com>
2026-04-26 10:11:58 +0000

    A79318:11116-58PMve: {26PM18:11e}

--- code文件/数据生成器.cpp
+++ code文件/数据生成器.cpp
@@ -8,7 +8,7 @@ void solve(){
     c=rand()%5000;if(c==0) c=5000;
     b=rand()%5000;if(b==0) b=5000;
     a=rand()%5000;if(a==0) a=5000;
-    cout<<a<<" "<<c;
+    cout<<a<<" "<<b<<" "<<c;
 }
 int main(){
     ios::sync_with_stdio(0);
