22aaa17f67318f63d78a34c2a048c8a32690a17a
yusiwen2014 <3907363360@qq.com>
2026-04-26 10:11:41 +0000

    A01318:11116-41PMve: {26PM18:11e}

--- code文件/数据生成器.cpp
+++ code文件/数据生成器.cpp
@@ -3,15 +3,19 @@
 using namespace std;
 #define el '\n'
 const ll N=1e5+5;
-
+ll a,b,c;
 void solve(){
-    
+    c=rand()%5000;if(c==0) c=5000;
+    b=rand()%5000;if(b==0) b=5000;
+    a=rand()%5000;if(a==0) a=5000;
+    cout<<a<<" "<<c;
 }
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
     //freopen("xxx.in","r",stdin);
     //freopen("xxx.out","w",stdout);
+    srand(time(0));
     ll T=1;
     //cin>>T;
     while(T--){
