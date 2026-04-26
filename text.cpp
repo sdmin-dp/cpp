53ba91eba9d884d98f51b52046b6e058cf6ab035
yusiwen2014 <3907363360@qq.com>
2026-04-26 08:21:48 +0000

    A63416:21116-48PMve: {26PM16:21e}

--- text.cpp
+++ text.cpp
@@ -28,8 +28,31 @@ void solve(){
     for(int i=1;i<=n;i++){
         p1=a[i].first,p2=a[i].second;
         if(p2==0) continue;
-        cout<<p1<<" "<<
+        if(cnt<k){
+            mp[p1]++;
+            q.push({p2,p1});
+            cnt++;
+            a[i].second=0;
+        }else{
+            if(mp.find(p1)==mp.end()) continue;
+            ll q2=q.top().first,q1=q.top().second,s=mp.size();
+            if(mp[q1]==1){
+                if(p2+(s-1)*(s-1)>q2+s*s){
+                    q.pop();
+                    mp[q1]--;
+                    mp[p1]++;
+                    mp.erase(mp.find(q1));
+                    q.push({p2,p1});
+                }
+            }
+        }
     }
+    sum=mp.size()*mp.size();
+    while(!q.empty()){
+        sum+=q.top().first;
+        q.pop();
+    }
+    cout<<sum;
 }
 int main(){
     ios::sync_with_stdio(0);
