6ad34db0ac5b43c55ae100520aee27e71e167ad4
yusiwen2014 <3907363360@qq.com>
2026-04-28 22:31:42 +0800

    new file:   "acc_\346\267\261\346\220\234\344\274\230\345\214\226_G.cpp"

---
+++ acc_深搜优化_G.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+#define el '\n'
+const ll N=50+5;
+ll n;
+ll a[N],sum,ans=1e12;
+ll sz[N];
+bool dfs(ll num,ll len,ll x){
+    if(x==n+1){
+        // for(int j=1;j<=num;j++) cerr<<sz[j]<<" "; 
+        // cerr<<el;
+        for(int i=1;i<=num;i++){
+            if(sz[i]!=len){
+                return 0;
+            }
+        }
+        ans=min(ans,len);
+        return 1;
+    }
+    for(int i=1;i<=num;i++){
+        if(sz[i]+a[x]<=len){
+            sz[i]+=a[x];
+            bool f=dfs(num,len,x+1);
+            if(f) return 1;
+            // if(sz[i]==a[x]&&!f) return 0;
+            sz[i]-=a[x];
+        }
+    }
+    return 0;
+}
+void solve(){
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        cin>>a[i];
+        sum+=a[i];
+    }
+    sort(a+1,a+n+1,greater<ll>());
+    for(int i=1;i<=n;i++){
+        memset(sz,0,sizeof(sz));
+        if(sum%i!=0) continue;
+        ll len=sum/i;
+        // cerr<<len<<el;
+        dfs(i,len,1);
+        
+        // cerr<<el;
+    }
+    cout<<ans;
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    //freopen("xxx.in","r",stdin);
+    //freopen("xxx.out","w",stdout);
+    ll T=1;
+    //cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
