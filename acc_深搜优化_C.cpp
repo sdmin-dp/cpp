7fb6d6e41cbb282bb315ba2479e3a3e7a6c91bdc
yusiwen2014 <3907363360@qq.com>
2026-04-28 10:52:00 +0000

    A58818:52118-0PMve: {28PM18:52e}

--- acc_深搜优化_C.cpp
+++ acc_深搜优化_C.cpp
@@ -1,11 +1,44 @@
 #include<bits/stdc++.h>
-#define ll long long
 using namespace std;
+#define ll long long
 #define el '\n'
-const ll N=1e5+5;
-
+ll n,m,g[50],half,w,ans,a[(1<<24)+1];
+void dfs1(ll i,ll sum){
+    if(i==half){
+        a[++m]=sum;
+        return;
+    }
+    dfs1(i+1,sum);
+    if(sum+g[i]<=w) dfs1(i+1,sum+g[i]);
+}
+void calc(ll val){
+    ll rest=w-val;
+    ll l=1,r=m;
+    while(l<r){
+        ll mid=(l+r+1)/2;
+        if(a[mid]<=rest) l=mid;
+        else r=mid-1;
+    }
+    ans=max(ans,a[l]+val);
+}
+void dfs2(ll i,ll sum){
+    if(i==n+1){
+        calc(sum);
+        return;
+    }
+    dfs2(i+1,sum);
+    if(sum+g[i]<=w) dfs2(i+1,sum+g[i]);
+}
 void solve(){
-    
+    cin>>w>>n;
+    for(int i=1;i<=n;i++) cin>>g[i];
+    sort(g+1,g+1+n,greater<ll>());
+    half=n/2+3;
+    dfs1(1,0);
+    sort(a+1,a+1+m);
+    m=unique(a+1,a+1+m)-(a+1);
+    dfs2(half,0);
+    cout<<ans<<el;
 }
 int main(){
     ios::sync_with_stdio(0);
