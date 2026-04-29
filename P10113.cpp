679b1a6e61f321a9dbb628eb2ec07cc7c1c2293b
yusiwen2014 <3907363360@qq.com>
2026-04-29 01:55:56 +0000

    A83509:55119-56AMve: {29AM09:55e}

--- P10113.cpp
+++ P10113.cpp
@@ -5,7 +5,28 @@ using namespace std;
 const ll N=1e5+5;
 ll n,m;
 vector<ll> g[N];
-
+ll dep[N],up[25][N];
+void dfs(ll x,ll f){
+    dep[x]=dep[f]+1;up[0][x]=f;
+    for(int i=1;i<20;i++) up[i][x]=up[i-1][up[i-1][x]];
+    for(auto i:g[x]) if(i!=f) dfs(i,x);
+}
+ll LCA(ll u,ll v){
+    if(dep[u]<dep[v]) swap(v,u);
+    for(int i=20;i>=0;i--){
+        if(dep[up[i][u]]>=dep[v]){
+            u=up[i][u];
+        }
+    }
+    if(u==v) return u;
+    for(int k=20;k>=0;k--){
+        if(up[k][u]!=up[k][v]){
+            u=up[k][u];
+            v=up[k][v];
+        }
+    }
+    return up[0][u];
+}
 void solve(){
     cin>>n;
     for(int i=1;i<n;i++){
@@ -17,6 +38,12 @@ void solve(){
     cin>>m;
     for(int i=1;i<=m;i++){
         ll len,x,lca;
+        cin>>len>>lca;
+        for(int j=2;j<=len;j++){
+            cin>>x;
+            lca=LCA(lca,x);
+        }
+        cout<<lca<<el;
     }
 }
 int main(){
