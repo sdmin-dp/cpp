09f8c1ed718eaac4e6a7f55dbf9fded3b912abc5
yusiwen2014 <3907363360@qq.com>
2026-04-29 04:15:40 +0000

    A48912:15119-40PMve: {29PM12:15e}

--- P4427.cpp
+++ P4427.cpp
@@ -1,18 +1,40 @@
 #include<bits/stdc++.h>
-#define ll long long
+#define ll int
 using namespace std;
 #define el '\n'
-const ll N=1e5+5;
+const ll N=5e5+5;
 ll n;
+ll dep[N],up[22][N];
 vector<ll> g[N];
-
+void dfs(ll x,ll f){
+    dep[x]=dep[f]+1;up[0][x]=f;
+    for(int i=1;i<20;i++) up[i][x]=up[i-1][up[i-1][x]];
+    for(auto i:g[x]) if(i!=f) dfs(i,x);
+}
+ll LCA(ll u,ll v){
+    if(dep[u]<dep[v]) swap(v,u);
+    for(int i=20;i>=0;i--){
+        if(dep[up[i][u]]>=dep[v]){
+            u=up[i][u];
+        }
+    }
+    if(u==v) return u;
+    for(int k=20;k>=0;k--){
+        if(up[k][u]!=up[k][v]){
+            u=up[k][u];
+            v=up[k][v];
+        }
+    }
+    return up[0][u];
+}
 void solve(){
     cin>>n;
     for(int i=1;i<n;i++){
-        ll u,v;cin>>u>>v;
-        g[u].push_back(v);
-        g[v].push_back(u);
+        ll x,y;cin>>x>>y;
+        g[x].push_back(y);
+        g[y].push_back(x);
     }
+    dfs(1,0);
 }
 int main(){
     ios::sync_with_stdio(0);
