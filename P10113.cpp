f6d9e9a871da4b8bb3cdceaf040361afd3d1e485
yusiwen2014 <3907363360@qq.com>
2026-04-29 01:52:55 +0000

    A23609:52119-55AMve: {29AM09:52e}

--- P10113.cpp
+++ P10113.cpp
@@ -1,53 +1,25 @@
 #include<bits/stdc++.h>
-using namespace std;
 #define ll long long
+using namespace std;
 #define el '\n'
 const ll N=1e5+5;
-ll n,m;
-ll fa[N];
-ll dep[N],up[20][N];
-void dfs(ll x,ll f){
-    dep[x]=dep[f]+1;up[0][x]=f;
-    for(int i=1;i<17;i++) up[i][x]=up[i-1][up[i-1][x]];
-    for(int i=1;i<n;i++) if(fa[i]==f) dfs(i,x);
-}
-ll LCA(ll u,ll v){
-    if(dep[u]<dep[v]) swap(v,u);
-    for(int i=17;i>=0;i--){
-        if(dep[up[i][u]]>=dep[v]){
-            u=up[i][u];
-        }
-    }
-    if(u==v) return u;
-    for(int k=17;k>=0;k--){
-        if(up[k][u]!=up[k][v]){
-            u=up[k][u];
-            v=up[k][v];
-        }
-    }
-    return up[0][u];
-}
+ll n;
+vector<ll> g[N];
 void solve(){
     cin>>n;
-    for(int i=1;i<n;i++) cin>>fa[i];
-    fa[0]=-1;
-    dfs(1,0);
-    cin>>m;
-    while(m--){
-        ll len,x,lca;
-        cin>>len;
+    for(int i=1;i<=n;i++){
+        ll x;
         cin>>x;
-        lca=x;
-        for(int i=2;i<=len;i++){
-            cin>>x;
-            lca=LCA(lca,x);
-        }
-        cout<<lca<<el;
+
     }
 }
 int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    //freopen("xxx.in","r",stdin);
+    //freopen("xxx.out","w",stdout);
     ll T=1;
-    // cin>>T;
+    //cin>>T;
     while(T--){
         solve();
     }
