f7bfde64b81e5569ebe04f32528ef746eb26d35f
yusiwen2014 <3907363360@qq.com>
2026-04-30 11:07:46 +0000

    A78219:07120-46PMve: {30PM19:07e}

--- code文件/数据生成器.cpp
+++ code文件/数据生成器.cpp
@@ -2,11 +2,18 @@
 #define ll long long
 using namespace std;
 #define el '\n'
-const ll mod2=1e5+5;
+const ll mod2=1e9+1;
 const ll mod=1e5+1;
 ll n,m;
 void solve(){
-    n=rand()%,m=rand();
+    n=rand()%mod,m=rand()%mod;
+    for(int i=1;i<=n;i++){
+        ll x=rand()%mod2;
+        cout<<x<<" ";
+    }
+    for(int i=1;i<=n;i++){
+        
+    }
 }
 int main(){
     ios::sync_with_stdio(0);
