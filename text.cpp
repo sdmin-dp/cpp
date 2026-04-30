a84571fbcb79692345f47bd0907297a1d219bcbc
yusiwen2014 <3907363360@qq.com>
2026-04-30 10:02:27 +0000

    A07018:02120-27PMve: {30PM18:02e}

--- text.cpp
+++ text.cpp
@@ -2,10 +2,22 @@
 #define ll long long
 using namespace std;
 #define el '\n'
-const ll N=1e5+5;
-const ll mxlg=20;
-ll st[N][mxlg];
+const ll N=1e6+5;
+const ll mxlog=20;
+ll n;
+ll st[N][mxlog];
+void init(){
+    for(int i=1;i<=mxlog;i++){
+        for(int j=1;j+(1<<i)<=n;j++){
+            st[j][i]=max(st[j][i-1],st[j+(1<<i-1)][i-1]);
+        }
+    }
+}
 void solve(){
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        cin>>st[i][0];
+    }
     
 }
 int main(){
