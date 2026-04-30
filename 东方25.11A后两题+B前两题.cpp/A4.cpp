c51bb36fcdf89fc566d9ac20f884e9e515a6ed9f
yusiwen2014 <3907363360@qq.com>
2026-04-30 12:10:53 +0000

    A36620:10120-53PMve: {30PM20:10e}

--- 东方25.11A后两题+B前两题.cpp/A4.cpp
+++ 东方25.11A后两题+B前两题.cpp/A4.cpp
@@ -4,8 +4,21 @@ using namespace std;
 #define el '\n'
 const ll N=1e5+5;
 ll n;
+ll a[N];
 void solve(){
-    
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        cin>>a[i];
+    }
+    ll ans=0;
+    for(int i=1;i<=n;i++){
+        for(int j=n;j>=1;j--){
+            if(a[i]!=a[j]){
+                ans=max(ans,llabs(j-i));
+            }
+        }
+    }
+    cout<<ans;
 }
 int main(){
     ios::sync_with_stdio(0);
