885736b80baffaaf98533f0cd69268f3f25d617c
yusiwen2014 <3907363360@qq.com>
2026-04-30 06:26:55 +0000

    A56114:26120-55PMve: {30PM14:26e}

--- text.cpp
+++ text.cpp
@@ -6,22 +6,26 @@ const ll N=1e5+5;
 string s,t;
 ll nxt[N];
 void solve(){
-    cin>>s>>t;
-    for(int i=1,j=0;i<t.size();i++){
-        while(j>0&&t[i]!=t[j]) j=nxt[j-1];
-        if(t[i]==t[j]) j++;
-        nxt[i]=j;
-    }
-    ll cnt=0;
-    for(int i=0,j=0;i<s.size();i++){
-        while(j>0&&s[i]!=t[j]) j=nxt[j-1];
-        if(s[i]==t[j]) j++;
-        if(j==t.size()){
-            j=0;
-            cnt++;
+    while(1){
+        cin>>s;
+        if(s==)
+        for(int i=1,j=0;i<t.size();i++){
+            while(j>0&&t[i]!=t[j]) j=nxt[j-1];
+            if(t[i]==t[j]) j++;
+            nxt[i]=j;
         }
+        ll cnt=0;
+        for(int i=0,j=0;i<s.size();i++){
+            while(j>0&&s[i]!=t[j]) j=nxt[j-1];
+            if(s[i]==t[j]) j++;
+            if(j==t.size()){
+                j=0;
+                cnt++;
+            }
+        }
+        cout<<cnt;
     }
-    cout<<cnt;
+    
 }
 int main(){
     ios::sync_with_stdio(0);
