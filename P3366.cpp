c5ac797c07c4afabb91c2dd4bb6431a7a6993c66
yusiwen2014 <3907363360@qq.com>
2026-04-30 09:03:10 +0000

    A55417:03120-10PMve: {30PM17:03e}

--- P3366.cpp
+++ P3366.cpp
@@ -4,13 +4,29 @@ using namespace std;
 #define el '\n'
 const ll N=1e5+5;
 ll n,m;
-vector<pair<ll,ll>> g[N];
+pair<ll,pair<ll,ll>> g[N];
+struct bcj{
+    vector<ll> fa;
+    bcj(ll len){
+        fa.reserve(len+1);
+        fa.resize(len+1);
+        for(int i=1;i<=len;i++) fa[i]=i;
+    }
+    ll find(ll x){
+        if(fa[x]==x) return x;
+        return fa[x]=find(fa[x]);
+    }
+};
 void solve(){
     cin>>n>>m;
+    bcj a(n);
     for(int i=1;i<=n;i++){
         ll u,v,w;cin>>u>>v>>w;
-        g[u].push_back({v,w});
-        g[v].push_back({u,w});
+        g[i].first=w;
+        g[i].second={min(u,v)}
+    }
+    for(int i=1;i<=m;i++){
+
     }
 }
 int main(){
