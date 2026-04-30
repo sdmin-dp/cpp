2dc0cd6cd40d6bdee325131b057088f112b31e9b
yusiwen2014 <3907363360@qq.com>
2026-04-30 10:56:50 +0000

    A05818:56120-50PMve: {30PM18:56e}

---
+++ 东方25.11A后两题+B前两题.cpp/B1.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+#define el '\n'
+const ll N=1e5+5;
+ll n;
+ll a[N];
+void solve(){
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        cin>>a[i];
+        a[i]+=a[i-1];
+    }
+    for(int i=1;i<=n;i++){
+        ll l,r;
+        cin>>l>>r;
+        
+    }
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    //freopen("xxx.in","r",stdin);
+    //freopen("xxx.out","w",stdout);
+    ll T=1;
+    //cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
