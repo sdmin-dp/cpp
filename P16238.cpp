f0a30e92be640b5691bf4609360579beaefd178b
yusiwen2014 <3907363360@qq.com>
2026-04-30 01:14:12 +0000

    A95909:14120-12AMve: {30AM09:14e}

---
+++ P16238.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+#define el '\n'
+const ll N=2e5+5;
+ll n;
+pair<ll,ll> a[N];
+void solve(){
+    cin>>n;
+    for(int i=1;i<=n;i++) cin>>a[i].first;
+    for(int i=1;i<=n;i++) cin>>a[i].second;
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    //freopen("xxx.in","r",stdin);
+    //freopen("xxx.out","w",stdout);
+    ll T=1;
+    //cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
