09511a47c9c90cdac6b0319de60813eef744b015
yusiwen2014 <3907363360@qq.com>
2026-04-30 02:47:01 +0000

    A50910:47120-1AMve: {30AM10:47e}

--- P3128.cpp
+++ P3128.cpp
@@ -6,6 +6,29 @@ const ll N=1e5+5;
 ll n,m;
 vector<ll> g[N];
 ll d[N];
+ll dep[N],up[22][N];
+vector<ll> g[N];
+void dfs(ll x,ll f){
+    dep[x]=dep[f]+1;up[0][x]=f;
+    for(int i=1;i<20;i++) up[i][x]=up[i-1][up[i-1][x]];
+    for(auto i:g[x]) if(i!=f) dfs(i,x);
+}
+ll LCA(ll u,ll v){
+    if(dep[u]<dep[v]) swap(v,u);
+    for(int i=20;i>=0;i--){
+        if(dep[up[i][u]]>=dep[v]){
+            u=up[i][u];
+        }
+    }
+    if(u==v) return u;
+    for(int k=20;k>=0;k--){
+        if(up[k][u]!=up[k][v]){
+            u=up[k][u];
+            v=up[k][v];
+        }
+    }
+    return up[0][u];
+}
 void solve(){
     cin>>n>>m;
     for(int i=1;i<n;i++){
@@ -13,7 +36,7 @@ void solve(){
         g[x].push_back(y);
         g[y].push_back(x);
     }
-    
+    dfs(1,0);
 }
 int main(){
     ios::sync_with_stdio(0);
