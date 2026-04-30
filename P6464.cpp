cde75e10689404603ff7fcb908186539f713223c
yusiwen2014 <3907363360@qq.com>
2026-04-30 05:05:11 +0000

    A31813:05120-11PMve: {30PM13:05e}

--- P6464.cpp
+++ P6464.cpp
@@ -2,10 +2,53 @@
 #define ll long long
 using namespace std;
 #define el '\n'
-const ll N=1e5+5;
-
+const ll N=1e2+5;
+ll n,m;
+ll dis[N][N];
 void solve(){
-    
+    cin>>n>>m;
+    memset(dis,0x3f,sizeof dis);
+    for(int i=1;i<=n;i++) dis[i][i]=0;
+    for(int i=1;i<=m;i++){
+        ll u,v,w;cin>>u>>v>>w;
+        dis[u][v]=w;
+        dis[v][u]=w;
+    }
+    for(int k=1;k<=n;k++)
+        for(int i=1;i<=n;i++)
+            for(int j=1;j<=n;j++)
+                if(dis[i][k]+dis[k][j]<dis[i][j])
+                    dis[i][j]=dis[i][k]+dis[k][j];
+    ll sum=0,ans=0;
+    ll idx=0,idy=0;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            // cerr<<dis[i][j]<<" ";
+            if(dis[i][j]>=0x3f3f3f3f3f3f3f3f) continue;
+            if(dis[i][j]>ans){
+                ans=dis[i][j];
+                idx=i,idy=j;
+            }
+            
+        }
+        // cerr<<el;
+    }
+    dis[idx][idy]=0;
+    for(int k=1;k<=n;k++)
+        for(int i=1;i<=n;i++)
+            for(int j=1;j<=n;j++)
+                if(dis[i][k]+dis[k][j]<dis[i][j])
+                    dis[i][j]=dis[i][k]+dis[k][j];
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            // cerr<<dis[i][j]<<" ";
+            if(dis[i][j]>=0x3f3f3f3f3f3f3f3f) continue;
+            sum+=dis[i][j];
+        }
+        // cerr<<el;
+    }
+    // cerr<<sum/2<<" "<<ans;
+    cout<<sum/2;
 }
 int main(){
     ios::sync_with_stdio(0);
