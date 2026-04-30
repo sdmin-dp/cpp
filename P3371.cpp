705d384b9f4bbe24e275f8350c0705d6d941a175
yusiwen2014 <3907363360@qq.com>
2026-04-30 08:52:36 +0000

    A19416:52120-36PMve: {30PM16:52e}

--- P3371.cpp
+++ P3371.cpp
@@ -4,14 +4,22 @@ using namespace std;
 #define el '\n'
 const ll N=1e5+5;
 ll n,m,b;
+ll dis[N];
 vector<pair<ll,ll>> g[N];
+void SPFA(){
+    queue<ll> q;
+    for(int i=1;i<=n;i++) dis[i]=(1<<31)-1;
+    q.push(b);
+    dis[b]=0;
+    while(!q.empy)
+}
 void solve(){
     cin>>n>>m>>b;
     for(int i=1;i<=m;i++){
         ll u,v,w;cin>>u>>v>>w;
         g[u].push_back({v,w});
     }
-    
+
 }
 int main(){
     ios::sync_with_stdio(0);
