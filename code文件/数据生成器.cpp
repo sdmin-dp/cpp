8457788e77055d22f3f1cbcd0ad1a23ca325b000
yusiwen2014 <3907363360@qq.com>
2026-04-30 11:05:50 +0000

    A20119:05120-50PMve: {30PM19:05e}

--- code文件/数据生成器.cpp
+++ code文件/数据生成器.cpp
@@ -3,19 +3,15 @@
 using namespace std;
 #define el '\n'
 const ll N=1e5+5;
-ll a,b,c;
+
 void solve(){
-    c=rand()%200;if(c==0) c=200;
-    b=rand()%c;if(b==0) b=c;
-    a=rand()%b;if(a==0) a=b;
-    cout<<a<<" "<<b<<" "<<c;
+    
 }
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
     //freopen("xxx.in","r",stdin);
     //freopen("xxx.out","w",stdout);
-    srand(time(0));
     ll T=1;
     //cin>>T;
     while(T--){
--- 东方25.11A后两题+B前两题.cpp/B1.cpp
+++ 东方25.11A后两题+B前两题.cpp/B1.cpp
@@ -3,9 +3,19 @@
 using namespace std;
 #define el '\n'
 const ll N=1e5+5;
-
+ll n,m;
+ll a[N];
 void solve(){
-    
+    cin>>n>>m;
+    for(int i=1;i<=n;i++) cin>>a[i];
+    sort(a+1,a+n+1);
+    for(int i=1;i<=m;i++){
+        ll l,r;
+        cin>>l>>r;
+        ll R=upper_bound(a+1,a+n+1,r)-a;
+        ll L=lower_bound(a+1,a+n+1,l)-a;
+        cout<<R-L<<el;
+    }
 }
 int main(){
     ios::sync_with_stdio(0);
