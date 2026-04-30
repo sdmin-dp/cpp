054b6420d057d9bf6df96339105b5fd13c31455c
yusiwen2014 <3907363360@qq.com>
2026-04-30 07:59:23 +0000

    A90315:59120-23PMve: {30PM15:59e}

--- text.cpp
+++ text.cpp
@@ -6,38 +6,38 @@ const ll N=1e5+5;
 ll n,m;
 ll dis[N][N];
 void solve(){
-    // cin>>n>>m;
-    // memset(dis,0x3f,sizeof dis);
-    // for(int i=1;i<=n;i++) dis[i][i]=0;
-    // for(int i=1;i<=m;i++){
-    //     ll u,v,w;
-    //     cin>>u>>v>>w;
-    //     if(dis[u][v]!=0x3f3f3f3f3f3f3f3f){
-    //         dis[u][v]=max(w,dis[u][v]);
-    //         dis[v][u]=max(w,dis[v][u]);
-    //     }
-    // }
-    // for(int k=1;k<=n;k++)
-    //     for(int i=1;i<=n;i++)
-    //         for(int j=1;j<=n;j++)
-    //             if(dis[i][k]+dis[k][j]<dis[i][j])
-    //                 dis[i][j]=dis[i][k]+dis[k][j];
-    // for(int i=1;i<=n;i++){
-    //     for(int j=1;j<=n;j++){
-    //         cout<<dis[i][j]<<" ";
-    //     }
-    // }
+    cin>>n>>m;
+    memset(dis,0x3f,sizeof dis);
+    for(int i=1;i<=n;i++) dis[i][i]=0;
+    for(int i=1;i<=m;i++){
+        ll u,v,w;
+        cin>>u>>v>>w;
+        if(dis[u][v]!=0x3f3f3f3f3f3f3f3f){
+            dis[u][v]=max(w,dis[u][v]);
+            dis[v][u]=max(w,dis[v][u]);
+        }
+    }
+    for(int k=1;k<=n;k++)
+        for(int i=1;i<=n;i++)
+            for(int j=1;j<=n;j++)
+                if(dis[i][k]+dis[k][j]<dis[i][j])
+                    dis[i][j]=dis[i][k]+dis[k][j];
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            cout<<dis[i][j]<<" ";
+        }
+    }
     
 }
 int main(){
-    // ios::sync_with_stdio(0);
-    // cin.tie(0);cout.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
     //freopen("xxx.in","r",stdin);
     //freopen("xxx.out","w",stdout);
     ll T=1;
     //cin>>T;
-    // while(T--){
-    //     solve();
-    // }
+    while(T--){
+        solve();
+    }
     return 0;
 }
