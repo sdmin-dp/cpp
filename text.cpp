e9516ac1239abc164425c41120c40e45dc4f9f3d
yusiwen2014 <3907363360@qq.com>
2026-04-30 10:48:25 +0000

    A77218:48120-25PMve: {30PM18:48e}

--- text.cpp
+++ text.cpp
@@ -1,35 +1,11 @@
 #include<bits/stdc++.h>
-#define ll int
+#define ll long long
 using namespace std;
 #define el '\n'
 const ll N=1e5+5;
-const ll mxlog=20;
-ll n,m;
-ll st[N][mxlog];
-ll lg[N];
-void init(){
-    for(int i=2;i<=n;i++) lg[i]=lg[i/2]+1;
-    for(int i=1;i<=mxlog;i++){
-        for(int j=1;j+(1<<i)-1<=n;j++){
-            st[j][i]=max(st[j][i-1],st[j+(1<<i-1)][i-1]);
-        }
-    }
-}
-ll query(ll l,ll r){
-    ll k=lg[r-l+1];
-    return max(st[l][k],st[r-(1<<k)+1][k]);
-}
+
 void solve(){
-    cin>>n>>m;
-    for(int i=1;i<=n;i++){
-        cin>>st[i][0];
-    }
-    init();
-    for(int i=1;i<=m;i++){
-        ll l,r;
-        cin>>l>>r;
-        cout<<query(l,r)<<el;
-    }
+    
 }
 int main(){
     ios::sync_with_stdio(0);
