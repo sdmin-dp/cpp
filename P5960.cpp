1a7ac30af8b29f43101ca841e1b8ec40715d0be7
yusiwen2014 <3907363360@qq.com>
2026-04-30 09:13:33 +0000

    A77517:13120-33PMve: {30PM17:13e}

---
+++ P5960.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+#define el '\n'
+const ll N=1e5+5;
+ll n,m;
+vector<pair<ll,ll>> g[N];
+vector<ll> dis(N,INT_MAX);
+vector<ll> vis(N);
+ll cnt[N];
+bool SPFA(){
+    queue<ll> q;
+    dis[0]=0;
+    q.push(0);
+    vis[0]=1;
+    while(!q.empty()){
+        auto x=q.front();
+        q.pop();
+        vis[x]=0;
+        for(auto i:g[x]){
+            if(dis[i.first]>dis[x]+i.second){
+                dis[i.first]=dis[x]+i.second;
+                if(!vis[i.first]){
+                    cnt[i.first]++;
+                    vis[i.first]=1;
+                    if(cnt[i.first]>n+1) return 0;
+                    q.push(i.first);
+                }
+            }
+        }
+    }
+    return 1;
+}
+void solve(){
+    cin>>n>>m;
+    for(int i=1;i<=m;i++){
+        ll u,v,w;
+        cin>>u>>v>>w;
+        g[v].push_back({u,w});
+    }
+    for(int i=1;i<=n;i++) g[0].push_back({i,0});
+    bool flag=SPFA();
+    if(!flag) cout<<"NO";
+    else for(int i=1;i<=n;i++) cout<<dis[i]<<" ";
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    //freopen("xxx.in","r",stdin);
+    //freopen("xxx.out","w",stdout);
+    ll T=1;
+    //cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
