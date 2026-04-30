9ba820ca0090e07dedc0197459f7b22eb62fd5b6
yusiwen2014 <3907363360@qq.com>
2026-04-30 04:14:57 +0000

    A80012:14120-57PMve: {30PM12:14e}

--- P8805.cpp
+++ P8805.cpp
@@ -4,8 +4,30 @@ using namespace std;
 #define el '\n'
 const ll N=1e5+5;
 ll n,m;
-vector<ll> g[N];
 ll sum[N];
+ll dep[N],up[22][N];
+vector<ll> g[N];
+void dfs(ll x,ll f){
+    dep[x]=dep[f]+1;up[0][x]=f;
+    for(int i=1;i<20;i++) up[i][x]=up[i-1][up[i-1][x]];
+    for(auto i:g[x]) if(i!=f) dfs(i,x);
+}
+ll LCA(ll u,ll v){
+    if(dep[u]<dep[v]) swap(v,u);
+    for(int i=20;i>=0;i--){
+        if(dep[up[i][u]]>=dep[v]){
+            u=up[i][u];
+        }
+    }
+    if(u==v) return u;
+    for(int k=20;k>=0;k--){
+        if(up[k][u]!=up[k][v]){
+            u=up[k][u];
+            v=up[k][v];
+        }
+    }
+    return up[0][u];
+}
 void dfs(ll x,ll fa){
     for(auto i:g[x]){
         if(i!=fa){
@@ -23,7 +45,9 @@ void solve(){
     }
     sum[1]=g[1].size();
     dfs(1,0);
-    for(int i=1)
+    for(int i=1;i<=m;i++){
+        
+    }
 }
 int main(){
     ios::sync_with_stdio(0);
