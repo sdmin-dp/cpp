f15a22a4a600922ab39e85969cfaa77186544f4c
yusiwen2014 <3907363360@qq.com>
2026-04-30 11:08:18 +0000

    A96219:08120-18PMve: {30PM19:08e}

--- code文件/数据生成器.cpp
+++ code文件/数据生成器.cpp
@@ -7,12 +7,15 @@ const ll mod=1e5+1;
 ll n,m;
 void solve(){
     n=rand()%mod,m=rand()%mod;
+    cout<<n<<" "<<m<<el;
     for(int i=1;i<=n;i++){
         ll x=rand()%mod2;
         cout<<x<<" ";
     }
-    for(int i=1;i<=n;i++){
-        
+    cout<<el;
+    for(int i=1;i<=m;i++){
+        ll l=rand()%mod2,r=rand()%mod2;
+        cout<<l<<" "<<r<<el;
     }
 }
 int main(){
