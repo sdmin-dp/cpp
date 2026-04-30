4c6dcfd042e86464cd23865e60a85d376ac59f48
yusiwen2014 <3907363360@qq.com>
2026-04-30 07:06:16 +0000

    A64715:06120-16PMve: {30PM15:06e}

--- P3374.cpp
+++ P3374.cpp
@@ -1,70 +1,51 @@
 #include<bits/stdc++.h>
-#define ll long long
 using namespace std;
-#define el '\n'
-const ll N=1e6+5;
-struct Segment{
-    ll l,r;
-    ll sum;
-}t[N<<2];
+#define ll long long
+const ll N=5e5+5;
 ll n,m;
-ll a[N];
-void pushup(ll idx){
-    t[idx].sum=t[idx*2].sum+t[idx*2+1].sum;
+ll a[N],c[N];
+ll lowbit(ll x){
+     return (x&-x);
 }
-void build(ll l=1,ll r=n,ll idx=1){
-    t[idx].l=l;
-    t[idx].r=r;
-    if(l==r){
-        t[idx].sum=a[l];
-        return;
+ll getsum(ll x){
+    ll t=0;
+    while(x>0){
+        t+=c[x];
+        x-=lowbit(x);
     }
-    ll mid=(l+r)>>1;
-    build(l,mid,idx*2);
-    build(mid+1,r,idx*2+1);
-    pushup(idx);
+    return t;
 }
-void update(ll x,ll y,ll idx=1){
-    if(t[idx].l==t[idx].r&&t[idx].l==x){
-        t[idx].sum+=y;
-        return;
+void add(ll x,ll d){
+    while(x<=n){
+        c[x]+=d;
+        x+=lowbit(x);
     }
-    ll mid=(t[idx].l+t[idx].r)/2;
-    if(x<=mid) update(x,y,idx*2);
-    else update(x,y,idx*2+1);
-    pushup(idx);
-}
-ll query(ll b,ll e,ll idx=1){
-    ll l=t[idx].l;
-    ll r=t[idx].r;
-    if(b<=l&&r<=e) return t[idx].sum;
-    ll mid=(l+r)/2;
-    ll sum=0;
-    if(b<=mid) sum+=query(b,e,idx*2);
-    if(mid<e) sum+=query(b,e,idx*2+1);
-    return sum;
 }
 void solve(){
     cin>>n>>m;
-    for(int i=1;i<=n;i++) cin>>a[i];
-    build();
-    // ll b,e;
-    // cin>>b>>e;
-    // cout<<query(b,e);
+    for(int i=1;i<=n;i++){
+        cin>>a[i];
+        add(i,a[i]);
+    }
     for(int i=1;i<=m;i++){
-        ll op,x,y;
-        cin>>op>>x>>y;
-        if(op==1) update(x,y);
-        else cout<<query(x,y)<<el;
+        ll k,x,y;
+        cin>>k>>x>>y;
+        if(k==1){
+            add(x,y);
+        }
+        else{
+            cout<<getsum(y)-getsum(x-1);
+            cout<<'\n';
+        }
     }
 }
 int main(){
-    //ios::sync_with_stdio(0);
-    //cin.tie(0);cout.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
     //freopen("xxx.in","r",stdin);
     //freopen("xxx.out","w",stdout);
     ll T=1;
-    //cin>>T;
+    // cin>>T;
     while(T--){
         solve();
     }
