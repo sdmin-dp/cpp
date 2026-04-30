3bca5f9fa74afc4fe71a24a441ae3c75beecd29b
yusiwen2014 <3907363360@qq.com>
2026-04-30 07:41:12 +0000

    A86315:41120-12PMve: {30PM15:41e}

--- text.cpp
+++ text.cpp
@@ -14,8 +14,8 @@ void add(ll x,ll y){
 ll get_sum(ll l,ll r){
     if(l>r) swap(l,r);
     ll sum1=0,sum2=0;
-    for(int i=l-1;i>=1;i-=low_bit(i)) sum1+=tree[i];
-    for(int i=r;i>=1;i-=low_bit(i)) sum2+=tree[i];
+    for(int i=l-1;i>=0;i-=low_bit(i)) sum1+=tree[i];
+    for(int i=r;i>=0;i-=low_bit(i)) sum2+=tree[i];
     return sum2-sum1;
 }
 void solve(){
