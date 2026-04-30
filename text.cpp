0ca0544dac4560b88ed43835edc7b364caa6a7a8
yusiwen2014 <3907363360@qq.com>
2026-04-30 07:06:37 +0000

    A73615:06120-37PMve: {30PM15:06e}

--- text.cpp
+++ text.cpp
@@ -3,36 +3,9 @@
 using namespace std;
 #define el '\n'
 const ll N=1e5+5;
-ll n;
-ll idx;
-ll trie[N][30];
-ll cnt[N];
-void insert(string s){
-    ll p=0;
-    for(auto &i:s){
-        ll x=i-'a';
-        if(trie[p][x]!=0)p=trie[p][x];
-        else{trie[p][x]=++idx;p=idx;}
-    }
-    cnt[p]++;
-}
-ll query(string s){
-    ll p=0;
-    for(auto &i:s){
-        ll x=i-'a';
-        if(trie[p][x]!=0)p=trie[p][x];
-        else return 0;
-    }
-    return cnt[p];
-}
+
 void solve(){
-    cin>>n;
-    for(int i=1;i<=n;i++){
-        char op;string s;
-        cin>>op>>s;
-        if(op=='I') insert(s);
-        else cout<<query(s)<<el;
-    }
+    cout<<8^(-8);
 }
 int main(){
     ios::sync_with_stdio(0);
