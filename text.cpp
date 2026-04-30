f8bfff69b8fb0748967104cc5f3998175787fe36
yusiwen2014 <3907363360@qq.com>
2026-04-30 07:29:41 +0000

    A47415:29120-41PMve: {30PM15:29e}

--- text.cpp
+++ text.cpp
@@ -6,14 +6,26 @@ const ll N=1e5+5;
 ll low_bit(ll x){
     return (x&(-x));
 }
-ll n;
+ll n,q;
+ll tree[N];
 void add(ll x,ll y){
-    for(int i=x;i<=n;i+=low_bit(i)){
-
-    }
+    for(int i=x;i<=n;i+=low_bit(i)) tree[i]+=y;
+}
+ll get_sum(ll l,ll r){
+    ll sum1=0,sum2=0;
+    for(int i=l-1;i>=1;i-=low_bit(i)) sum1+=tree[i];
+    for(int i=r;i>=1;i-=low_bit(i)) sum2+=tree[i];
+    return sum2-sum1;
 }
 void solve(){
-    
+    cin>>n>>q;
+    for(int i=1;i<=n;i++){
+        ll x;cin>>x;
+        add(i,x);
+    }
+    for(int i=1;i<=m;i++){
+        
+    }
 }
 int main(){
     ios::sync_with_stdio(0);
