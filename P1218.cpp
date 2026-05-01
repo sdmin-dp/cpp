bca937a296d3dd54ea633d627d968aa793d3f16c
yusiwen2014 <3907363360@qq.com>
2026-05-01 01:25:45 +0000

    A96509:25121-45AMve: {1AM09:25e}

--- P1218.cpp
+++ P1218.cpp
@@ -4,16 +4,25 @@ using namespace std;
 #define el '\n'
 const ll N=1e5+5;
 ll n;
+bool isprime(ll x){
+    if(x<2) return 0;
+    for(ll i=2;i*i<=x;i++) if(x%i==0) return 0;
+    return 1;
+}
 void dfs(ll x,ll prime){
     if(x==n+1){
         cout<<prime<<el;
         return;
     }
-    
+    for(int i=1;i<=9;i++){
+        if(isprime(prime*10+i)){
+            dfs(x+1,prime*10+i);
+        }
+    }
 }
 void solve(){
     cin>>n;
-
+    dfs(1,0);
 }
 int main(){
     ios::sync_with_stdio(0);
