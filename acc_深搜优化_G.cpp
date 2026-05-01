b8efa5ad52bae870f3e88ab4571c39d36be8b509
yusiwen2014 <3907363360@qq.com>
2026-05-01 01:57:49 +0000

    A58309:57121-49AMve: {1AM09:57e}

--- acc_深搜优化_G.cpp
+++ acc_深搜优化_G.cpp
@@ -2,36 +2,10 @@
 #define ll long long
 using namespace std;
 #define el '\n'
-const ll N=50+5;
+const ll N=1e5+5;
 ll n;
-ll a[N],sum,ans=1e12;
-ll sz[N];
-bool vis[N];
-bool dfs(ll num,ll len,ll x){
-    if(x==n+1){
-        // for(int j=1;j<=num;j++) cerr<<sz[j]<<" "; 
-        // cerr<<el;
-        for(int i=1;i<=num;i++){
-            if(sz[i]!=len){
-                return 0;
-            }
-        }
-        ans=min(ans,len);
-        return 1;
-    }
-    for(int i=1;i<=num;i++){
-        if(!vis[x]&&sz[i]+a[x]<=len){
-            vis[x]=1;
-            sz[i]+=a[x];
-            bool f=dfs(num,len,x+1);
-            if(f) return 1;
-            // if(sz[i]==a[x]&&!f) return 0;
-            sz[i]-=a[x];
-            vis[x]=0;
-        }
-    }
-    return 0;
-}
+ll a[N];
+ll sum;
 void solve(){
     cin>>n;
     for(int i=1;i<=n;i++){
@@ -40,14 +14,9 @@ void solve(){
     }
     sort(a+1,a+n+1,greater<ll>());
     for(int i=n;i>=1;i--){
-        memset(sz,0,sizeof(sz));
         if(sum%i!=0) continue;
-        ll len=sum/i;
-        // cerr<<len<<el;
-        if(dfs(i,len,1)) break;        
-        // cerr<<el;
+        
     }
-    cout<<ans;
 }
 int main(){
     ios::sync_with_stdio(0);
