6368c42b79b3ccb029153e13edc86be5a4c92d02
yusiwen2014 <3907363360@qq.com>
2026-05-01 03:39:09 +0000

    A64211:39121-9AMve: {1AM11:39e}

--- P14918.cpp
+++ P14918.cpp
@@ -5,43 +5,11 @@ using namespace std;
 const ll N=1e5+5;
 ll n;
 ll a[N];
-vector<ll> v[N];
-void fenjie(ll x){
-    ll k=x;
-    for(ll i=2;i*i<=k;i++){
-        ll cnt=0;
-        while(k%i==0){
-            cnt++;
-            k/=i;
-        }
-        if(cnt) v[i].push_back(cnt);
-    }
-    if(k){
-        v[k].push_back(1);
-    }
-}
 void solve(){
     cin>>n;
     for(int i=1;i<=n;i++){
         cin>>a[i];
-        fenjie(a[i]);
     }
-    for(int i=1;i<10;i++){
-        cerr<<i<<":";
-        for(auto j:v[i]){
-            cerr<<j<<" ";
-        }
-        cerr<<el;
-    }
-    // ll ans=0;
-    // for(int i=0;i<N-5;i++){
-    //     sort(v[i].begin(),v[i].end());
-    //     ll mid=v[i][v[i].size()/2+1];
-    //     for(auto &i:v[i]){
-    //         ans+=abs(i-mid);
-    //     }
-    // }
-    // cout<<ans;
 }
 int main(){
     ios::sync_with_stdio(0);
