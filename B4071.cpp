e1da8ec33de86a24e1b5cf7d8b7f2842d8a0f0d5
yusiwen2014 <3907363360@qq.com>
2026-05-01 04:13:09 +0000

    A81112:13121-9PMve: {1PM12:13e}

--- B4071.cpp
+++ B4071.cpp
@@ -5,12 +5,27 @@ using namespace std;
 const ll N=1e3+5;
 ll n;
 pair<ll,ll> a[N];
+ll cnt[N];
+vector<ll> v[N];
 void solve(){
     cin>>n;
     for(int i=1;i<=n;i++){
         cin>>a[i].first>>a[i].second;
+        cnt[a[i].first]++;
+        v[a[i].first].push_back(a[i].second);
     }
-    
+    for(int i=1;i<=n;i++){
+        
+    }
+    for(int i=1;i<=n;i++){
+        ll sum=cnt[1],num=0;
+        for(int j=2;j<=n;j++){
+            if(cnt[j]>=i){
+                sum+=cnt[j]-i;
+
+            }
+        }
+    }
 }
 int main(){
     ios::sync_with_stdio(0);
