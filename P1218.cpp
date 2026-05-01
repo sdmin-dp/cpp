95ea92a98dfb4b56245c09541f2f2bb7a467f15b
yusiwen2014 <3907363360@qq.com>
2026-05-01 01:23:40 +0000

    A58209:23121-40AMve: {1AM09:23e}

---
+++ P1218.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+#define el '\n'
+const ll N=1e5+5;
+ll n;
+void dfs(ll x,ll prime){
+    if(x==n+1){
+        cout<<prime<<el;
+        return;
+    }
+    
+}
+void solve(){
+    cin>>n;
+
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    //freopen("xxx.in","r",stdin);
+    //freopen("xxx.out","w",stdout);
+    ll T=1;
+    //cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
