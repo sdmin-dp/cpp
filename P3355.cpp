d571e9c580fa687f671484550c976840ae1b4c36
yusiwen <3907363360@qq.com>
2026-05-02 10:33:54 +0000

    2026年5月2日 18:33

--- P3355.cpp
+++ P3355.cpp
@@ -1,53 +1,27 @@
 #include<bits/stdc++.h>
-using namespace std;
 #define ll long long
+using namespace std;
 #define el '\n'
-const ll N=205;
-ll n,m,t,match[N*N];
-bool bad[N][N],vis[N*N];
-vector<ll> g[N*N];
-ll dx[]={1,1,2,2,-1,-1,-2,-2},dy[]={2,-2,1,-1,2,-2,1,-1};
-bool dfs(ll u){
-    for(auto v:g[u]){
-        if(!vis[v]){
-            vis[v]=1;
-            if(!match[v]||dfs(match[v])){
-                match[v]=u;
-                return 1;
-            }
-        }
-    }
-    return 0;
-}
+const ll N=1e5+5;
+ll n,m;
+pair<ll,ll> a[N];
+ll dx[]={-2,-1,1,2,2,1,-1,-2},dy[]={1,2,2,1,-1,-2,-2,-1};
 void solve(){
-    cin>>n>>m>>t;
-    for(int i=1;i<=t;i++){
-        ll x,y;cin>>x>>y;
-        bad[x][y]=1;
+    cin>>n>>m;
+    for(int i=1;i<=m;i++){
+        cin>>a[i].first>>a[i].second;
     }
     for(int i=1;i<=n;i++){
-        for(int j=1;j<=m;j++){
-            if(bad[i][j]||(i+j)%2)continue;
+        for(int j=1;j<=n;j++){
             for(int k=0;k<8;k++){
-                int nx=i+dx[k],ny=j+dy[k];
-                if(nx>=1&&nx<=n&&ny>=1&&ny<=m&&!bad[nx][ny]){
-                    g[(i-1)*m+j].push_back((nx-1)*m+ny);
-                }
+                ll x=i+dx[k],y=j+dy[k];
             }
         }
     }
-    ll ans=0;
-    for(int i=1;i<=n;i++)
-        for(int j=1;j<=m;j++)
-            if(!bad[i][j]&&(i+j)%2==0){
-                memset(vis,0,sizeof(vis));
-                if(dfs((i-1)*m+j))ans++;
-            }
-    cout<<n*m-t-ans<<el;
 }
 int main(){
-    // iios::sync_with_stdio(0);
-    // cin.tie(0);cout.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
     //freopen("xxx.in","r",stdin);
     //freopen("xxx.out","w",stdout);
     ll T=1;
