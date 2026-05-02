4b899ca79b323213076100096a46867df3d706a5
yusiwen <3907363360@qq.com>
2026-05-02 10:51:57 +0000

    2026年5月2日 18:51

---
+++ exam/B1.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+#define el '\n'
+const ll N=1e5+5;
+ll n;
+ll erfen(){
+    ll l=0,r=n,mid=0,ans=0;
+    while(l<=r){
+        mid=(l+r)/2;
+        if(mid*mid*mid<n){
+            ans=mid;
+            l=mid+1;
+        }else{
+            r=mid-1;
+        }
+    }
+    return ans;
+}
+void solve(){
+    cin>>n;
+    ll ans=erfen();
+    cout<<ans<<" "<<ans+1;
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    //freopen("xxx.in","r",stdin);
+    //freopen("xxx.out","w",stdout);
+    ll T=1;
+    //cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
