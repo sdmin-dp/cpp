641ebc6adb0dcfc13d65d04def4227e35cd38b66
yusiwen <3907363360@qq.com>
2026-05-02 12:29:00 +0000

    2026年5月2日 20:29

--- exam/B3.cpp
+++ exam/B3.cpp
@@ -1,29 +1,33 @@
-#include<iostream>
-#define ll int
+#include<bits/stdc++.h>
+#define ll long long
 using namespace std;
 #define el '\n'
 const ll N=5e2+5;
 ll n;
 ll a[N][N];
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);cout.tie(0);
+void solve(){
     cin>>n;
     for(int i=1;i<=n;i++){
         for(int j=1;j<=n;j++){
             cin>>a[i][j];
         }
     }
-    long long ans=0;
+    ll ans=0;
     for(int x1=1;x1<=n;x1++){
         for(int y1=1;y1<=n;y1++){
-            for(int x2=1;x2<=n;x2++){
-                for(int y2=1;y2<=n;y2++){
+            for(int x2=x1;x2<=n;x2++){
+                for(int y2=y1;y2<=n;y2++){
                     ll mn=10000;
+                    bool flag=1;
                     for(int i=x1;i<=x2;i++){
                         for(int j=y1;j<=y2;j++){
-                            mn=min(mn,a[i][j]);
+                            if(a[i][j]<mn) mn=a[i][j];
+                            if(mn<100){
+                                flag=0;
+                                break;
+                            }
                         }
+                        if(!flag) break;
                     }
                     if(mn==100){
                         ans++;
@@ -33,5 +37,14 @@ int main(){
         }
     }
     cout<<ans;
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    ll T=1;
+    //cin>>T;
+    while(T--){
+        solve();
+    }
     return 0;
 }
