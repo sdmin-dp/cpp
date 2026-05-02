ff6ccc3216d23cbc1fb48a8b196d394f06623715
yusiwen <3907363360@qq.com>
2026-05-02 10:39:03 +0000

    2026年5月2日 18:39

---
+++ exam/A2.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+#define el '\n'
+const ll N=1e5+5;
+ll n,k;
+ll a[N];
+void solve(){
+    cin>>n>>k;
+    for(int i=1;i<=n;i++){
+        cin>>a[i];
+    }
+    sort(a+1,a+n+1);
+    ll ans=1e18;
+    for(int i=1;i+k-1<=n;i++){
+        ans=min(ans,a[i+k-1]-a[i]);
+    }
+    cout<<ans;
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    //freopen("xxx.in","r",stdin);
+    //freopen("xxx.out","w",stdout);
+    ll T=1;
+    //cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
