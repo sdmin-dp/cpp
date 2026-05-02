daac1b5cdebf6bc2b42464627418c56070d28090
yusiwen2014 <3907363360@qq.com>
2026-05-02 01:27:07 +0000

    A44609:27122-7AMve: {2AM09:27e}

--- 模板及重要代码/克鲁斯查尔(最小生成树算法).cpp
+++ 模板及重要代码/克鲁斯查尔(最小生成树算法).cpp
@@ -6,45 +6,41 @@ const ll N=1e5+5;
 struct bcj{
     vector<ll> fa;
     bcj(ll len){
-        fa.reserve(len+5);
-        fa.resize(len+5);
-        for(int i=1;i<=len;i++){
-            fa[i]=i;
-        }
+        fa.reserve(len+1);
+        fa.resize(len+1);
+        for(int i=1;i<=len;i++) fa[i]=i;
     }
     ll find(ll x){
-        if(fa[x]==x) return x;
+        if(x==fa[x]) return x;
         return fa[x]=find(fa[x]);
     }
 };
 ll n,m;
-vector<pair<ll,ll>> g[N];
-vector<pair<ll,pair<ll,ll>>> edge;
+vector<pair<ll,pair<ll,ll>>> g; 
 void solve(){
     cin>>n>>m;
     bcj a(n);
     for(int i=1;i<=m;i++){
-        ll x,y,z;
-        cin>>x>>y>>z;
-        g[x].push_back({z,y});
-        g[y].push_back({z,x});
-        edge.push_back({z,{min(x,y),max(x,y)}});
+        ll u,v,w;
+        cin>>u>>v>>w;
+        g.push_back({w,{min(u,v),max(u,v)}});
     }
-    sort(edge.begin(),edge.end());
-    ll cnt=0;
-    ll ans=0;
-    for(auto i:edge){
+    sort(g.begin(),g.end());
+    ll cnt=0,sum=0;
+    for(auto i:g){
+        ll w=i.first,u=i.second.first,v=i.second.second;
+        ll fu=a.find(u),fv=a.find(v);
+        if(fu!=fv){
+            cnt++;
+            sum+=w;
+            a.fa[fu]=fv;
+        }
         if(cnt==n-1){
-            cout<<ans;
+            cout<<sum;
             return;
         }
-        ll u=i.second.first,v=i.second.second,w=i.first;
-        if(a.find(u)!=a.find(v)){
-            cnt++;
-            ans+=w;
-            a.fa[a.find(u)]=a.fa[a.find(v)];
-        }
     }
+    cout<<"orz";
 }
 int main(){
     ios::sync_with_stdio(0);
