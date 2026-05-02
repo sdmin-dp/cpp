f9a98e67cf8c56bd2392e37b64000b17be2fecd7
yusiwen <3907363360@qq.com>
2026-05-02 10:42:33 +0000

    2026年5月2日 18:42

---
+++ exam/A3.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+#define el '\n'
+const ll N=1e5+5;
+ll n;
+ll cnt,x;
+void solve(){
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        cin>>x;
+        while(x%2==0){
+            cnt++;
+            x/=2;
+        }
+    }
+    cout<<cnt;
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    //freopen("xxx.in","r",stdin);
+    //freopen("xxx.out","w",stdout);
+    ll T=1;
+    //cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
