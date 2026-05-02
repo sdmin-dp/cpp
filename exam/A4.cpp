c7e851b61e50ac022c3bc9c9a6e5a551698d683b
yusiwen <3907363360@qq.com>
2026-05-02 11:34:00 +0000

    2026年5月2日 19:34

--- exam/A4.cpp
+++ exam/A4.cpp
@@ -8,19 +8,25 @@ struct node{
 };
 ll n;
 node a[N];
+bool cmp(node x,node y){
+    return x.h>y.h;
+}
 void solve(){
     cin>>n;
     for(int i=1;i<=n;i++)
         cin>>a[i].x>>a[i].y>>a[i].h;
+    sort(a+1,a+n+1,cmp);
     for(int cx=1;cx<=100;cx++){
         for(int cy=1;cy<=100;cy++){
-            ll h=a[1].h-abs(a[1].x-cx)+abs(a[1].y-cy);
+
+            ll h=a[1].h+abs(a[1].x-cx)+abs(a[1].y-cy);
+
             bool f=1;
+
             for(int i=2;i<=n;i++){
                 ll k=abs(a[i].x-cx)+abs(a[i].y-cy);
-                if(a[i].h==0&&!(h==k||h==a[i].h-k)){
-                    f=0;
-                    break;
+                if(a[i].h==0){
+                    continue;
                 }else if(h!=a[i].h-k){
                     f=0;
                     break;
