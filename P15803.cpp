155270fe678c9611887582af4729b8d3b1921a32
yusiwen <3907363360@qq.com>
2026-05-03 02:48:36 +0000

    2026年5月3日 10:48

--- P15803.cpp
+++ P15803.cpp
@@ -6,19 +6,25 @@ const ll N=1e5+5;
 ll n,m;
 vector<array<ll,3>> g[N];
 vector<array<ll,4>> e;
-void dijkstra(ll b,ll mx){
+vector<ll> dijkstra(ll b,ll mx){
     priority_queue<pair<ll,ll>,vector<pair<ll,ll>>,greater<pair<ll,ll>>> q;
-    q.push({0,1});
+    q.push({0,b});
     vector<ll> dis(0,n+1);
-    dis[1]=0;
+    dis[b]=0;
     while(!q.empty()){
         auto x=q.top();
         q.pop();
         if(dis[x.second]>x.first) continue;
         for(auto i:g[x.second]){
-            
+            ll u=x.second,v=i[0],w=i[1],w2=i[2];
+            if(w2>mx) continue;
+            if(dis[v]>x.first+w){
+                dis[v]=x.first+w;
+                q.push({dis[v],v});
+            }
         }
     }
+    return dis;
 }
 void solve(){
     cin>>n>>m;
@@ -29,7 +35,7 @@ void solve(){
         e.push_back({u,v,w,w2});
     }
     for(int i=1;i<=n;i++){
-
+        
     }
 }
 int main(){
