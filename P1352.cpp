9315bce3e779a6413dfebf0f88b124bc6306dfd8
yusiwen <3907363360@qq.com>
2026-05-03 04:23:24 +0000

    2026年5月3日 12:23

--- P1352.cpp
+++ P1352.cpp
@@ -1,39 +1,15 @@
 #include<bits/stdc++.h>
-#define ll long long
 using namespace std;
+#define ll long long
 #define el '\n'
-const ll N=6e3+5;
+const ll N=1e5+5;
 ll n;
-vector<ll> g[N];
 ll happy[N];
-ll dep[N];
-void dfs(ll u,ll fa){
-    dep[u]=dep[fa]+1;
-    for(auto i:g[u]) if(i!=fa) dfs(i,u);
-}   
+vector<ll> a[N];
+bool fa[N];
 void solve(){
-    cin>>n;
-    for(int i=1;i<=n;i++){
-        cin>>happy[i];
-    }
-    for(int i=1;i<n;i++){
-        ll u,v;cin>>u>>v;
-        g[u].push_back(v);
-        g[v].push_back(u);
-    }
-    ll ans=0;
-    for(int i=1;i<=n;i++){
-        memset(dep,0,sizeof(dep));
-        dfs(1,0);
-        ll sum1=0,sum2=0;
-        for(int i=1;i<=n;i++){
-            if(dep[i]%2) sum1+=happy[i];
-            else sum2+=happy[i];
-        }
-        ans=max(max(sum1,sum2),ans);
-    }
-    cout<<ans;
-}   
+    
+}
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
