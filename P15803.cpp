0a6be52fead60a1175081b55d037cd540b672bbe
yusiwen <3907363360@qq.com>
2026-05-03 02:27:20 +0000

    2026年5月3日 10:27

--- P15803.cpp
+++ P15803.cpp
@@ -5,7 +5,14 @@ using namespace std;
 const ll N=1e5+5;
 ll n,m;
 vector<array<ll,3>> g[N];
+vector<array<ll,4>> e;
 void solve(){
+    cin>>n>>m;
+    for(int i=1;i<=m;i++){
+        ll u,v,w,w2;
+        g[u].push_back({v,w,w2});
+        g[v].push_back({u,w,w2});
+    }
     
 }
 int main(){
