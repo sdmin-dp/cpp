fc8daac2e6ea67bd228583c94a3b0d4d534a47d9
yusiwen <3907363360@qq.com>
2026-05-03 04:29:18 +0000

    2026年5月3日 12:29

--- P1352.cpp
+++ P1352.cpp
@@ -5,10 +5,38 @@ using namespace std;
 const ll N=1e5+5;
 ll n;
 ll happy[N];
-vector<ll> a[N];
+vector<ll> g[N];
 bool fa[N];
+pair<ll,ll> dp[N];
+void dfs(ll u){
+    dp[u].first=0;
+    dp[u].second=happy[u];
+    for(auto v:g[u]){
+        dfs(v);
+        dp[u].first+=max(dp[v].first,dp[v].second);
+        dp[u].second+=dp[v].first;
+    }
+}
 void solve(){
-    
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        cin>>happy[i];
+    }
+    for(int i=1;i<n;i++){
+        ll u,v;
+        cin>>v>>u;
+        fa[u]=1;
+        g[u].push_back(v);
+    }
+    ll root=0;
+    for(int i=1;i<=n;i++){
+        if(!fa[i]){
+            root=i;
+            break;
+        }
+    }
+    dfs(root);
+    cout<<max(dp[root].first,dp[root].second);
 }
 int main(){
     ios::sync_with_stdio(0);
