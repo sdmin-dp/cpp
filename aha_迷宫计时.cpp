5789726a1e6f20bdc6363acaff9723e3da8b53c9
yusiwen <3907363360@qq.com>
2026-05-06 12:43:34 +0000

    2026年5月6日 20:43

---
+++ aha_迷宫计时.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll int
+#define el '\n'
+const ll N=15+5;
+struct node{
+    ll x,y,step;
+};
+ll dx[]={0,1,0,-1},dy[]={1,0,-1,0};
+ll n,m,t;
+char a[N][N];
+bool vis[N][N];
+ll bx,by,ex,ey;
+bool flag=0;
+void dfs(ll x,ll y,ll step){
+    if(flag) return;
+    ll dis=abs(x-ey)+abs(y-ey);
+    ll shenyu=t-step;
+    if(dis>shenyu&&abs(dis-shenyu)%2!=0) return;
+    if(x==ex&&y==ey&&step==t){
+        flag=1;
+        return;
+    }
+    for(int i=0;i<4;i++){
+        ll xx=x+dx[i],yy=y+dy[i];
+        if(xx>=1&&xx<=n&&yy>=1&&yy<=m&&!vis[xx][yy]&&a[xx][yy]!='X'){
+            vis[xx][yy]=1;
+            dfs(xx,yy,step+1);
+            vis[xx][yy]=0;
+        }
+    }
+}
+void solve(){
+    while(cin>>n>>m>>t){
+        flag=0;
+        if(n==0&&m==0&&t==0) return;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                cin>>a[i][j];
+                if(a[i][j]=='S') bx=i,by=j;
+                if(a[i][j]=='D') ex=i,ey=j;
+            }
+        }
+        dfs(bx,by,0);
+        if(flag) cout<<"YES"<<el;
+        else cout<<"NO"<<el;
+    }
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    //freopen("xxx.in","r",stdin);
+    //freopen("xxx.out","w",stdout);
+    ll T=1;
+    //cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
