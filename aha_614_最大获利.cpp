49b32a171dda5495da749f688124442f2b159aa0
yusiwen <3907363360@qq.com>
2026-05-07 13:03:19 +0000

    2026年5月7日 21:03

--- aha_614_最大获利.cpp
+++ aha_614_最大获利.cpp
@@ -20,7 +20,8 @@ ll& dis(ll x,ll y){
 }
 void dijkstra(){
     priority_queue<node> q;
-    q.push({0,b});
+    if(eat[b]==1) q.push({b,0,1,0});
+    else q.push({b,0,0,1});
     for(int i=1;i<=n;i++) for(int j=-10;j<=10;j++) dis(i,j)=inf;
     memset(dist,0x3f,sizeof(dist));
     dis(b,1)=0;
