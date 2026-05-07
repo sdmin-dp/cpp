ea746574c56b7fd8b200c103e2e6cd61a8c1adcc
yusiwen <3907363360@qq.com>
2026-05-07 08:15:45 +0000

    2026年5月7日 16:15

--- aha_迷宫计时.cpp
+++ aha_迷宫计时.cpp
@@ -1,63 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define ll int
+#define ll long long
 #define el '\n'
-const ll N=7+3;
-struct node{
-    ll x,y,step;
-};
-ll dx[]={0,1,0,-1},dy[]={1,0,-1,0};
-ll n,m,t;
-char a[N][N];
-bool vis[N][N];
-ll bx,by,ex,ey;
-bool flag=0;
-void dfs(ll x,ll y,ll step){
-    if(step>t) return;
-    if(flag) return;
-    ll dis=abs(x-ex)+abs(y-ey);
-    ll shenyu=t-step;
-    if(dis>shenyu||abs(dis-shenyu)%2!=0) return;
-    if(x==ex&&y==ey&&step==t){
-        flag=1;
-        return;
-    }
-    for(int i=0;i<4;i++){
-        ll xx=x+dx[i],yy=y+dy[i];
-        if(xx>=1&&xx<=n&&yy>=1&&yy<=m&&!vis[xx][yy]&&a[xx][yy]!='X'){
-            vis[xx][yy]=1;
-            dfs(xx,yy,step+1);
-            vis[xx][yy]=0;
-        }
-    }
-}
+const ll N=1e5+5;
+
 void solve(){
-    while(~(scanf("%d%d%d",&n,&m,&t))){
-        // printf("%d %d %d",n,m,t);
-        memset(vis,0,sizeof vis);
-        flag=0;
-        if(n==0&&m==0&&t==0) return;
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                scanf(" %c",&a[i][j]);
-                if(a[i][j]=='S') bx=i,by=j;
-                if(a[i][j]=='D') ex=i,ey=j;
-            }
-        }
-        vis[bx][by]=1;
-        if(abs(bx-ex)+abs(by-ey)>t){
-            // cout<<"NO"<<el;
-            printf("NO\n");
-            continue;
-        }
-        dfs(bx,by,0);
-        if(flag) printf("YES\n");
-        else printf("NO\n");
-    }
+    
 }
 int main(){
-    // ios::sync_with_stdio(0);
-    // cin.tie(0);cout.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
     //freopen("xxx.in","r",stdin);
     //freopen("xxx.out","w",stdout);
     ll T=1;
@@ -67,11 +19,3 @@ int main(){
     }
     return 0;
 }
-
-/*
-4 4 6
-S...
-....
-....
-...D
-*/
