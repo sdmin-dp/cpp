5d50a5d756d3b4805da7f93c8811781ec3ce73b5
yusiwen <3907363360@qq.com>
2026-05-07 08:06:47 +0000

    2026年5月7日 16:06

--- aha_迷宫计时.cpp
+++ aha_迷宫计时.cpp
@@ -14,7 +14,6 @@ ll bx,by,ex,ey;
 bool flag=0;
 void dfs(ll x,ll y,ll step){
     if(step>t) return;
-    if(step==t&&(x!=ex||y!=ey)) return;
     if(flag) return;
     ll dis=abs(x-ex)+abs(y-ey);
     ll shenyu=t-step;
