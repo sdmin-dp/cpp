183bfa9cc95cbc651bef284fb1aa0f009a04e339
yusiwen <3907363360@qq.com>
2026-05-07 12:33:06 +0000

    2026年5月7日 20:33

--- aha_614_最大获利.cpp
+++ aha_614_最大获利.cpp
@@ -3,10 +3,14 @@ using namespace std;
 #define ll long long
 #define el '\n'
 const ll N=1e4+5;
+
 ll n,m,k;
 vector<vector<pair<ll,ll>>> g;
 ll eat[N];
-ll dis[N][25]; 
+ll dist[N][25]; 
+ll& dis(ll x,ll y){
+    return dist[x][y+10];
+}
 void init(){
     g.clear();
 }
