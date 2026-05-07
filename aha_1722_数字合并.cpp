5ea7bf924ea224bf2e30c7e1851044d2502b23e8
yusiwen <3907363360@qq.com>
2026-05-07 14:02:55 +0000

    2026年5月7日 22:02

--- aha_1722_数字合并.cpp
+++ aha_1722_数字合并.cpp
@@ -17,8 +17,10 @@ void solve(){
     }
     for(ll i=1;i<=maxn;i++){
         for(int j=1;j<=n;j++){
-            dp[i][j]=dp[i-1][dp[i-1][j]+1];
-            if(dp[i][j]!=0) ans=max(i,ans);
+            if(dp[i][j]!=0){
+                dp[i][j]=dp[i-1][dp[i-1][j]+1];
+                if(dp[i][j]!=0) ans=max(i,ans);
+            }
         }
     }
     cout<<ans;
