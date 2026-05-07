fc3e21283a7c90297a051c15d931e5ab376fd741
yusiwen <3907363360@qq.com>
2026-05-07 11:01:57 +0000

    2026年5月7日 19:01

--- P10724.cpp
+++ P10724.cpp
@@ -8,9 +8,16 @@ ll a[N];
 void solve(){
     cin>>n;
     for(int i=1;i<=n;i++) cin>>a[i];
-    for(int i=1;i<=n;i++){
-        
+    ll cnt=0;
+    for(int l=1;l<=n;l++){
+        for(int r=l;r<=n;r++){
+            ll sum=1;
+            for(int i=l;i<=r;i++) sum*=a[i];
+            ll k=sqrt(sum);
+            if(k*k==sum) cnt++;
+        }
     }
+    cout<<cnt;
 }
 int main(){
     ios::sync_with_stdio(0);
