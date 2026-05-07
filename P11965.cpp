71f68d784dc9993738f1981e22985df3d0f6aac8
yusiwen <3907363360@qq.com>
2026-05-07 09:15:56 +0000

    2026年5月7日 17:15

--- P11965.cpp
+++ P11965.cpp
@@ -2,10 +2,24 @@
 using namespace std;
 #define ll long long
 #define el '\n'
-const ll N=1e5+5;
-ll n,m;
+const ll N=2e5+5;
+ll n;
+string s;
+ll xr[N];
+map<ll,ll> mp;
 void solve(){
-    
+    cin>>n;
+    cin>>s;
+    s=' '+s;
+    for(int i=1;i<=n;i++){
+        ll k=(1<<(s[i]-'a'));
+        xr[i]=xr[i-1]^k;
+    }
+    ll cnt=0;
+    for(int i=1;i<=n;i++){
+        cnt+=mp.count(xr[i]);
+        mp[xr[i]]++;
+    }
 }
 int main(){
     ios::sync_with_stdio(0);
