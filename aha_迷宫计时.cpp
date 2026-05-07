dac255979f8c0318798d14dca47a5805756b0aab
yusiwen <3907363360@qq.com>
2026-05-07 08:16:11 +0000

    2026年5月7日 16:16

--- aha_迷宫计时.cpp
+++ aha_迷宫计时.cpp
@@ -3,13 +3,13 @@ using namespace std;
 #define ll long long
 #define el '\n'
 const ll N=7+5;
-
+ll n,m,t;
 void solve(){
     
 }
 int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);cout.tie(0);
+    // ios::sync_with_stdio(0);
+    // cin.tie(0);cout.tie(0);
     //freopen("xxx.in","r",stdin);
     //freopen("xxx.out","w",stdout);
     ll T=1;
