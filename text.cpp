6e083d99f797259ead0fe56d27b90844e8f5f2da
yusiwen <3907363360@qq.com>
2026-05-10 10:11:37 +0000

    2026年5月10日 18:11

--- text.cpp
+++ text.cpp
@@ -11,25 +11,29 @@ bool vis[N];
 int fr (int x) {
     return (f[x] == x ? x : f[x] = fr(f[x]));
 }
+
 void init () {
     for (int i = 1; i <= n; i++)
         f[i] = i;
 }
-void dfs(ll u, ll fa) {
+
+void dfs(int x, int fa)
+{
     int dd = -1, ddd = -1;
-    for (auto v : G[u]) {
+    for (auto v : G[x])
+    {
         if (v == fa)
             continue;
-        dfs(v, u);
+        dfs(v, x);
         int t = dis[v] + 1;
-        dis[u] = max(dis[u], t);
+        dis[x] = max(t, dis[x]);
         if (t > dd)
             ddd = dd, dd = t;
         else if (t > ddd)
             ddd = t;
     }
-    g[u] = max(dd, ddd);
-    res = max(res, g[u]);
+    g[x] = max(dd, ddd);
+    res = max(res, g[x]);
 }
 
 void solve() {
@@ -38,10 +42,10 @@ void solve() {
     for (int i = 1; i <= m; i++) {
         int x, y;
         cin >> x >> y;
-        f[fr(x)] = fr(y);
         G[x].push_back(y);
         G[y].push_back(x);
     }
+ 
     for (int i = 1; i <= n; i++) {
         if (vis[i] || f[i] != i) {
             continue;
@@ -51,6 +55,7 @@ void solve() {
         ans[i] = res;
         vis[i] = true;
     }
+
     for (int i = 1; i <= q; i++) {
         int op;
         cin >> op;
@@ -66,10 +71,10 @@ void solve() {
             if (x == y)
                 continue;
             // for (int i = 1; i <= n; i++)
-            //     cout << ans[i] << ' ';
-            int t = max(((ans[x] + 1) >> 1 + (ans[y] + 1) >> 1) + 1, max(ans[x], ans[y]));
-            f[x] =y;
-            ans[y] = t;
+                // cout << ans[i] << ' ';
+            int t = max((((ans[x] + 1) >> 1) + ((ans[y] + 1) >> 1)) + 1, max(ans[x], ans[y]));
+            f[fr(x)] = fr (y);
+            ans[fr(x)] = t;
         }
     }
 }
