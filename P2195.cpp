0a7c888e8076c37147aab8ff6035b2df31eabc54
yusiwen <3907363360@qq.com>
2026-05-10 09:13:58 +0000

    2026年5月10日 17:13

--- P2195.cpp
+++ P2195.cpp
@@ -2,55 +2,10 @@
 using namespace std;
 #define ll long long
 #define el '\n'
-const int N=3e5+5;
-vector<int>g[N];
-ll n,m,q;
-ll f[N],d[N],v[N];
-ll L,P;
-ll find(ll x){
-    if(x==f[x]) return x;
-    return f[x]=find(f[x]);
-}
-void dfs(int u,int p,int s){
-    if(s>L) L=s,P=u;
-    for(int v:g[u]) if(v!=p) dfs(v,u,s+1);
-}
-int get_d(int u){
-    L=-1;
-    dfs(u,-1,0);
-    L=-1;
-    dfs(P,-1,0);
-    return L;
-}
+const ll N=1e5+5;
+
 void solve(){
-    cin>>n>>m>>q;
-    for(int i=1;i<=n;i++)f[i]=i;
-    while(m--){
-        int u,v;cin>>u>>v;
-        g[u].push_back(v);
-        g[v].push_back(u);
-        ll fu=find(u),fv=find(v);
-        if(fu!=fv) f[fu]=fv;
-    }
-    for(int i=1;i<=n;i++){
-        int r=find(i);
-        if(!v[r]){
-            d[r]=get_d(i);
-            v[r]=1;
-        }
-    }
-    while(q--){
-        int o,x,y;cin>>o>>x;
-        if(o==1) cout<<d[find(x)]<<el;
-        else{
-            cin>>y;
-            int fx=find(x),fy=find(y);
-            if(fx!=fy){
-                d[fy]=max({d[fx],d[fy],(d[fx]+1)/2+(d[fy]+1)/2+1});
-                f[fx]=fy;
-            }
-        }
-    }
+    
 }
 int main(){
     ios::sync_with_stdio(0);
