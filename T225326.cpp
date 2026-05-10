60a29040914b702052cc1a576d49bb104943ba87
yusiwen <3907363360@qq.com>
2026-05-10 08:32:03 +0000

    2026年5月10日 16:32

--- T225326.cpp
+++ T225326.cpp
@@ -1,19 +1,34 @@
 #include<bits/stdc++.h>
-using namespace std;
 #define ll long long
+using namespace std;
 #define el '\n'
-const ll N=1e5+5;
+const ll N=4e4+5;
 ll n;
 vector<pair<ll,ll>> g[N];
-
+ll ans=-1e12,k;
+void dfs(ll x,ll fa,ll dis){
+    if(dis>ans){
+        k=x;
+        ans=dis;
+    }
+    for(auto i:g[x]){
+        ll v=i.first;
+        if(v==fa) continue;
+        dfs(v,x,dis+i.second);
+    }
+}
 void solve(){
     cin>>n;
     for(int i=1;i<n;i++){
-        ll u,v,w;
-        cin>>u>>v>>w;
-        g[u].push_back({v,w});
-        g[v].push_back({u,w});
+        ll x,y,z;
+        cin>>x>>y>>z;
+        g[x].push_back({y,z});
+        g[y].push_back({x,z});
     }
+    dfs(1,0,0);
+    ans=-1e12;
+    dfs(k,0,0);
+    cout<<ans;
 }
 int main(){
     ios::sync_with_stdio(0);
