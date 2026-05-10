f5b9a98cbfce7e1d9fa28f18bb941e2e8275ee3f
yusiwen <3907363360@qq.com>
2026-05-10 12:02:11 +0000

    2026年5月10日 20:02

--- P3304.cpp
+++ P3304.cpp
@@ -2,11 +2,18 @@
 using namespace std;
 #define ll long long
 #define el '\n'
-const ll N=1e5+5;
+const ll N=2e5+5;
 ll n;
 vector<pair<ll,ll>> g[N];
+
 void solve(){
-    
+    cin>>n;
+    for(int i=1;i<n;i++){
+        ll u,v,w;
+        cin>>u>>v>>w;
+        g[u].push_back({v,w});
+        g[v].push_back({u,w});
+    }
 }
 int main(){
     ios::sync_with_stdio(0);
