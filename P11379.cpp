e58466b2da01854dec0d38d24768f3dd226822a8
yusiwen2014 <3907363360@qq.com>
2026-05-19 13:51:51 +0000

    2026年5月19日 21:51

--- P11379.cpp
+++ P11379.cpp
@@ -2,13 +2,27 @@
 using namespace std;
 #define ll long long
 #define el '\n'
-const ll N=1e5+5;
+const ll N=1e3+5;
 ll n,k;
 bool color[N];
 vector<ll> g[N];
-void solve(){
-    cin>>n;
+ll ans;
+void dfs(ll cnt,ll stp,ll x,ll fa){
+    if(cnt<=k) ans=max(ans,stp);
+    else return;
+    for(auto i:g[x]) if(i!=fa) dfs(cnt+color[i],stp+1,i,x);
     
+}
+void solve(){
+    cin>>n>>k;
+    for(int i=1;i<=n;i++) cin>>color[i];
+    for(int i=1,u,v;i<n;i++){
+        cin>>u>>v;
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+    for(int i=1;i<=n;i++) dfs(color[i],1,i,0);
+    cout<<ans;
 }
 int main(){
     ios::sync_with_stdio(0);
