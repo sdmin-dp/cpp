3fddd20b13ffdd0618f61014239bf65a356fde59
yusiwen2014 <3907363360@qq.com>
2026-05-26 19:00:32 +0800

    modified:   P10725.cpp 	deleted:    P1462_2.in 	deleted:    P1462_5.in 	deleted:    P1525 	deleted:    P16236

--- P10725.cpp
+++ P10725.cpp
@@ -4,29 +4,15 @@ using namespace std;
 #define el '\n'
 const ll N=1e5+5;
 ll n;
-ll color[N];
+bool color[N];
 vector<ll> g[N];
-ll b,ans,dist;
-ll pre[N];
-void dfs(ll x,ll fa,ll dis){
-    pre[x]=fa;
-    if(dis>dist){
-        b=x;
-        dist=dis;
-    }
+ll dep[N];
+ll dfs(ll x,ll fa){
+    dep[x]=dep[fa]+1;
     for(auto i:g[x]){
         if(i==fa) continue;
-        dfs(i,x,dis+1);
-    }
-}
-void dfs2(ll x,ll fa,ll dis){
-    if(dis>dist&&color[x]!=color[b]){
-        // b=x;
-        dist=dis;
-    }
-    for(auto i:g[x]){
-        if(i==fa) continue;
-        dfs(i,x,dis+1);
+        dfs(i,x);
+
     }
 }
 void solve(){
@@ -38,10 +24,7 @@ void solve(){
         g[u].push_back(v);
         g[v].push_back(u);
     }
-    dfs(1,0,0);
-    dist=0;
-    dfs2(b,0,0);
-    cout<<dist;
+
 }
 int main(){
     ios::sync_with_stdio(0);
