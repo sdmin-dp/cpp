34614323c2fb831df4312bb646d6d7622714d0e8
yusiwen2014 <3907363360@qq.com>
2026-06-23 18:13:32 +0800

    modified:   P10265.cpp

--- P10265.cpp
+++ P10265.cpp
@@ -3,19 +3,16 @@ using namespace std;
 #define ll long long
 #define el '\n'
 const ll N=1e5+5;
-ll n,k;
-ll x;
-ll cnt1,cnt2;
+ll n,m;
 void solve(){
-    cin>>n>>k;
+    cin>>n>>m;
     for(int i=1;i<=n;i++){
         for(int j=1;j<=n;j++){
-            cin>>x;
-            if(i==k&&x==1) cnt1++;
-            if(j==k&&x==1) cnt2++;
+            bool flag;
+            cin>>flag;
+            if(flag) 
         }
     }
-    cout<<cnt1<<" "<<cnt2<<" "<<cnt1+cnt2;
 }
 int main(){
     ios::sync_with_stdio(0);
