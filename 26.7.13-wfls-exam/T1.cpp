0295e221f0d29d39fc835bf21b892b668b5e4074
yusiwen <3907363360@qq.com>
2026-07-13 05:48:04 +0000

    2026年7月13日 13:48

---
+++ 26.7.13-wfls-exam/T1.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define el '\n'
+const ll N=1e7+5;
+ll l,r;
+bool isprime[N];
+vector<ll> prime;
+void init(){
+    
+}
+void solve(){
+    cin>>l>>r;
+    
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    //freopen("xxx.in","r",stdin);
+    //freopen("xxx.out","w",stdout);
+    ll T=1;
+    //cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
