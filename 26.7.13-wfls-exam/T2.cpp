02f98f2526a6dfa65ba631fcff802b5b408920b3
yusiwen <3907363360@qq.com>
2026-07-13 07:56:08 +0000

    2026年7月13日 15:56

--- 26.7.13-wfls-exam/T2.cpp
+++ 26.7.13-wfls-exam/T2.cpp
@@ -2,28 +2,26 @@
 using namespace std;
 #define ll long long
 #define el '\n'
-const ll N = (1 << 20) + 5;
-ll n, m;
+const ll N=(1<<20)+5;
+ll n,m;
 bool vis[N];
 void solve(){
-    cin >> n >> m;
-    for(ll i = 1; i <= n; i++){
-        ll a;
-        cin >> a;
-        vis[a] = 1;
+    cin>>n>>m;
+    for(ll i=1;i<=n;i++){
+        ll a;cin>>a;
+        vis[a]=1;
     }
-    for(ll i = 19; i >= 0; i--){
-        for(ll mask = (1 << 20) - 1; mask >= 0; mask--){
-            if((mask >> i) & 1){
-                if(vis[mask]) vis[mask ^ (1 << i)] = 1;
+    for(ll i=19;i>=0;i--){
+        for(ll mask=(1<<20)-1;mask>=0;mask--){
+            if((mask>>i)&1){
+                if(vis[mask])vis[mask^(1<<i)]=1;
             }
         }
     }
-    for(ll i = 1; i <= m; i++){
-        ll x;
-        cin >> x;
-        if(x >= (1 << 20)) cout<<"no"<<el;
-        else if(vis[x]) cout<<"yes"<<el;
+    for(ll i=1;i<=m;i++){
+        ll x;cin>>x;
+        if(x>=(1<<20))cout<<"no"<<el;
+        else if(vis[x])cout<<"yes"<<el;
         else cout<<"no"<<el;
     }
 }
@@ -31,8 +29,6 @@ int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
     ll T=1;
-    while(T--){
-        solve();
-    }
+    while(T--){solve();}
     return 0;
 }
