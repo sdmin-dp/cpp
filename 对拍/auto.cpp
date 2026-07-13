3fe5dcb502717e9039a1a5230f68320122f16d62
yusiwen <3907363360@qq.com>
2026-07-13 02:18:36 +0000

    2026年7月13日 10:18

---
+++ 对拍/auto.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define el '\n'
+const ll N=1e5+5;
+ll n;
+void solve(){
+    cout<<"请输入想运行的测试样例组数:";
+    cin>>n;
+    system("g++ -std=c++14 -O2 ac.cpp -o ac");
+    system("g++ -std=c++14 -O2 ac.cpp -o ac");
+
+    for(int i=1;i<=n;i++){
+        
+    }
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    //freopen("xxx.in","r",stdin);
+    //freopen("xxx.out","w",stdout);
+    ll T=1;
+    //cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
