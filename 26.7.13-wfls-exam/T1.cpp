4ac983f7130b1e1aa21d5904692ab95f3e14ee4b
yusiwen <3907363360@qq.com>
2026-07-13 06:08:54 +0000

    2026年7月13日 14:08

--- 26.7.13-wfls-exam/T1.cpp
+++ 26.7.13-wfls-exam/T1.cpp
@@ -4,27 +4,44 @@ using namespace std;
 #define el '\n'
 const ll N=1e7+5;
 ll l,r;
-bool isprime[N];
-void init(){
-    
+ll min_prime[N];
+bool is_power[N];
+vector<ll> prime;
+
+void init(int n){
+    is_power[1] = false;
+    for(ll i=2; i<=n; i++){
+        if(!min_prime[i]){
+            min_prime[i] = i;
+            is_power[i] = true;
+            prime.push_back(i);
+        }
+        for(auto j:prime){
+            if(i*j > n) break;
+            min_prime[i*j] = j;
+            if(i % j == 0){
+                is_power[i*j]=(is_power[i]&& (min_prime[i] == j));
+                break;
+            }
+            is_power[i*j]=0;
+        }
+    }
 }
 void solve(){
     cin>>l>>r;
-    init();
+    init(r);
     ll ans=0;
     for(int i=l;i<=r;i++){
-        if(!isprime[i]) ans+=i;
-        else ans++;
+        if(i == 1) continue;
+        if(is_power[i]) ans+=min_prime[i];
+        else ans+=1;
     }    
-    cout<<ans;
+    cout<<ans<<el;
 }
 int main(){
     ios::sync_with_stdio(0);
-    cin.tie(0);cout.tie(0);
-    //freopen("xxx.in","r",stdin);
-    //freopen("xxx.out","w",stdout);
+    cin.tie(0); cout.tie(0);
     ll T=1;
-    //cin>>T;
     while(T--){
         solve();
     }
