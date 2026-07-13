aef9b582c07744aa1b291b0ffc88259192d9c370
yusiwen <3907363360@qq.com>
2026-07-13 02:27:46 +0000

    2026年7月13日 10:27

--- 对拍/auto.cpp
+++ 对拍/auto.cpp
@@ -10,9 +10,9 @@ void solve(){
     system("g++ -std=c++14 -O2 ac.cpp -o ac.exe");
     system("g++ -std=c++14 -O2 baoli.cpp -o baoli.exe");
     for(int i=1;i<=n;i++){
-        system("ac.exe");
-        system("baoli.exe");
-        if(system("fc output1 output2")){
+        system("./ac.exe");
+        system("./baoli.exe");
+        if(system("fc output1.out output2.out")){
             cout<<"发现错误,请查看input和output文件";
             break;
         }
