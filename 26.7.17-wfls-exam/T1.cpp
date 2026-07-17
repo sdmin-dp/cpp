5375fed7c995762eebdc23e9ce0245a59f96ab1f
yusiwen <3907363360@qq.com>
2026-07-17 00:46:56 +0000

    2026年7月17日 08:46

--- 26.7.17-wfls-exam/T1.cpp
+++ 26.7.17-wfls-exam/T1.cpp
@@ -2,11 +2,23 @@
 using namespace std;
 #define ll long long
 #define el '\n'
-const ll N=1e5+5;
+const ll N=5e5+5;
 ll n,m;
-pair<ll,ll> a[N];
+vector<ll> t;
 void solve(){
-    
+    cin>>n>>m;
+    ll ans=0,x=0;
+    for(int i=1;i<=n;i++){
+        ll u,v;
+        cin>>u>>v;
+        ans+=abs(u-v);
+        t.push_back(u);
+        t.push_back(v);
+    }
+    sort(t.begin(),t.end());
+    x=t[t.size()/2-1];
+    for(auto i:t) ans+=abs(i-x);
+    cout<<ans;
 }
 int main(){
     ios::sync_with_stdio(0);
