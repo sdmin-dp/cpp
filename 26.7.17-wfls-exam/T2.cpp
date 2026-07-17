042b50608b376e86899c29e5ec8ad50a268715d3
yusiwen <3907363360@qq.com>
2026-07-17 01:06:27 +0000

    2026年7月17日 09:06

--- 26.7.17-wfls-exam/T2.cpp
+++ 26.7.17-wfls-exam/T2.cpp
@@ -3,17 +3,23 @@ using namespace std;
 #define ll long long
 #define el '\n'
 const ll N=1e5+5;
-
+ll n;
+ll a[N];
+ll x[N];
+ll k=1;
 void solve(){
-    
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        cin>>a[i];
+        x[1]+=k*a[i];
+        k=-k;
+    }
+    for(int i=2;i<=n;i++) x[i]=a[i-1]*2-x[i-1];
+    for(int i=1;i<=n;i++) cout<<x[i]<<" ";
 }
 int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);cout.tie(0);
-    //freopen("xxx.in","r",stdin);
-    //freopen("xxx.out","w",stdout);
     ll T=1;
-    //cin>>T;
+    // cin>>T;
     while(T--){
         solve();
     }
