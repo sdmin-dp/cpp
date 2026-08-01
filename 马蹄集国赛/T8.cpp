1aa0ccb6d790f8ee91e46e7bcbea85b31287f974
yusiwen <3907363360@qq.com>
2026-08-01 07:04:34 +0000

    2026年8月1日 15:04

--- 马蹄集国赛/T8.cpp
+++ 马蹄集国赛/T8.cpp
@@ -33,7 +33,7 @@ void solve(){
     ll cnt=0;
     for(int i=1;i<=n;i++){
         for(int j=1;j<=n;j++){
-            auto t=query(i,j);
+            // auto t=query(i,j);
             if(t.first-t.second<=m) cnt++;
         }
     }
