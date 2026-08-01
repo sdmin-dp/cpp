c869d527a087f08b697313cc7584a2e2a55d8e5f
yusiwen <3907363360@qq.com>
2026-08-01 07:04:05 +0000

    2026年8月1日 15:04

--- 马蹄集国赛/T8.cpp
+++ 马蹄集国赛/T8.cpp
@@ -29,11 +29,14 @@ void solve(){
     cin>>n>>m;
     for(int i=1;i<=n;i++) cin>>a[i];
     ll mx=-1e12,mn=1e12;
+    ll cnt=0;
     for(int i=1;i<=n;i++){
         for(int j=1;j<=n;j++){
-
+            auto t=query(i,j);
+            if(t.first-t.second<=m) cnt++;
         }
     }
+    cout<<cnt;
 }
 int main(){
     ios::sync_with_stdio(0);
