f249522d5aaa5a233eb939eb9d2bfecbb5080b23
yusiwen <3907363360@qq.com>
2026-08-01 08:58:15 +0000

    2026年8月1日 16:58

--- 马蹄集国赛/T9.cpp
+++ 马蹄集国赛/T9.cpp
@@ -29,7 +29,7 @@ void solve(){
                     else dp[rx][ry].second=min({dp[rx-1][ry].second,dp[rx][ry-1].second,a[rx][ry]});
                     if(dp[rx][ry].first-dp[rx][ry].second<=k){
                         cnt++;
-                        cerr<<" "<<rx<<" "<<ry<<" "<<dp[rx][ry].first<<" "<<dp[rx][ry].second<<el;
+                        cer<<rx<<" "<<ry<<" "<<dp[rx][ry].first<<" "<<dp[rx][ry].second<<el;
                     }
                     
                 }
