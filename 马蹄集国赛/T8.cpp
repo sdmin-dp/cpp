113dad417d3617a65be837412a4995821d74b0af
yusiwen <3907363360@qq.com>
2026-08-01 06:43:20 +0000

    2026年8月1日 14:43

--- 马蹄集国赛/T8.cpp
+++ 马蹄集国赛/T8.cpp
@@ -2,10 +2,19 @@
 using namespace std;
 #define ll long long
 #define el '\n'
-const ll N=1e5+5;
+const ll N=1e4+5;
 ll n,m;
+ll a[N];
 void solve(){
-    
+    cin>>n>>m;
+    for(int i=1;i<=n;i++) cin>>a[i];
+    ll mx=-1e12,mn=1e12;
+    for(int i=1;i<=n;i++){
+        mx=max(mx,a[i]);mn=min(mn,a[i]);
+        while(mx){
+
+        }
+    }
 }
 int main(){
     ios::sync_with_stdio(0);
