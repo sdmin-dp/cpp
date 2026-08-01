4d05709946bb9b59538419209a077e694de5d816
yusiwen <3907363360@qq.com>
2026-08-01 06:19:12 +0000

    2026年8月1日 14:19

--- 马蹄集国赛/T4.cpp
+++ 马蹄集国赛/T4.cpp
@@ -2,11 +2,22 @@
 using namespace std;
 #define ll long long
 #define el '\n'
-const ll N=1e5+5;
+const ll N=1e6+5;
 ll n;
 ll a[N];
+bool flag=1;
 void solve(){
-    
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        cin>>a[i];
+    }
+    for(int i=2;i<=n;i++){
+        if(a[i]!=a[i-1]){
+            flag=0;
+            break;
+        }
+    }
+    cout<<(flag?0:1);
 }
 int main(){
     ios::sync_with_stdio(0);
