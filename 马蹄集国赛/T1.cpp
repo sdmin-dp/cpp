35e98086cce681feae4e5ffa52eabdd447d26507
yusiwen <3907363360@qq.com>
2026-08-01 06:03:01 +0000

    2026年8月1日 14:03

---
+++ 马蹄集国赛/T1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+#define ll long long
+#define el '\n'
+const ll N=1e5+5;
+ll n;
+void solve(){
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        cout<<"matiji"<<el;
+    }
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    //freopen("xxx.in","r",stdin);
+    //freopen("xxx.out","w",stdout);
+    ll T=1;
+    //cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
