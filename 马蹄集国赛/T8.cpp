30f5f62c20c5554920abb5adaf401ee0946fdfb3
yusiwen <3907363360@qq.com>
2026-08-01 06:52:09 +0000

    2026年8月1日 14:52

--- 马蹄集国赛/T8.cpp
+++ 马蹄集国赛/T8.cpp
@@ -3,8 +3,20 @@ using namespace std;
 #define ll long long
 #define el '\n'
 const ll N=1e4+5;
+const ll mxlog=20;
 ll n,m;
 ll a[N];
+pair<ll,ll> st[20][N];
+ll lg[N];
+void init(){
+    for(int i=;i<=n;i++) lg[i]=lg[i-1]/2;
+    for(int i=1;i<=n;i++) st[0][i].first=st[0][i].second=a[i];
+    for(int k=1;k<=mxlog;k++){
+        for(int i=1;i<=n;i++){
+            st[k][i]
+        }
+    }
+}
 void solve(){
     cin>>n>>m;
     for(int i=1;i<=n;i++) cin>>a[i];
