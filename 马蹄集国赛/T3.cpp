4126d329cf44939e58b6853b5b37f343126003b5
yusiwen <3907363360@qq.com>
2026-08-01 06:15:16 +0000

    2026年8月1日 14:15

--- 马蹄集国赛/T3.cpp
+++ 马蹄集国赛/T3.cpp
@@ -5,15 +5,20 @@ using namespace std;
 const ll N=1e5+5;
 ll n,m;
 ll a[N],b[N];
+ll sum=0,res=1;
 void solve(){
     cin>>n>>m;
     for(int i=1;i<=n;i++){
         cin>>a[i];
+        res*=a[i];
     }
     for(int i=1;i<=m;i++){
         cin>>b[i];
+        sum+=b[i];
     }
-    
+    if(res==1) cout<<1;
+    else if(res==-1&&sum%2==1) cout<<-1;
+    else cout<<1;
 }
 int main(){
     ios::sync_with_stdio(0);
