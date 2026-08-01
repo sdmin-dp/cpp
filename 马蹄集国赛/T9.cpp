67e7635a323194580cdbb3662689e434a2fb9b9d
yusiwen <3907363360@qq.com>
2026-08-01 07:30:26 +0000

    2026年8月1日 15:30

--- 马蹄集国赛/T9.cpp
+++ 马蹄集国赛/T9.cpp
@@ -4,9 +4,14 @@ using namespace std;
 #define el '\n'
 const ll N=1e2+5;
 ll n,m,k;
-
+ll a[N][N];
 void solve(){
-    
+    cin>>n>>m>>k;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            cin>>a[i][j];
+        }
+    }
 }
 int main(){
     ios::sync_with_stdio(0);
