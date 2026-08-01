f03cd2e2b723645b80b157279bf15a04795c1f28
yusiwen <3907363360@qq.com>
2026-08-01 06:23:48 +0000

    2026年8月1日 14:23

--- 马蹄集国赛/T5.cpp
+++ 马蹄集国赛/T5.cpp
@@ -3,9 +3,22 @@ using namespace std;
 #define ll long long
 #define el '\n'
 const ll N=1e5+5;
-
+ll n;
+bool ishuiwen(ll x){
+    string s=to_string(x);
+    string s2=s;
+    reverse(s.begin(),s.end());
+    return (s2==s);
+}
 void solve(){
-    
+    cin>>n;
+    ll cnt=0;
+    for(int i=1;i<=n;i++){
+        if(ishuiwen(i)){
+            cnt++;
+        }
+    }
+    cout<<cnt;
 }
 int main(){
     ios::sync_with_stdio(0);
