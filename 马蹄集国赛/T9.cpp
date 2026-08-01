a1cec01a134fa2ea920b67696c886fd6e8139dbe
yusiwen <3907363360@qq.com>
2026-08-01 08:17:44 +0000

    2026年8月1日 16:17

--- 马蹄集国赛/T9.cpp
+++ 马蹄集国赛/T9.cpp
@@ -5,34 +5,9 @@ using namespace std;
 const ll N=1e2+5;
 ll n,m,k;
 ll a[N][N];
-
+ll dp[N][N][N][N];
 void solve(){
-    cin>>n>>m>>k;
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=m;j++){
-            cin>>a[i][j];
-        }
-    }
-    ll cnt=0;
-    for(int lx=1;lx<=n;lx++){
-        for(int ly=1;ly<=n;ly++){
-            pair<ll,ll> dp={a[lx][ly],a[lx][ly]};
-            for(int rx=lx;rx<=n;rx++){
-                for(int ry=ly;ry<=n;ry++){
-                    dp.first=max(dp.first,a[rx][ry]);
-                    dp.second=min(dp.second,a[rx][ry]);
-                    // cerr<<lx<<" "<<ly<<" "<<rx<<" "<<ry<<el;
-                    if(dp.first-dp.second<=k){
-                        cnt++;
-                        
-                        cerr<<dp.first<<" "<<dp.second<<" "<<lx<<" "<<ly<<" "<<rx<<" "<<ry<<el;
-                    }
-                    // if(lx==1&&ly==1&&rx==2&&ry==2) cerr<<"shabi  "<<dp.first<<" "<<dp.second<<" "<<lx<<" "<<ly<<" "<<rx<<" "<<ry<<el;
-                }
-            }
-        }
-    }
-    cout<<cnt;
+    
 }
 int main(){
     ios::sync_with_stdio(0);
